934466aa54d91cbdf80bfc040cdedfeca46d5850
matheustkaczyk <matheus111904@hotmail.com>
2021-09-20 16:08:36 -0300

    Desafio 9 concluido

---
+++ desafio9.sql
@@ -0,0 +1,8 @@
+SELECT
+CONCAT(FirstName, ' ', LastName) AS `Nome completo`,
+COUNT(*) AS `Total de pedidos`
+FROM w3schools.orders O
+INNER JOIN w3schools.employees AS E
+  ON E.EmployeeID = O.EmployeeID
+GROUP BY O.EmployeeID
+ORDER BY 2;
