1be704176baeff0d7c83ebe666759f2d0643d952
matheustkaczyk <matheus111904@hotmail.com>
2021-09-20 16:07:07 -0300

    Desafio 1 concluido

---
+++ desafio1.sql
@@ -0,0 +1,6 @@
+SELECT C.COUNTRY_NAME AS `País`,
+CASE WHEN R.REGION_NAME = 'Europe' THEN 'incluído' ELSE 'não incluído' END AS `Status Inclusão`
+  FROM hr.countries C
+JOIN hr.regions R
+  ON C.REGION_ID = R.REGION_ID
+ORDER BY C.COUNTRY_NAME;
