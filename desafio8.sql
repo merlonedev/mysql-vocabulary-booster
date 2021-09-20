a99b9c8bcc9be4b9bbd48bdd95e7de41c125dc41
matheustkaczyk <matheus111904@hotmail.com>
2021-09-20 16:08:26 -0300

    Desafio 8 concluido

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+SELECT
+C.ContactName AS `Nome de contato`,
+S.ShipperName AS `Empresa que fez o envio`,
+O.OrderDate AS `Data do pedido`
+FROM w3schools.orders O
+JOIN w3schools.shippers S
+  ON S.ShipperID = O.ShipperID
+JOIN w3schools.customers C
+  ON O.CustomerID = C.CustomerID
+WHERE S.ShipperName IN ('Speedy Express', 'United Package')
+ORDER BY 1, 2, 3;
