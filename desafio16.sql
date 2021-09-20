0b126a27237ee9c02286bd66b4a6af2b03aa3c28
Thalles Carneiro <thallescarneiro@gmail.com>
2021-09-20 18:41:33 -0300

    Resolve os Desafios [16-20]

---
+++ desafio16.sql
@@ -0,0 +1,25 @@
+USE hr;
+
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(employeeEmail VARCHAR(200))
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE jobs_quantity INT;
+	SELECT
+		(SELECT
+			COUNT(*)
+		FROM
+			hr.job_history AS JH
+		WHERE
+			JH.EMPLOYEE_ID = E.EMPLOYEE_ID)
+	FROM
+		hr.employees AS E
+	WHERE
+		E.EMAIL = employeeEmail
+	INTO
+		jobs_quantity;
+	RETURN jobs_quantity;
+END $$
+DELIMITER ;
+
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR') AS `total_empregos`;
