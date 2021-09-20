61353c195eefb2fa1dac12ea458a4fd261c915f4
Caio Morato <morato_dev@protonmail.com>
2021-09-20 16:07:34 -0300

    req 7

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS 'Nome completo',
+jh.START_DATE AS 'Data de início',
+e.SALARY AS 'Salário'
+FROM hr.employees AS e
+JOIN hr.job_history AS jh
+ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) BETWEEN 01 AND 03
+ORDER BY FIRST_NAME, DATE(HIRE_DATE);
