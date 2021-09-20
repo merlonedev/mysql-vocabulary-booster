d2cc861f3466680d7635b3bde7cc981c523ee306
Erick Silva <erick.silva6653@gmail.com>
2021-09-20 18:54:02 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,15 @@
+SELECT 
+    jobs.JOB_TITLE AS Cargo,
+    ROUND(AVG(employees.SALARY), 2) AS `Média Salarial`,
+    CASE
+        WHEN AVG(employees.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN AVG(employees.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN AVG(employees.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        ELSE 'CEO'
+    END AS Senioridade
+FROM
+    hr.jobs AS jobs
+        INNER JOIN
+    hr.employees AS employees
+    ON jobs.JOB_ID = employees.JOB_ID
+GROUP BY Cargo;
