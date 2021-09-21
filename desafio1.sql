3a868994f07ea036a7d4011ddb2bc2a0ddf2501d
camila-mp <ca.malvp@gmail.com>
2021-09-21 15:47:13 -0300

    corrige nome de coluna em desafio1

--- desafio1.sql
+++ desafio1.sql
@@ -3,6 +3,6 @@
 --   *A primeira deve possuir o alias "País" e exibir o nome do país.
 --   *A segunda deve possuir o alias "Status Inclusão" e deve exibir "incluído" se o país em questão está incluso em "Europe", ou "não incluído", caso contrário.
 -- Os resultados devem estar ordenados pelo nome do país em ordem alfabética.
-SELECT countries.COUNTRY_NAME, IF(REGION_ID=1, 'incluído', 'não incluído') AS `Status Inclusão`
+SELECT countries.COUNTRY_NAME AS `País`, IF(REGION_ID=1, 'incluído', 'não incluído') AS `Status Inclusão`
 FROM hr.countries
 ORDER BY countries.COUNTRY_NAME;
