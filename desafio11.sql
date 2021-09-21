c74facd38b640aae6f82fe0756f0d6ea7c649d48
julio11lp <julio11lp@hotmail.com>
2021-09-21 18:45:36 -0300

    Desafio 11 feito com subqueries

--- desafio11.sql
+++ desafio11.sql
@@ -1,10 +1,18 @@
 SELECT 
     cust1.ContactName AS `Nome`,
     cust1.Country AS `País`,
-    COUNT(cust2.Country) - 1 AS `Número de compatriotas`
+    (SELECT 
+            COUNT(Country) - 1
+        FROM
+            w3schools.customers
+        GROUP BY Country
+        HAVING Country = cust1.Country) AS `Número de compatriotas`
 FROM
     w3schools.customers AS cust1
-INNER JOIN w3schools.customers AS cust2 ON cust2.Country = cust1.Country
-GROUP BY `Nome`
-HAVING COUNT(cust2.Country) - 1 >  0
+WHERE (SELECT 
+            COUNT(Country)
+        FROM
+            w3schools.customers
+        GROUP BY Country
+        HAVING Country = cust1.Country) > 0
 ORDER BY `Nome`;
