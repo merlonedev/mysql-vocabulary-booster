5ea162809d155ce4bad89e190dc6f1b45e365ec3
RyanLaiber <ryanlaiber@gmail.com>
2021-09-21 18:53:05 -0300

    add req 13

---
+++ desafio13.sql
@@ -0,0 +1,9 @@
+SELECT 
+    p.ProductName AS 'Produto', p.Price AS 'Preço'
+FROM
+    w3schools.products AS p
+        INNER JOIN
+    w3schools.order_details AS od ON od.ProductID = p.ProductID
+GROUP BY Produto
+HAVING MAX(od.Quantity) > 80
+ORDER BY Produto;
