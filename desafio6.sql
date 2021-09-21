c925d705e867341912092fe832a279fe229a6c4a
Stefano <stefanooliveira1@gmail.com>
2021-09-21 18:53:15 -0300

    Req 6 fix - changes HIRE_DATE to START_DATE

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT 
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
     j.JOB_TITLE AS 'Cargo',
-    e.HIRE_DATE AS `Data de início do cargo`,
+    jh.START_DATE AS `Data de início do cargo`,
     d.DEPARTMENT_NAME AS 'Departamento'
 FROM
     hr.jobs j
@@ -9,4 +9,6 @@ FROM
     hr.employees e ON j.JOB_ID = e.JOB_ID
         INNER JOIN
     hr.departments d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
+		INNER JOIN
+	hr.job_history jh ON j.JOB_ID = jh.JOB_ID
 ORDER BY `Nome completo` DESC , j.JOB_TITLE;
