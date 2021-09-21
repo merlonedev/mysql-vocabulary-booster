38f1dfad76141740143a31e7a4873b2280a0a26e
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-09-21 16:29:08 -0300

    Requisito 6 e 7 - Ajustes CodeClimate

--- desafio6.sql
+++ desafio6.sql
@@ -4,11 +4,11 @@ SELECT
     jh.START_DATE `Data de início do cargo`,
     d.DEPARTMENT_NAME AS `Departamento`
 FROM
-	hr.job_history jh
-		INNER JOIN
+    hr.job_history jh
+        INNER JOIN
     hr.employees e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
         INNER JOIN
     hr.jobs j ON j.JOB_ID = jh.JOB_ID
-		INNER JOIN
-	hr.departments d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
-ORDER BY `Nome completo` DESC, JOB_TITLE ASC;
+        INNER JOIN
+    hr.departments d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC , JOB_TITLE ASC;
--- desafio7.sql
+++ desafio7.sql
@@ -3,8 +3,9 @@ SELECT
     jh.START_DATE `Data de início`,
     e.SALARY AS `Salário`
 FROM
-	hr.job_history jh
-		INNER JOIN
+    hr.job_history jh
+        INNER JOIN
     hr.employees e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
-WHERE MONTH(jh.START_DATE) IN (1, 2, 3)
-ORDER BY `Nome completo` ASC, `Data de início` ASC;
+WHERE
+    MONTH(jh.START_DATE) IN (1 , 2, 3)
+ORDER BY `Nome completo` ASC , `Data de início` ASC;
