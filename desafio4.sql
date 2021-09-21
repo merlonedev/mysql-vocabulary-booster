e3cced133ed2ca77daa79d616628b8468b1c4d8f
André Moreno <deco616@hotmail.com>
2021-09-21 18:04:04 -0300

    Desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,15 @@
+SELECT 
+    j.JOB_TITLE, 
+    ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
+    CASE
+		WHEN ROUND(AVG(e.SALARY), 2)< 5801 THEN 'Júnior'
+        WHEN ROUND(AVG(e.SALARY), 2) < 7501 THEN 'Pleno'
+        WHEN ROUND(AVG(e.SALARY), 2) < 10500 THEN 'Sênior'
+        ELSE 'CEO'
+    END AS 'Senioridade'
+FROM
+    hr.jobs AS j
+        INNER JOIN
+    hr.employees AS e ON j.JOB_ID = e.JOB_ID
+GROUP BY j.JOB_TITLE
+ORDER BY `Média salarial`, JOB_TITLE;
