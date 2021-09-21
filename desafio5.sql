abcf80d2ea5d663d39c0acbcc342a567ca61d405
Renan Ferreira <renansantiago2013@hotmail.com>
2021-09-21 17:38:58 -0300

    Resolve requisitos 5 e 6

---
+++ desafio5.sql
@@ -0,0 +1,11 @@
+SELECT 
+    j.JOB_TITLE AS 'Cargo',
+    (j.MAX_SALARY - j.MIN_SALARY) AS 'Variação Salarial',
+    ROUND(j.MIN_SALARY / 12, 2) AS 'Média mínima mensal',
+    ROUND(j.MAX_SALARY / 12, 2) AS 'Média máxima mensal'
+FROM
+    hr.jobs AS j
+        INNER JOIN
+    hr.employees AS e ON e.JOB_ID = j.JOB_ID
+GROUP BY e.JOB_ID
+ORDER BY (j.MAX_SALARY - j.MIN_SALARY), JOB_TITLE; 
