32a4ce2f63a1562f5d55f9094fd88ef0433ddab4
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-09-21 18:59:33 -0300

    Desafios 11 ao 15 corrigidos II

--- desafio15.sql
+++ desafio15.sql
@@ -1,17 +1,3 @@
-DELIMITER $$
-
-CREATE PROCEDURE buscar_media_por_cargo(
-	IN cargo VARCHAR(300)
-)
-BEGIN
-SELECT ROUND(AVG(SALARY), 2)
-    FROM hr.employees AS emp
-    INNER JOIN hr.jobs AS jobs
-    ON emp.JOB_ID = jobs.JOB_ID
-    WHERE jobs.JOB_TITLE = cargo;
-END $$
-
-DELIMITER ;
-
-CALL buscar_media_por_cargo("Programmer");
+DELIMITER $$ CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(300)) BEGIN SELECT ROUND(AVG(SALARY), 2) FROM hr.employees AS emp INNER JOIN hr.jobs AS jobs ON emp.JOB_ID = jobs.JOB_ID WHERE jobs.JOB_TITLE = cargo;
+END $$ DELIMITER ;
 
