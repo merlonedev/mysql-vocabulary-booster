0a2bacc16b4eebfd9c4e1779828b85d7a4c197da
camila-mp <ca.malvp@gmail.com>
2021-09-21 15:40:44 -0300

    desafio1 concluido

---
+++ desafio1.sql
@@ -0,0 +1,8 @@
+-- Exiba os países indicando se cada um deles se encontra ou não na região formada pela Europa (banco hr)
+-- Seu relatório deve apresentar 02 colunas:
+--   *A primeira deve possuir o alias "País" e exibir o nome do país.
+--   *A segunda deve possuir o alias "Status Inclusão" e deve exibir "incluído" se o país em questão está incluso em "Europe", ou "não incluído", caso contrário.
+-- Os resultados devem estar ordenados pelo nome do país em ordem alfabética.
+SELECT countries.COUNTRY_NAME, IF(REGION_ID=1, 'incluído', 'não incluído') AS `Status Inclusão`
+FROM hr.countries
+ORDER BY countries.COUNTRY_NAME;
---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+-- Exiba os cargos com seu nível de renumeração associado, com base no salário máximo do cargo (banco hr)
+-- Seu relatório deve possuir as seguintes colunas:
+--   *A primeira deve possuir o alias "Cargo" e exibir o nome do cargo.
+--   *A segunda deve possuir o alias "Nível" e exibir o nível de remuneração do cargo com base no seu salário máximo, categorizando os níveis de remuneração da seguinte forma:
+-- ->Salário máximo entre 5000 e 10000 arrow_right Baixo
+-- ->Salário máximo entre 10001 e 20000 arrow_right Médio
+-- ->Salário máximo entre 20001 e 30000 arrow_right Alto
+-- ->Salário máximo acima de 30000 arrow_right Altíssimo
+-- Os resultados devem estar ordenados pelo nome do cargo em ordem alfabética.
