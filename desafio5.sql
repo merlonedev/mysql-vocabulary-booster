a43c57f873800d9989f3eae0b50ab0794325bd9e
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-09-21 15:40:33 -0300

    qualqur coisa

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,3 @@
-SELECT 
-    jobs.JOB_TITLE AS 'Cargo',
-    jobs.MAX_SALARY - jobs.MIN_SALARY AS 'Variação Salarial',
-    ROUND(jobs.MIN_SALARY / 12, 2) AS 'Média mínima mensal',
+SELECT jobs.JOB_TITLE AS 'Cargo', jobs.MAX_SALARY - jobs.MIN_SALARY AS 'Variação Salarial', ROUND(jobs.MIN_SALARY / 12, 2) AS 'Média mínima mensal',
     ROUND(jobs.MAX_SALARY / 12, 2) AS 'Média máxima mensal'
-FROM
-    jobs AS jobs
-ORDER BY jobs.MAX_SALARY - jobs.MIN_SALARY , jobs.JOB_TITLE;
+FROM jobs AS jobs ORDER BY jobs.MAX_SALARY - jobs.MIN_SALARY , jobs.JOB_TITLE;
