404ce66e2b95480d28e5ba8950af0e0ba2637bf6
Rodrigo-R-Freitas <rodrigoaz8@gmail.com>
2021-09-21 14:34:33 -0300

    Desafios 1 a 5(hr)

---
+++ desafio1.sql
@@ -0,0 +1,10 @@
+SELECT 
+    COUNTRY_NAME AS 'País',
+    CASE
+        WHEN REGION_ID = 1 THEN 'incluído'
+        ELSE 'não incluído'
+    END AS 'Status Inclusão'
+FROM
+    countries
+ORDER BY COUNTRY_NAME;
+	
