548c17c9a0d5c0399ce0df7fae611fbcce483c7f
Victor Emmanuel <victorpassos2012@gmail.com>
2021-09-21 19:12:29 -0300

    Desafio 8 feito

---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+SELECT 
+    c.ContactName AS 'Nome de contato',
+    s.ShipperName AS 'Empresa que fez o envio',
+    o.OrderDate AS 'Data do pedido'
+FROM
+    w3schools.customers AS c
+        JOIN
+    orders AS o ON o.CustomerID = c.CustomerID
+        JOIN
+    shippers AS s ON o.ShipperID = s.ShipperID
+WHERE
+    s.ShipperID = 1 OR s.ShipperID = 2;
