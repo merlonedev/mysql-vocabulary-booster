6463d6420f8dc0ef509647cd29dc8a990111d9c7
Patrick Dack <patrickdack89@gmail.com>
2021-09-21 19:42:15 -0300

    Corrige Requisito 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,9 +1,9 @@
 SELECT
-	CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
+	  CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
     COUNT(o.EmployeeID) AS 'Total de pedidos'
 FROM
-	orders AS o
-		INNER JOIN
-	employees AS e ON o.EmployeeID = e.EmployeeID
+	  orders AS o
+		  INNER JOIN
+	  employees AS e ON o.EmployeeID = e.EmployeeID
 GROUP BY `Nome completo`
 ORDER BY `Total de pedidos`;
