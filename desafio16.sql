d85fc7e3341b5fb7878286a9090614aef89a9174
Milton Castro <castro.milton07@gmail.com>
2021-09-21 19:10:07 -0300

    ADD-ADJUST-CHALLENGE-16-OF-TABS_CodeClimate

--- desafio16.sql
+++ desafio16.sql
@@ -4,13 +4,13 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE jobs_quantity INT;
-    SELECT COUNT(jh.EMPLOYEE_ID)
-	FROM hr.job_history AS jh
-	INNER JOIN hr.employees AS e
-	ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID AND e.EMAIL LIKE email
-	GROUP BY e.EMPLOYEE_ID INTO jobs_quantity;
-    RETURN jobs_quantity;
+DECLARE jobs_quantity INT;
+SELECT COUNT(jh.EMPLOYEE_ID)
+FROM hr.job_history AS jh
+INNER JOIN hr.employees AS e
+ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID AND e.EMAIL LIKE email
+GROUP BY e.EMPLOYEE_ID INTO jobs_quantity;
+RETURN jobs_quantity;
 END $$
 
 DELIMITER ;
