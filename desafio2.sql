f30ecf036ea2a7747f9fffcf31c39e0d6a8cfe07
camila-mp <ca.malvp@gmail.com>
2021-09-21 15:58:23 -0300

    Conclui desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -7,3 +7,13 @@
 -- ->Salário máximo entre 20001 e 30000 arrow_right Alto
 -- ->Salário máximo acima de 30000 arrow_right Altíssimo
 -- Os resultados devem estar ordenados pelo nome do cargo em ordem alfabética.
+SELECT
+    JOB_TITLE AS `Cargo`,
+    CASE
+        WHEN MAX_SALARY BETWEEN 5000 AND 10000  THEN 'Baixo'
+        WHEN MAX_SALARY BETWEEN 10001 AND 20000  THEN 'Médio'
+        WHEN MAX_SALARY BETWEEN 20001 AND 30000 THEN 'Alto'
+        WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
+    END AS `Nível`
+FROM hr.jobs
+ORDER BY JOB_TITLE;
