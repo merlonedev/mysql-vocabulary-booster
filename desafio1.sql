c8636f0088a2505f26bd0f9914222a1db56f3459
thales <thalesdaniel2013@gmail.com>
2021-09-21 18:50:56 -0300

    feito etapa 4 e corrigido a etapa 1 e 2 novamente

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,4 @@
 SELECT COUNTRY_NAME AS 'País', 
-IF(REGION_ID = 1, 'incluido', 'não incluido') AS 'Europe' 
-FROM hr.countries;
+IF(REGION_ID = 1, 'incluido', 'não incluido') AS 'Status Inclusão' 
+FROM hr.countries
+ORDER BY COUNTRY_NAME;
