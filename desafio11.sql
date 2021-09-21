c5a6a8eb9b473517a935c28fd42ac6a6e1eb7199
julio11lp <julio11lp@hotmail.com>
2021-09-21 18:39:06 -0300

    correcao lint ex 11

--- desafio11.sql
+++ desafio11.sql
@@ -4,7 +4,7 @@ SELECT
     COUNT(cust2.Country) - 1 AS `Número de compatriotas`
 FROM
     w3schools.customers AS cust1
-	INNER JOIN w3schools.customers AS cust2 ON cust2.Country = cust1.Country
+INNER JOIN w3schools.customers AS cust2 ON cust2.Country = cust1.Country
 GROUP BY `Nome`
 HAVING COUNT(cust2.Country) - 1 >  0
 ORDER BY `Nome`;
