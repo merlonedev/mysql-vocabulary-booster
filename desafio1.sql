53b90b1cd133c4da85ff97ae2464fa0e487cd854
Kevin Oliveira <kevin.zero@hotmail.com>
2021-09-21 15:28:55 -0300

    1 - Finaliza desafios 1 a 4

---
+++ desafio1.sql
@@ -0,0 +1,9 @@
+SELECT 
+    COUNTRY_NAME AS 'País',
+    IF(REGION_NAME = 'Europe',
+        'incluído',
+        'não incluído') AS 'Status Inclusão'
+FROM
+    hr.countries
+        JOIN
+    hr.regions ON hr.countries.REGION_ID = hr.regions.REGION_ID;
