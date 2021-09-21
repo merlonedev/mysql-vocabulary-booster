9aa0a4d157db212cc80dc1df72b98a59b0acfaa2
johnpligeiro <johnpligeiro@gmail.com>
2021-09-21 19:56:28 -0300

    Desafio 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,9 +1,9 @@
 USE hr;
-SELECT 
-    CONCAT(e.first_name, " ", e.last_name) AS `Nome completo`,
-    DATE_FORMAT(j.start_date, '%d/%m/%Y') AS `Data de início`,
-    DATE_FORMAT(j.end_date, '%d/%m/%Y') AS `Data de rescisão`,
-    (DATEDIFF(j.end_date, j.start_date) / 365) AS `Anos trabalhados`
+SELECT
+  CONCAT(e.first_name, ' ', e.last_name) AS `Nome completo`,
+  DATE_FORMAT(j.start_date, '%d/%m/%Y') AS `Data de início`,
+  DATE_FORMAT(j.end_date, '%d/%m/%Y') AS `Data de rescisão`,
+  DATEDIFF(j.end_date, j.start_date) / 365 AS `Anos trabalhados`
 FROM
     employees AS e
         INNER JOIN
