69f64df4cf577b6bac295b79bea15a1647bf582e
Milton Castro <castro.milton07@gmail.com>
2021-09-21 02:27:36 -0300

    Desafios Iniciais -  1, 2, 3, 4, 5, 6, 7, 8, 9 e 10

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT
+	UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+	jh.START_DATE AS 'Data de início',
+    e.SALARY AS 'Salário'
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS jh
+ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) BETWEEN 1 AND 3
+ORDER BY UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)), e.HIRE_DATE;
