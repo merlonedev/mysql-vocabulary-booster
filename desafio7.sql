7bacc4e4c9544f5db265e6d15ae4789574c1a9d4
Victor Emmanuel <victorpassos2012@gmail.com>
2021-09-21 18:56:01 -0300

    Desafio 7 feito

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+SELECT 
+    UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+    jh.START_DATE AS 'Data de início',
+    e.SALARY AS 'Salário'
+FROM
+    hr.employees AS e
+        JOIN
+    job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) BETWEEN 1 AND 3
+ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME), jh.START_DATE
