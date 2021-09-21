e41c650417ead65defc7260a3cd62d7bcabd0fdd
José Carlos <kalarruda@hotmail.com>
2021-09-21 19:51:37 -0300

    desafio 8 adicionado order by data

--- desafio8.sql
+++ desafio8.sql
@@ -1,14 +1,13 @@
 SELECT 
     C.ContactName AS 'Nome de contato',
     S.ShipperName AS 'Empresa que fez o envio',
-    O.OrderDate AS `Data do pedido`
+    O.OrderDate AS 'Data do pedido'
 FROM
     w3schools.customers AS C
         INNER JOIN
-    w3schools.shippers AS S
+    w3schools.orders AS O ON O.CustomerID = C.CustomerID
         INNER JOIN
-    w3schools.orders AS O ON S.ShipperID = O.ShipperID
-        AND O.CustomerID = C.CustomerID
+    w3schools.shippers AS S ON S.ShipperID = O.ShipperID
 WHERE
     S.ShipperName IN ('Speedy Express' , 'United Package')
-ORDER BY C.ContactName , S.ShipperName;
+ORDER BY C.ContactName , S.ShipperName , O.OrderDate;
