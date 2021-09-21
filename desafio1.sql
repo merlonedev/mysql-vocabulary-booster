08bcf845a02cb1c4fc4dc99e146a5e0579355675
Euller Braz <euller.braz99@gmail.com>
2021-09-21 17:27:32 -0300

    Corrigido codeclimate

--- desafio1.sql
+++ desafio1.sql
@@ -1,10 +1,10 @@
 SELECT c.COUNTRY_NAME AS `País`,
-    IF ((SELECT
+    (IF ((SELECT
 		REGION_NAME
 	FROM
 		hr.regions AS r
 	WHERE
-		c.REGION_ID = r.REGION_ID) = 'Europe', 'incluído', 'não incluído') AS `Status Inclusão`
+		c.REGION_ID = r.REGION_ID) = 'Europe', 'incluído', 'não incluído')) AS `Status Inclusão`
 FROM
 	hr.countries AS c
 ORDER BY `País`;
