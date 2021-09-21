5f6f5f5bd193f5e09c6c90e2abb0e68452fd0b88
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-09-21 20:48:22 -0300

    done 15-17

---
+++ desafio16.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(emp_email VARCHAR(100))
+	RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE job_quantity INT;
+SELECT 
+    COUNT(*)
+FROM
+    hr.employees e
+        INNER JOIN
+    hr.job_history jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE
+    e.EMAIL = emp_email INTO job_quantity;
+    RETURN job_quantity;
+END $$
+
+DELIMITER ;
