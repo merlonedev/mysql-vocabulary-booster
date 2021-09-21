084cd7a27067cc9ec9ad8245dac31b54eadf0248
thiagopcdev <thiagopc_dev@hotmail.com>
2021-09-21 15:14:40 -0300

    fix 5

--- desafio2.sql
+++ desafio2.sql
@@ -1,11 +1,10 @@
-SELECT 
-  job_title AS Cargo,
-  CASE
-	WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
+SELECT job_title AS Cargo,
+CASE
+  WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
   WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
   WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
   ELSE 'Altíssimo'
-  END AS Nivel
+END AS Nivel
 FROM
   hr.jobs
 ORDER BY Cargo;
