a48f182cd3156fda51126203801bbb7e8e16ae34
Isaac Cruz <isaac.clm1@gmail.com>
2021-09-21 17:14:37 -0300

    Corrige espaçamentos

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT 
-	CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
-	(SELECT JOB_TITLE FROM hr.jobs WHERE hr.employees.JOB_ID = hr.jobs.JOB_ID) AS Cargo,
-  HIRE_DATE AS `Data de início do cargo`,
-  (SELECT DEPARTMENT_NAME FROM hr.departments WHERE hr.departments.DEPARTMENT_ID = hr.employees.DEPARTMENT_ID) AS Departamento
+CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
+(SELECT JOB_TITLE FROM hr.jobs WHERE hr.employees.JOB_ID = hr.jobs.JOB_ID) AS Cargo,
+HIRE_DATE AS `Data de início do cargo`,
+(SELECT DEPARTMENT_NAME FROM hr.departments WHERE hr.departments.DEPARTMENT_ID = hr.employees.DEPARTMENT_ID) AS Departamento
 FROM hr.employees
 ORDER BY `Nome completo` DESC, Cargo ASC;
--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
 SELECT 
-  (SELECT UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) FROM hr.employees WHERE hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID) AS `Nome completo`,
-  START_DATE AS `Data de início`,
-  (SELECT SALARY FROM hr.employees WHERE hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID) AS `Salário`
+(SELECT UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) FROM hr.employees WHERE hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID) AS `Nome completo`,
+START_DATE AS `Data de início`,
+(SELECT SALARY FROM hr.employees WHERE hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID) AS `Salário`
 FROM hr.job_history
 WHERE MONTH(START_DATE) < 4
 ORDER BY `Nome completo` ASC, START_DATE ASC;
