1aaf107972827e6abfb21b0872215937d9d5aa23
Josimar Souza <contact.josimarsouza@gmail.com>
2021-09-21 17:13:28 -0300

    Corrigi code climate

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
-	j.JOB_TITLE AS `Cargo`,
-  jh.START_DATE AS `Data de início do cargo`,
-  d.DEPARTMENT_NAME AS `Departamento`
+j.JOB_TITLE AS `Cargo`,
+jh.START_DATE AS `Data de início do cargo`,
+d.DEPARTMENT_NAME AS `Departamento`
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS jh
 ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
