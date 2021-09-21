d9c5f5c41ee4152e50b872547a5b3e3de3f9ec72
Ramon Paschoal <rpaschoal90@gmail.com>
2021-09-21 15:11:18 -0300

    Requisito 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,3 @@
-SELECT * FROM hr.countries;
-SELECT * FROM hr.regions;
-
 SELECT c.COUNTRY_NAME AS País, IF (c.REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão' FROM hr.countries AS c
 INNER JOIN hr.regions AS r
 ON c.REGION_ID = r.REGION_ID
