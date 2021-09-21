b06ab7657ad757c7d4342fe104c2e725ba3b2590
AndreHammel <hammel.engineer@gmail.com>
2021-09-21 20:54:27 -0300

    Update desafio18.sql

--- desafio18.sql
+++ desafio18.sql
@@ -1,12 +1,27 @@
-select 
-	concat(emp.FIRST_NAME, ' ', emp.LAST_NAME) as `Nome completo`,
-  concat( if( day(jh.START_DATE) between 1 and 9, concat(0,day(jh.START_DATE)), day(jh.START_DATE)) , '/', if( month(jh.START_DATE) between 1 and 9, concat(0,month(jh.START_DATE)), month(jh.START_DATE)) , '/', year(jh.START_DATE)) as 'Data de início',
-  concat( if( day(jh.END_DATE) between 1 and 9, concat(0,day(jh.END_DATE)), day(jh.END_DATE)), '/', if( month(jh.END_DATE) between 1 and 9, concat(0,month(jh.END_DATE)), month(jh.END_DATE)), '/', year(jh.END_DATE)) as 'Data de rescisão',
-  ROUND((DATEDIFF( (jh.END_DATE) ,(jh.START_DATE) )) / 365, 2) as `Anos trabalhados`
-from
-	employees as emp
-join
-	job_history as jh
-on
-	emp.EMPLOYEE_ID = jh.EMPLOYEE_ID 
-order by `Nome completo`, `Anos trabalhados` ASC;
+SELECT 
+    CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS `Nome completo`,
+    CONCAT(IF(DAY(jh.START_DATE) BETWEEN 1 AND 9,
+                CONCAT(0, DAY(jh.START_DATE)),
+                DAY(jh.START_DATE)),
+            '/',
+            IF(MONTH(jh.START_DATE) BETWEEN 1 AND 9,
+                CONCAT(0, MONTH(jh.START_DATE)),
+                MONTH(jh.START_DATE)),
+            '/',
+            YEAR(jh.START_DATE)) AS 'Data de início',
+    CONCAT(IF(DAY(jh.END_DATE) BETWEEN 1 AND 9,
+                CONCAT(0, DAY(jh.END_DATE)),
+                DAY(jh.END_DATE)),
+            '/',
+            IF(MONTH(jh.END_DATE) BETWEEN 1 AND 9,
+                CONCAT(0, MONTH(jh.END_DATE)),
+                MONTH(jh.END_DATE)),
+            '/',
+            YEAR(jh.END_DATE)) AS 'Data de rescisão',
+    ROUND((DATEDIFF((jh.END_DATE), (jh.START_DATE))) / 365,
+            2) AS `Anos trabalhados`
+FROM
+    employees AS emp
+        JOIN
+    job_history AS jh ON emp.EMPLOYEE_ID = jh.EMPLOYEE_ID
+ORDER BY `Nome completo` , `Anos trabalhados` ASC;
