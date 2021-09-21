dac60b8be31d7c57e04b3f9c371bd2bd93b1214b
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-09-21 17:54:26 -0300

    Requisitos 15 e 17 Erros

--- desafio15.sql
+++ desafio15.sql
@@ -1,4 +1,4 @@
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(30)) 
-BEGIN 
+BEGIN
 SELECT ROUND(AVG(em.SALARY),2) AS `Média salarial` FROM employees AS em JOIN jobs as jb ON jb.JOB_ID = em.JOB_ID WHERE jb.JOB_TITLE = cargo
 END 
