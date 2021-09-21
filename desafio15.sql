18b7ee3a21cb801b71c56f597fdfd3d3be30d891
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-09-21 19:13:44 -0300

    Desafios 8, 12 e 15

--- desafio15.sql
+++ desafio15.sql
@@ -1,2 +1,12 @@
-DELIMITER $$ CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(300)) BEGIN SELECT ROUND(AVG(SALARY), 2) FROM hr.employees AS emp INNER JOIN hr.jobs AS jobs ON emp.JOB_ID = jobs.JOB_ID WHERE jobs.JOB_TITLE = cargo;
-END $$ DELIMITER ;
+DELIMITER $$ 
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(300))
+BEGIN 
+SELECT
+ROUND(AVG(SALARY), 2) AS 'Média salarial'
+FROM hr.employees AS emp
+INNER JOIN hr.jobs AS jobs ON emp.JOB_ID = jobs.JOB_ID
+WHERE jobs.JOB_TITLE = cargo;
+END $$
+
+DELIMITER ;
--- desafio8.sql
+++ desafio8.sql
@@ -1,4 +1,5 @@
-SELECT cust.CustomerName AS 'Nome de contato',
+SELECT
+cust.CustomerName AS 'Nome de contato',
 shipp.ShipperName AS 'Empresa que fez o envio',
 ord.OrderDate AS 'Data do pedido'
 FROM w3schools.customers AS cust
@@ -6,5 +7,6 @@ INNER JOIN w3schools.orders AS ord
 ON cust.CustomerID = ord.CustomerID
 INNER JOIN w3schools.shippers AS shipp
 ON ord.ShipperID = shipp.ShipperID
-WHERE shipp.ShipperName IN ('Speedy Express', 'United Package')
+WHERE shipp.ShipperName = 'Speedy Express'
+OR shipp.ShipperName = 'United Package'
 ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
