9b94ee00aa9303623c5980c5bef064001f93a8eb
julio11lp <julio11lp@hotmail.com>
2021-09-21 18:37:58 -0300

    Alteracoes desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,10 @@
 SELECT 
     cust1.ContactName AS `Nome`,
     cust1.Country AS `País`,
-    COUNT(cust1.Country) - 1 AS `Número de compatriotas`
+    COUNT(cust2.Country) - 1 AS `Número de compatriotas`
 FROM
-    w3schools.customers AS cust1,
-    w3schools.customers AS cust2
-WHERE cust1.Country = cust2.Country
+    w3schools.customers AS cust1
+	INNER JOIN w3schools.customers AS cust2 ON cust2.Country = cust1.Country
 GROUP BY `Nome`
-HAVING COUNT(cust1.Country) - 1 > 0
+HAVING COUNT(cust2.Country) - 1 >  0
 ORDER BY `Nome`;
