0a0f1634cb35c39d8a850a3e4900e692ee79e73f
Flavio_Cordeiro <flavio_ribeirorc@hotmail.com>
2021-09-21 17:28:43 -0300

    Glória a Deux

--- desafio6.sql
+++ desafio6.sql
@@ -1,14 +1,13 @@
-SELECT 
-    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-    jb.JOB_TITLE AS 'Cargo',
-    jh.START_DATE AS 'Data de início do cargo',
-    d.DEPARTMENT_NAME AS 'Departamento'
-FROM
-    employees AS e
-        JOIN
-    jobs AS jb ON jb.JOB_ID = e.JOB_ID
-        JOIN
-    job_history AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
-        JOIN
-    departments AS d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
-ORDER BY `Nome completo` DESC , `Data de início do cargo` DESC , `Cargo` ASC;
+SELECT
+CONCAT(employees.FIRST_NAME, " ", employees.LAST_NAME) AS 'Nome completo',
+jobs.JOB_TITLE AS Cargo,
+job_history.START_DATE AS 'Data de início do cargo',
+departments.DEPARTMENT_NAME AS 'Departamento'
+FROM employees AS employees
+JOIN job_history AS job_history
+ON job_history.EMPLOYEE_ID = employees.EMPLOYEE_ID
+JOIN jobs AS jobs
+ON jobs.JOB_ID = job_history.JOB_ID
+JOIN departments AS departments
+ON departments.DEPARTMENT_ID = job_history.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, Cargo ASC;
