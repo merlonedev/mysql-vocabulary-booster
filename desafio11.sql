6f59cc1ba9f4eb7ec470d095e7f3e3bbaa911d1c
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-09-21 10:34:34 -0300

    Requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,11 @@
+    SELECT 
+    t1.ContactName AS 'Nome',
+    t1.Country AS 'País',
+	Count(t1.Country) - 1 as 'Número de compatriotas'
+        FROM
+            w3schools.customers AS t1,
+            w3schools.customers AS t2
+	where t1.Country = t2.Country
+    group by t1.ContactName
+    Having Count(t1.Country) - 1 <> 0
+	Order by t1.ContactName ASC;
