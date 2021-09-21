26dc599310c7065dce134c3deeb4e2573b350f6e
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-09-21 17:34:25 -0300

    adiciona desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,4 +1,30 @@
 SELECT 
-    p.ProductName AS Produto
+    p.ProductName AS Produto,
+    (SELECT 
+            MIN(Quantity)
+        FROM
+            w3schools.order_details
+        WHERE
+            ProductID = p.ProductID) AS Mínima,
+    (SELECT 
+            MAX(Quantity)
+        FROM
+            w3schools.order_details
+        WHERE
+            ProductID = p.ProductID) AS Máxima,
+    (SELECT 
+            ROUND(AVG(Quantity), 2)
+        FROM
+            w3schools.order_details
+        WHERE
+            ProductID = p.ProductID) AS Média
 FROM
-    w3schools.products as p;
+    w3schools.products AS p
+WHERE
+    (SELECT 
+            ROUND(AVG(Quantity), 2)
+        FROM
+            w3schools.order_details
+        WHERE
+            ProductID = p.ProductID) > 20
+ORDER BY Média , Produto;
