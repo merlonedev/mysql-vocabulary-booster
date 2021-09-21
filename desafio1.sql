68f5dcbf1daec854ff36a15942ab6fb148af96dd
Ed Santos <edsantos2822@gmail.com>
2021-09-21 16:32:56 -0300

    Finalizando - Requisito 1

---
+++ desafio1.sql
@@ -0,0 +1,7 @@
+SELECT 
+	COUNTRY_NAME AS 'País',
+    IF (REGION_ID = (
+			SELECT REGION_ID FROM hr.regions
+            WHERE REGION_NAME = 'Europe'
+		), 'incluído', 'não inluído') AS 'Status Inclusão'
+FROM hr.countries ORDER BY COUNTRY_NAME;
