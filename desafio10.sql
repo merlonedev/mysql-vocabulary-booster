48aab90030604253f189fc42c6898c85454b7509
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-09-21 16:22:12 -0300

    add desafio 10, ajeitando desafio 9

---
+++ desafio10.sql
@@ -0,0 +1,5 @@
+SELECT (SELECT ProductName FROM w3schools.products WHERE ProductID = od.ProductID) AS Produto, MIN(Quantity) AS Mínima, MAX(Quantity) AS Máxima, ROUND(AVG(Quantity), 2) AS Média 
+FROM w3schools.order_details AS od
+GROUP BY Produto
+HAVING Média > 20
+ORDER BY Média;
--- desafio9.sql
+++ desafio9.sql
@@ -1,6 +1,3 @@
-SELECT * FROM w3schools.employees;
-SELECT * FROM w3schools.orders;
-
 SELECT 
 	(SELECT CONCAT(FirstName, ' ', LastName) FROM w3schools.employees WHERE EmployeeID = o.EmployeeID) AS `Nome completo`,
     COUNT(o.CustomerID) AS `Total de pedidos`
