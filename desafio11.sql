00594a08962e86f237603383fa5886e57fd3c09e
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-09-21 10:43:12 -0300

    Requisito 11 corrigido

--- desafio11.sql
+++ desafio11.sql
@@ -6,6 +6,6 @@
             w3schools.customers AS t1,
             w3schools.customers AS t2
 	where t1.Country = t2.Country
-    group by t1.ContactName
+    group by t1.ContactName, t1.Country
     Having Count(t1.Country) - 1 <> 0
 	Order by t1.ContactName ASC;
