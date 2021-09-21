ebe53b3e6aff14845ced0332593161ac309b58e8
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-09-21 16:30:25 -0300

    fix desafio 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,7 @@
 SELECT 
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-    DATE_FORMAT(jh.START_DATE, '%d/%m/%y') AS 'Data de início',
-    DATE_FORMAT(jh.END_DATE, '%d/%m/%y') AS 'Data de rescisão',
+    DATE_FORMAT(jh.START_DATE, '%d/%m/%Y') AS 'Data de início',
+    DATE_FORMAT(jh.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
     ROUND(TIMESTAMPDIFF(DAY,
                 jh.START_DATE,
                 jh.END_DATE) / 365,
