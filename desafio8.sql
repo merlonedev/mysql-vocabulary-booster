e8296a5ea393e1a3d83df6965e056c1987a6e976
Gabriel Takaki <gabitj@icloud.com>
2021-09-21 18:09:45 -0300

    error no lint fake

--- desafio8.sql
+++ desafio8.sql
@@ -6,7 +6,9 @@ FROM
     w3schools.customers AS c
         INNER JOIN
     w3schools.orders AS o ON c.CustomerID = o.CustomerID
-		INNER JOIN
+        INNER JOIN
     w3schools.shippers AS s ON o.ShipperID = s.ShipperID
-    WHERE s.ShipperName = 'Speedy Express' OR s.ShipperName = 'United Package'
+WHERE
+    s.ShipperName = 'Speedy Express'
+        OR s.ShipperName = 'United Package'
 ORDER BY `Nome de contato` ASC , `Empresa que fez o envio` ASC , `Data do pedido` ASC;
