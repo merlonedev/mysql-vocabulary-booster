888282c250bc5c779e145ce744537d39c297ab4f
Martin <escorpmartin97@gmail.com>
2021-09-21 15:31:56 -0300

    [desafio1 - code climate]

--- desafio1.sql
+++ desafio1.sql
@@ -1,13 +1,14 @@
 SELECT *  FROM hr.countries;
 SELECT * FROM hr.regions;
 
-SELECT
-	c.country_name AS 'País',
-CASE
-	WHEN r.region_name = 'Europe' THEN 'incluído'
-    ELSE 'não incluído'
+SELECT 
+    c.country_name AS 'País',
+    CASE
+        WHEN r.region_name = 'Europe' THEN 'incluído'
+        ELSE 'não incluído'
     END 'Status Inclusão'
-FROM hr.countries AS c
-INNER JOIN hr.regions AS r
+FROM
+    hr.countries AS c
+        INNER JOIN
+    hr.regions AS r
 ORDER BY c.country_name;
-    
