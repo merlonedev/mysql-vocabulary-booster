2bab4c95dac81371ae586b146b7aa1177f6863fc
gabrielfndz <gabriel.omercador@hotmail.com>
2021-09-21 17:00:19 -0300

    Desafio 6 Refatoração

--- desafio6.sql
+++ desafio6.sql
@@ -1,12 +1,14 @@
 SELECT 
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
     j.JOB_TITLE AS 'Cargo',
-    e.HIRE_DATE AS 'Data de início do cargo',
+    jh.START_DATE AS 'Data de início do cargo',
     d.DEPARTMENT_NAME AS 'Departamento'
 FROM
     employees AS e
+    INNER JOIN
+    job_history as jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
         INNER JOIN
-    jobs AS j ON j.JOB_ID = e.JOB_ID
+    jobs AS j ON j.JOB_ID = jh.JOB_ID
         INNER JOIN
-    departments AS d ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
+    departments AS d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
 ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC , Cargo;
