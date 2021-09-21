cd6a02e6f9c927e851b7a93b95b609e73920bbfa
Gabriel Takaki <gabitj@icloud.com>
2021-09-21 15:36:37 -0300

    Faz 4?

---
+++ desafio4.sql
@@ -0,0 +1,15 @@
+SELECT 
+    h.JOB_TITLE AS Cargo,
+    REPLACE(AVG(e.SALARY), .000000, .00) AS `Média salarial`,
+    CASE
+        WHEN AVG(e.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN AVG(e.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN AVG(e.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        WHEN AVG(e.SALARY) > 10500 THEN 'CEO'
+    END AS Senioridade
+FROM
+    hr.employees AS e
+        INNER JOIN
+    hr.jobs AS h ON e.JOB_ID = h.JOB_ID
+GROUP BY e.JOB_ID
+ORDER BY e.SALARY ASC, h.JOB_TITLE ASC;
