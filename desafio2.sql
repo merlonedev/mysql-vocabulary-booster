397cf80b77c4823a697354e3dc20bbf70a0e8242
Guilherme Dias <gmd.delta@gmail.com>
2021-09-21 00:59:32 -0300

    [PT-BR]Resolvido requisito 2. [EN] Solved 2nd requirement

---
+++ desafio2.sql
@@ -0,0 +1,12 @@
+SELECT 
+    JOB_TITLE AS 'Cargo',
+    CASE
+        WHEN MAX_SALARY BETWEEN 5000 AND 10000 THEN 'Baixo'
+        WHEN MAX_SALARY BETWEEN 10000 AND 20000 THEN 'Médio'
+        WHEN MAX_SALARY BETWEEN 20000 AND 30000 THEN 'Alto'
+        WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
+        ELSE 'Salário fora dos parâmetros'
+    END AS 'Nível'
+FROM
+    hr.jobs
+ORDER BY JOB_TITLE ASC;
