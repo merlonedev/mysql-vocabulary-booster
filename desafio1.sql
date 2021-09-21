fb02d14ef1e297c4393c9dfd1e7439004a3925bc
Martin <escorpmartin97@gmail.com>
2021-09-21 15:25:30 -0300

    [desafio1]

---
+++ desafio1.sql
@@ -0,0 +1,13 @@
+SELECT *  FROM hr.countries;
+SELECT * FROM hr.regions;
+
+SELECT
+	c.country_name AS 'País',
+CASE
+	WHEN r.region_name = 'Europe' THEN 'incluído'
+    ELSE 'não incluído'
+    END 'Status Inclusão'
+FROM hr.countries AS c
+INNER JOIN hr.regions AS r
+ORDER BY c.country_name;
+    
