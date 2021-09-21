cb0e4216ecf43eba1e34c840ff214c4bb27e6acf
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-09-21 19:28:04 -0300

    Requisito 11 Correcao

--- desafio11.sql
+++ desafio11.sql
@@ -1,12 +1,19 @@
 SELECT 
     ContactName AS `Nome`,
-    Country AS `País`,
+    Country AS País,
     (SELECT 
             COUNT(CustomerID) - 1
         FROM
             customers
         WHERE
-            Country = customers_externo.Country) AS `Número de compatriotas`
+            Country = customers_externo.Country) AS Número de compatriotas
 FROM
     customers AS customers_externo
+WHERE
+    (SELECT 
+            COUNT(CustomerID) - 1
+        FROM
+            customers
+        WHERE
+            Country = customers_externo.Country) > 0
 ORDER BY ContactName ASC;
