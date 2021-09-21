3fdf5200ca27d129c01e901ede7849899682a325
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-09-21 11:29:38 -0300

    Requisito 13

---
+++ desafio13.sql
@@ -0,0 +1,7 @@
+	SELECT 
+    p.ProductName AS 'Produto', p.Price AS 'Preço'
+	FROM
+    w3schools.products as p
+    inner join w3schools.order_details as od on od.ProductID = p.ProductID
+    where od.Quantity > 80
+    order by p.ProductName;
