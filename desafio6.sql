500f4b9a68a691785499b9d4432ba8209a88c41a
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2021-09-21 17:25:19 -0300

    Resolve desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
-SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome Completo`,
+SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
 (SELECT j.JOB_TITLE FROM hr.jobs AS j
 WHERE j.JOB_ID = e.JOB_ID) AS Cargo,
 HIRE_DATE AS `Data de início do cargo`,
 (SELECT d.DEPARTMENT_NAME FROM hr.departments AS d
 WHERE d.DEPARTMENT_ID = e.DEPARTMENT_ID) AS Departamento
 FROM hr.employees AS e
-ORDER BY `Nome Completo` DESC, Cargo ASC;
+ORDER BY `Nome completo` DESC, Cargo ASC;
