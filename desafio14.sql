56e67fda7aa721510fc32afd22cb87c290da6c90
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-09-21 15:52:02 -0300

    14

--- desafio14.sql
+++ desafio14.sql
@@ -1,2 +1,1 @@
-(SELECT DISTINCT customers.Country AS `País`FROM customers UNION (SELECT DISTINCT Country AS `País` FROM suppliers ORDER BY `País` ASC LIMIT 5;
-
+( SELECT DISTINCT customers.Country AS `País`FROM customers) UNION ( SELECT DISTINCT Country AS `País` FROM suppliers) ORDER BY `País` ASC LIMIT 5;
