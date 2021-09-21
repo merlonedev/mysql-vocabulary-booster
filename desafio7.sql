2ca441d790405efe0dee616bffa4288b465a2239
Erick Silva <erick.silva6653@gmail.com>
2021-09-21 12:51:31 -0300

    desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+SELECT
+    ucase(CONCAT(employees.FIRST_NAME,
+            ' ',
+            employees.LAST_NAME)) AS `Nome completo`,
+    j_history.START_DATE AS `Data de início`,
+    employees.SALARY AS Salário
+FROM
+	hr.job_history AS j_history
+        JOIN
+	hr.employees AS employees ON employees.EMPLOYEE_ID = j_history.EMPLOYEE_ID
+WHERE
+	MONTH(j_history.START_DATE) BETWEEN 01 AND 03
+ORDER BY `Nome completo`, `Data de início`;
