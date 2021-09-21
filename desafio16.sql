c37fbdc24120c0f8f1063a8764a849ec1b4b6829
AndreHammel <hammel.engineer@gmail.com>
2021-09-21 20:51:18 -0300

    Update desafio16.sql

--- desafio16.sql
+++ desafio16.sql
@@ -4,14 +4,14 @@ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario (val varchar(30))
 RETURNS INT
 READS SQL DATA 
 BEGIN
-	RETURN (
-    	SELECT 
-		COUNT(jh.EMPLOYEE_ID) 
-		FROM
-		employees AS emp
-		JOIN
-		job_history AS jh ON emp.EMPLOYEE_ID = jh.EMPLOYEE_ID
-		WHERE emp.EMAIL = val
+RETURN (
+  SELECT 
+  COUNT(jh.EMPLOYEE_ID) 
+	FROM
+	  employees AS emp
+	JOIN
+	  job_history AS jh ON emp.EMPLOYEE_ID = jh.EMPLOYEE_ID
+	WHERE emp.EMAIL = val
     );
 END
 $$
