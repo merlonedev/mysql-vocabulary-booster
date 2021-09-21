5d7f5868a5b2eea1760fff0972e54d85791bf98e
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-09-21 14:50:41 -0300

    Fix codeclimate

--- desafio5.sql
+++ desafio5.sql
@@ -3,7 +3,7 @@ SELECT
     MAX_SALARY - MIN_SALARY AS 'Variação Salarial',
     ROUND(AVG(MIN_SALARY / 12), 2) AS 'Média mínima mensal',
     ROUND(AVG(Max_SALARY / 12), 2) AS 'Média máxima mensal'
-FROM
+    FROM
     jobs
     GROUP BY JOB_TITLE, MAX_SALARY - MIN_SALARY
     ORDER BY MAX_SALARY - MIN_SALARY, JOB_TITLE;
