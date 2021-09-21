047982662b28d81fecafe6b32c9cc0e5fc645041
Ed Santos <edsantos2822@gmail.com>
2021-09-21 16:36:17 -0300

    Corrigindo - Requisito 1- Erros de Style (Code Climate)

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,6 @@
-SELECT 
-	COUNTRY_NAME AS 'País',
+SELECT COUNTRY_NAME AS 'País',
     IF (REGION_ID = (
-			SELECT REGION_ID FROM hr.regions
+		SELECT REGION_ID FROM hr.regions
             WHERE REGION_NAME = 'Europe'
-		), 'incluído', 'não inluído') AS 'Status Inclusão'
+	), 'incluído', 'não inluído') AS 'Status Inclusão'
 FROM hr.countries ORDER BY COUNTRY_NAME;
