b8c99c69cf0742a0e5887c1cf36ba7ee4fec23c5
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-09-21 15:22:56 -0300

    Desafios 1 ao 5 corrigidos II

--- desafio4.sql
+++ desafio4.sql
@@ -1,2 +1,1 @@
 SELECT jobs.JOB_TITLE AS 'Cargo', ROUND(AVG(emp.SALARY), 2) AS 'Média salarial', CASE WHEN emp.SALARY BETWEEN 2000 AND 5800 THEN 'Júnior' WHEN emp.SALARY BETWEEN 5801 AND 7500 THEN 'Pleno' WHEN emp.SALARY BETWEEN 7501 AND 10500 THEN 'Sênior' ELSE 'CEO' END AS 'Senioridade' FROM hr.employees AS emp INNER JOIN hr.jobs AS jobs ON emp.JOB_ID = jobs.JOB_ID GROUP BY `Cargo` ORDER BY `Média salarial`, `Cargo`;
-
