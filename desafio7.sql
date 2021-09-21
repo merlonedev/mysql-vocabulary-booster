3153557818f698b29c4b66763fa7526da52be252
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-09-21 15:55:23 -0300

    7_

--- desafio7.sql
+++ desafio7.sql
@@ -1,3 +1,3 @@
-SELECT CONCAT(employees.FIRST_NAME,' ',employees.LAST_NAME) AS `Nome Completo`, job_history.START_DATE AS `Data de início do cargo`, employees.SALARY AS `Salário` FROM job_history AS job_history INNER JOIN employees AS employees ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID WHERE MONTH(job_history.START_DATE) BETWEEN 1 AND 3 ORDER BY CONCAT(employees.FIRST_NAME,' ', employees.LAST_NAME) ASC , job_history.START_DATE ASC;
+SELECT CONCAT(employees.FIRST_NAME,' ',employees.LAST_NAME) AS `Nome Completo`, job_history.START_DATE AS `Data de início do cargo`, employees.SALARY AS `Salário` FROM job_history AS job_history INNER JOIN employees AS employees ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID WHERE MONTH(job_history.START_DATE) BETWEEN 1 AND 3 ORDER BY CONCAT(employees.FIRST_NAME,' ', employees.LAST_NAME) ASC , job_history.START_DATE ASC
 
         
