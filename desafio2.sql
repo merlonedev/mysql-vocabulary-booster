2ed8c1db33db56b354d28503830f480c63f1ee2f
Wadson Almeida <wadson.dev92@gmail.com>
2021-09-21 18:28:21 -0300

    Resolve codeclimate desafio 2

--- desafio2.sql
+++ desafio2.sql
@@ -3,7 +3,7 @@ case
 	when max_salary between 5000 and 10000 then 'Baixo'
     when max_salary between 10001 and 20000 then 'Médio'
     when max_salary between 20001 and 30000 then 'Alto'
-	when max_salary > 30000 then 'Altíssimo'
+	else 'Altíssimo'
 end as Nível
 from hr.jobs
 order by job_title;
