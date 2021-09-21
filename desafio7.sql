183dc6a9dc00f7277673252964604086f02f41a0
Luciano Lanes <llaneslopes@gmail.com>
2021-09-21 16:31:39 -0300

    FIX Desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,7 @@
+SELECT 
+    UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS `Nome completo`,
+    HIRE_DATE AS `Data de início`,
+    SALARY AS `Salário`
+FROM
+    hr.employees
+ORDER BY `Nome completo`, `Data de início`;
