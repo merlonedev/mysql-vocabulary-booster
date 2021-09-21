7a47a947b367caa742870d43ac552c5b3b7cc6b3
Martin <escorpmartin97@gmail.com>
2021-09-21 16:32:00 -0300

    [desafio4]

--- desafio2.sql
+++ desafio2.sql
@@ -4,8 +4,7 @@ SELECT
         WHEN max_salary BETWEEN 5000 AND 1000 THEN 'Baixo'
         WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
         WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
-        WHEN max_salary > 30000 THEN 'Altíssimo'
-        ELSE '-'
+        ELSE Altíssimo
     END AS 'Nível'
 FROM
     hr.jobs
