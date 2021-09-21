1e432bbdac34e957e956f2a1cbc5daf1ae91c417
Eder Santos <3derms@gmail.com>
2021-09-21 16:06:03 -0300

    Exibe os cargos com seu nível de renumeração associado, com base no salário máximo do cargo

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,9 @@
 SELECT 
-	COUNTRY_NAME,
-	CASE
-		WHEN REGION_ID = 1 THEN 'Europe'
-		ELSE 'não incluído'
-	END AS 'Status Inclusão'
-From hr.countries
+    COUNTRY_NAME,
+    CASE
+        WHEN REGION_ID = 1 THEN 'Europe'
+        ELSE 'não incluído'
+    END AS 'Status Inclusão'
+FROM
+    hr.countries
 ORDER BY COUNTRY_NAME;
