0e296b4977423a4739e6252320be823dc01ccc11
Eder Santos <3derms@gmail.com>
2021-09-21 20:18:37 -0300

    Corrige Climate issue

--- desafio6.sql
+++ desafio6.sql
@@ -8,7 +8,7 @@ FROM
         INNER JOIN
     hr.job_history AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
         INNER JOIN
-	hr.jobs AS j ON jh.JOB_ID = j.JOB_ID
+    hr.jobs AS j ON jh.JOB_ID = j.JOB_ID
         INNER JOIN
     hr.departments AS d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
 ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC , j.JOB_TITLE;
