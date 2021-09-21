370597d90e685954fc643cb3eb8512a721826ce5
Eder Santos <3derms@gmail.com>
2021-09-21 18:23:14 -0300

    Utilizando INNER JOIN para região igual a Europa

--- desafio1.sql
+++ desafio1.sql
@@ -1,9 +1,11 @@
 SELECT 
-    COUNTRY_NAME AS 'País',
+    c.COUNTRY_NAME AS 'País',
     CASE
-        WHEN REGION_ID = 1 THEN 'incluído'
+        WHEN r.REGION_NAME = 'Europe' THEN 'incluído'
         ELSE 'não incluído'
     END AS 'Status Inclusão'
 FROM
-    hr.countries
+    hr.countries AS c
+INNER JOIN hr.regions AS r
+ON c.REGION_ID = r.REGION_ID
 ORDER BY COUNTRY_NAME;
