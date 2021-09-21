e3e17da9ff40f54d04b1b70c9cf1dc95edd2c98f
Tulio <tuliotrindade99@gmail.com>
2021-09-21 13:21:18 -0300

    desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,12 @@
+  
+SELECT 
+	prds.ProductName AS 'Produto',
+	MIN(od.Quantity) AS 'Mínima',
+	MAX(od.Quantity) AS 'Máxima',
+	ROUND(AVG(od.Quantity), 2) AS 'Média'
+FROM w3schools.products AS prds
+INNER JOIN w3schools.order_details AS od
+ON prds.ProductID = od.ProductID
+GROUP BY prds.ProductName
+HAVING `Média` > 20.00
+ORDER BY `Média` , `Produto`;
