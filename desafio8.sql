9435e0a1b3e980377f25afc6d7d084975b74ae41
Martin <escorpmartin97@gmail.com>
2021-09-21 18:53:26 -0300

    [desafio8]

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+SELECT 
+    c.ContactName AS 'Nome de contato',
+    s.ShipperName AS 'Empresa que fez o envio',
+    o.OrderDate AS 'Data do pedido'
+FROM
+    w3schools.orders AS o
+        INNER JOIN
+    w3schools.shippers AS s ON s.ShipperID = o.ShipperID
+        INNER JOIN
+    w3schools.customers AS c ON c.CustomerID = o.CustomerID
+WHERE
+    ShipperName = 'Speedy Express'
+        OR ShipperName = 'United Package'
+ORDER BY `Nome de contato` , `Empresa que fez o envio` , `Data do pedido`;
