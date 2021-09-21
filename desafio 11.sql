d851be321d23fd3b2ed7a8f863c2096fedef6a63
Uriel <urielbochi@gmail.com>
2021-09-21 18:42:19 -0300

    nada mais faz sentido!

--- desafio 11.sql
+++ desafio 11.sql
@@ -1,13 +1,9 @@
-SELECT
-  coutr1.ContactName AS 'Nome',
-  coutr1.Country AS 'País',
-  COUNT(*) AS 'Número de compatriotas' 
-FROM
-  w3schools.customers AS coutr1,
-  w3schools.customers AS coutr2 
-WHERE
-  coutr1.Country = coutr2.Country AND coutr1.ContactName <> coutr2.ContactName
-GROUP BY
-  `Nome`, coutr1.Country 
-ORDER BY
-    `Nome`;
+SELECT 
+coutr1.ContactName AS `Nome`,
+coutr1.Country AS `País`,
+(SELECT 
+COUNT(*) - 1 FROM w3schools.customers coutr2
+WHERE coutr2.Country = coutr1.Country) AS `Número de compatriotas`
+FROM w3schools.customers AS coutr1
+HAVING `Número de compatriotas` <> 0
+ORDER BY `Nome`;
