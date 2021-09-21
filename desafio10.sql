97de4482bd536b6ec4c674e2f9024353c1d2c763
alestori7@gmail.com <alestori7@gmail.com>
2021-09-21 17:51:45 -0300

    Desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,4 +1,12 @@
 SELECT 
-    *
+    pr.ProductName AS Produto,
+    MIN(od.Quantity) AS Mínima,
+    MAX(od.Quantity) AS Máxima,
+    ROUND(AVG(od.Quantity), 2) AS Média
 FROM
-    w3schools.employees;
+    w3schools.products pr
+        JOIN
+    w3schools.order_details od ON od.ProductID = pr.ProductID
+GROUP BY pr.ProductName
+HAVING ROUND(AVG(od.Quantity)) > 20
+ORDER BY Média , Produto;
