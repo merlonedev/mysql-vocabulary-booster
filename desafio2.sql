a8918b1332a80292347326c47be2676d7bda0703
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-09-21 15:35:21 -0300

    Resolvendo desafios 1 e 2

---
+++ desafio2.sql
@@ -0,0 +1,11 @@
+SELECT
+    job_title AS 'Cargo',
+    (CASE
+		WHEN MAX_SALARY > 4999 AND MAX_SALARY < 10001 THEN 'Baixo'
+        WHEN MAX_SALARY > 10001 AND MAX_SALARY < 20001 THEN 'Médio'
+        WHEN MAX_SALARY > 20001 AND MAX_SALARY < 30001 THEN 'Alto'
+        WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
+    END) AS 'Nível'
+FROM
+    hr.jobs
+ORDER BY job_title ASC;
