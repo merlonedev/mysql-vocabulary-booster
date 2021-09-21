b523c3ff9ca2deb6bef0defaff8960402eca7c7b
Uriel <urielbochi@gmail.com>
2021-09-21 16:31:13 -0300

    tentativa 11 2.0

--- desafio 11.sql
+++ desafio 11.sql
@@ -1,6 +1,8 @@
-SELECT coutr1.ContactName AS 'Nome', coutr1.Country AS 'País', COUNT(coutr1.Country) - 1 AS 'Número de compatriotas'
-from w3schools.customers AS coutr1, w3schools.customers AS coutr2
+SELECT coutr1.ContactName AS 'Nome',
+coutr1.Country AS 'País',
+COUNT(coutr1.Country) - 1 AS 'Número de compatriotas'
+FROM w3schools.customers AS coutr1, w3schools.customers AS coutr2
 WHERE coutr1.Country = coutr2.Country
 GROUP BY `Nome`
-HAVING `Número de compatriotas` > 0 
+HAVING `Número de compatriotas` <> 0
 ORDER BY `Nome`;
