9078f9d6aaabe2393c984da2a62ffa9847b5c9ac
julio11lp <julio11lp@hotmail.com>
2021-09-21 14:52:04 -0300

    Correcao desafio1

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,8 @@
 SELECT 
-    COUNTRY_NAME AS País,
+    COUNTRY_NAME AS `País`,
     IF(REGION_ID = 1,
         'incluído',
         'não incluído') AS `Status Inclusão`
 FROM
-    hr.countries;
+    hr.countries
+ORDER BY `País`;
