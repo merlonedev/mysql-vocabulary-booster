b9212260a99746e70d0343ccf4c64b247fadc82c
gabrielfndz <gabriel.omercador@hotmail.com>
2021-09-21 20:46:04 -0300

    Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+SELECT 
+    c1.ContactName AS 'Nome',
+    c1.Country AS 'País',
+    COUNT(c1.Country) - 1 AS 'Número de compatriotas'
+FROM
+    customers AS c1,
+    customers AS c2
+WHERE
+    c1.Country = c2.Country
+GROUP BY Nome , País
+HAVING `Número de compatriotas` <> 0
+ORDER BY `Nome`;
