57f6a8fcd6e9d5a0df192676a22be526f92f516a
Gabriel Takaki <gabitj@icloud.com>
2021-09-21 16:55:56 -0300

    corrige o job_history dos requsitos 6 e 7

--- desafio6.sql
+++ desafio6.sql
@@ -1,13 +1,15 @@
 SELECT 
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome Completo',
     h.JOB_TITLE AS 'Cargo',
-    e.HIRE_DATE AS 'Data de início do cargo',
+    j.START_DATE AS 'Data de início do cargo',
     d.DEPARTMENT_NAME AS 'Departamento'
 FROM
     hr.employees AS e
         INNER JOIN
     hr.jobs AS h
+		INNER JOIN
+	hr.job_history AS j
         INNER JOIN
     hr.departments AS d ON e.JOB_ID = h.JOB_ID
-        AND e.DEPARTMENT_ID = d.DEPARTMENT_ID
+        AND e.DEPARTMENT_ID = d.DEPARTMENT_ID AND e.EMPLOYEE_ID = j.EMPLOYEE_ID
 ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC , h.JOB_TITLE ASC;
--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,11 @@
 SELECT 
-    UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS 'Nome Completo',
-    HIRE_DATE AS 'Data de início',
-    SALARY AS 'Salário'
+    UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome Completo',
+    j.START_DATE AS 'Data de início',
+    e.SALARY AS 'Salário'
 FROM
-    hr.employees
+    hr.employees AS e
+    INNER JOIN
+    hr.job_history AS j ON e.EMPLOYEE_ID = j.EMPLOYEE_ID
 WHERE
-    HIRE_DATE BETWEEN '1987-01-01' AND '1987-3-30';
+   MONTH(j.START_DATE) <= 3
+ORDER BY 'Nome Completo', j.START_DATE;
