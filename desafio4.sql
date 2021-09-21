28ba52f658302cd3650f54333a18a06efd899c79
Isaac Cruz <isaac.clm1@gmail.com>
2021-09-21 16:15:34 -0300

    Add: Resquisito 4

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+SELECT 
+	JOB_TITLE AS Cargo, 
+	(SELECT ROUND(AVG(SALARY), 2) FROM hr.employees WHERE hr.employees.JOB_ID = hr.jobs.JOB_ID) AS `Média salarial`,
+  CASE 
+		WHEN (SELECT ROUND(AVG(SALARY), 2) FROM hr.employees WHERE hr.employees.JOB_ID = hr.jobs.JOB_ID) BETWEEN 2000 AND 5800 THEN 'Júnior'
+    WHEN (SELECT ROUND(AVG(SALARY), 2) FROM hr.employees WHERE hr.employees.JOB_ID = hr.jobs.JOB_ID) BETWEEN 5801 AND 7500 THEN 'Pleno'
+    WHEN (SELECT ROUND(AVG(SALARY), 2) FROM hr.employees WHERE hr.employees.JOB_ID = hr.jobs.JOB_ID) BETWEEN 7501 AND 10500 THEN 'Sênior'
+    WHEN (SELECT ROUND(AVG(SALARY), 2) FROM hr.employees WHERE hr.employees.JOB_ID = hr.jobs.JOB_ID) > 10500 THEN 'CEO'
+	END AS Senioridade
+FROM hr.jobs
+ORDER BY `Média salarial` ASC, Cargo ASC;
