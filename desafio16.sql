d4f3d4b15128282beee4d952838459bfbf692e4f
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-09-21 18:54:49 -0300

    Requisito 16

---
+++ desafio16.sql
@@ -0,0 +1,22 @@
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(employee_email VARCHAR(100))
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE quant INT;
+SELECT 
+    COUNT(jh.EMPLOYEE_ID)
+FROM
+    hr.job_history jh
+        INNER JOIN
+    (SELECT 
+        *
+    FROM
+        hr.employees
+    WHERE
+        EMAIL = employee_email) e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+GROUP BY jh.EMPLOYEE_ID INTO quant;
+    RETURN quant;
+END $$
+
+DELIMITER ;
