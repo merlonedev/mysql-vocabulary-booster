1a2723d8d3ec66132af30ab4fb57efcf060ef0dc
julio11lp <julio11lp@hotmail.com>
2021-09-21 15:45:55 -0300

    Desafio6

---
+++ desafio6.sql
@@ -0,0 +1,11 @@
+SELECT 
+    CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS `Nome completo`,
+    job.JOB_TITLE AS `Cargo`,
+    hist.START_DATE AS `Data de início do cargo`,
+    dep.DEPARTMENT_NAME AS `Departamento`
+FROM
+    hr.employees AS emp
+	INNER JOIN hr.jobs AS job ON job.JOB_ID = emp.JOB_ID
+    INNER JOIN hr.job_history AS hist ON hist.EMPLOYEE_ID = emp.EMPLOYEE_ID
+    INNER JOIN hr.departments AS dep ON dep.DEPARTMENT_ID = emp.DEPARTMENT_ID
+ORDER BY `Nome completo`, `Cargo`;
