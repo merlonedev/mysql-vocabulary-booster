8e2e67504c1ca739a1e5c2e1599d509a23929569
alestori7@gmail.com <alestori7@gmail.com>
2021-09-20 22:23:46 -0300

    Desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,15 @@
+SELECT 
+    cus.ContactName AS 'Nome de contato',
+    shi.ShipperName AS 'Empresa que fez o envio',
+    o.OrderDate AS 'Data do pedido'
+FROM
+    w3schools.customers cus
+        JOIN
+    w3schools.orders o ON o.CustomerID = cus.CustomerID
+        JOIN
+    w3schools.shippers shi ON shi.ShipperID = o.ShipperID
+WHERE
+    shi.ShipperName IN ('Speedy Express' , 'United Package')
+ORDER BY cus.ContactName , shi.ShipperName , o.OrderDate; 
+
+
