4d4d620aa1784b5a61aeef175c983496ef3ac7f6
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-09-21 14:39:06 -0300

    Requisito 18

---
+++ desafio18.sql
@@ -0,0 +1,26 @@
+SELECT 
+    CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
+    CONCAT(IF(LENGTH(DAY(START_DATE)) = 1,
+                CONCAT('0', DAY(START_DATE)),
+                DAY(START_DATE)),
+            '/',
+            IF(LENGTH(MONTH(START_DATE)) = 1,
+                CONCAT('0', MONTH(START_DATE)),
+                MONTH(START_DATE)),
+            '/',
+            YEAR(START_DATE)) AS `Data de início`,
+    CONCAT(IF(LENGTH(DAY(END_DATE)) = 1,
+                CONCAT('0', DAY(END_DATE)),
+                DAY(END_DATE)),
+            '/',
+            IF(LENGTH(MONTH(END_DATE)) = 1,
+                CONCAT('0', MONTH(END_DATE)),
+                MONTH(END_DATE)),
+            '/',
+            YEAR(END_DATE)) AS `Data de rescisão`,
+    ROUND(DATEDIFF(END_DATE, START_DATE) / 365, 2) AS `Anos trabalhados`
+FROM
+    job_history j
+        INNER JOIN
+    employees e ON e.EMPLOYEE_ID = j.EMPLOYEE_ID
+ORDER BY `Nome completo` ASC, `Anos trabalhados` ASC;
