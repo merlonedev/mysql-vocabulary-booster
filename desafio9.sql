5d96009d4afca827224cec95cac61cd4b6df2a53
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-09-21 17:12:52 -0300

    adiciona desafio 9 agora de verdade pq esqueci de salvar :p

---
+++ desafio9.sql
@@ -0,0 +1,14 @@
+SELECT 
+    CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
+    (SELECT 
+            COUNT(*)
+        FROM
+            w3schools.orders
+        WHERE
+            EmployeeID = e.EmployeeID) AS 'Total de pedidos'
+FROM
+    w3schools.employees AS e
+        INNER JOIN
+    w3schools.orders AS o ON e.EmployeeID = o.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`;
