155673a8e40345a66ca2417a9022695c88bdf4c6
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-09-21 15:11:50 -0300

    arrumando clode climate

--- desafio4.sql
+++ desafio4.sql
@@ -1,14 +1,13 @@
-SELECT * FROM hr.employees;
-
 SELECT 
-	JOB_ID AS Cargo, 
+    JOB_ID AS Cargo,
     ROUND(AVG(SALARY), 2) AS `Média salarial`,
     CASE
-		WHEN ROUND(AVG(SALARY), 2)  > 10500 THEN 'CEO'
-        WHEN ROUND(AVG(SALARY), 2)  > 7500 THEN 'Sênior'
-        WHEN ROUND(AVG(SALARY), 2)  > 5800 THEN 'Pleno'
-        WHEN ROUND(AVG(SALARY), 2)  > 2000 THEN 'Júnior'
+        WHEN ROUND(AVG(SALARY), 2) > 10500 THEN 'CEO'
+        WHEN ROUND(AVG(SALARY), 2) > 7500 THEN 'Sênior'
+        WHEN ROUND(AVG(SALARY), 2) > 5800 THEN 'Pleno'
+        WHEN ROUND(AVG(SALARY), 2) > 2000 THEN 'Júnior'
     END AS Senioridade
-FROM hr.employees
+FROM
+    hr.employees
 GROUP BY Cargo
-ORDER BY `Média salarial`, Cargo;
+ORDER BY `Média salarial` , Cargo;
