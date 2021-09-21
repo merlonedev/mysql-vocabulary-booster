52e89a6e5d86710117f952c4a23ced8c6b6a4164
Jossany Moura <mjossany@gmail.com>
2021-09-21 16:09:53 -0300

    Finaliza requisito 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,10 @@
-select * from Jesus;
+SELECT 
+    c.country_name AS 'País',
+    IF(c.region_id = 1,
+        'incluído',
+        'não incluído') AS 'Status Inclusão'
+FROM
+    hr.countries AS c
+        INNER JOIN
+    hr.regions AS r ON c.region_id = r.region_id
+order by c.country_name;
