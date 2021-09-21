7625a2a06c628d1198f49c812dd3a15e2b3d7222
Luciano Lanes <llaneslopes@gmail.com>
2021-09-21 19:40:46 -0300

    FIX Code Climate

--- desafio10.sql
+++ desafio10.sql
@@ -2,7 +2,7 @@ SELECT
     p.ProductName AS Produto,
     MIN(Quantity) AS `Mínima`,
     MAX(Quantity) AS `Máxima`,
-	ROUND(AVG(Quantity), 2) AS `Média`
+    ROUND(AVG(Quantity), 2) AS `Média`
 FROM
     w3schools.order_details AS od
         INNER JOIN
