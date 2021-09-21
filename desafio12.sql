7ddb852ab79ed7570e8af5226ed1a52a6f0ee632
Uriel <urielbochi@gmail.com>
2021-09-21 17:18:54 -0300

    tentativa req 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,7 +1,10 @@
 SELECT CONCAT(EMP1.FIRST_NAME, ' ', EMP1.LAST_NAME) AS 'Nome completo funcionário 1', 
 EMP1.SALARY AS 'Salário funcionário 1', 
 EMP1.PHONE_NUMBER AS 'Telefone funcionário 1',
-CONCAT(EMP2.FIRST_NAME, ' ', EMP2.LAST_NAME) AS 'Nome completo funcionário 2'
+CONCAT(EMP2.FIRST_NAME, ' ', EMP2.LAST_NAME) AS 'Nome completo funcionário 2',
+EMP2.SALARY AS 'Salário funcionário 2',
+EMP2.PHONE_NUMBER A S 'Telefone funcionário 2'
 FROM hr.employees AS EMP1
 JOIN hr.employees AS EMP2 
 ON EMP2.JOB_ID = EMP1.JOB_ID AND EMP2.EMPLOYEE_ID <> EMP1.EMPLOYEE_ID
+ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
