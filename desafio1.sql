99e046795bbe1b800988bd48f8c39531e20fcc09
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-09-21 15:02:56 -0300

    Corrige erro ortografico

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,6 @@
 SELECT
   c.COUNTRY_NAME AS 'País',
-  IF (r.REGION_NAME = 'Europe', 'incluido', 'não incluido') AS 'Status Inclusão'
+  IF (r.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM countries c
 JOIN regions r ON c.REGION_ID = r.REGION_ID
 ORDER BY c.COUNTRY_NAME;
