9c08e905e323c2e63ac3a6e79a205d292ba4c9ae
thales <thalesdaniel2013@gmail.com>
2021-09-21 20:48:35 -0300

    corrigido code climate

--- desafio4.sql
+++ desafio4.sql
@@ -1,14 +1,16 @@
 SELECT 
-   J.JOB_TITLE, ROUND(AVG(E.SALARY),2) AS 'Média salarial',
-   CASE
-WHEN AVG(E.SALARY) <= 5800 THEN 'Júnior'
-WHEN AVG(E.SALARY) <= 7800 THEN 'Pleno'
-WHEN AVG(E.SALARY) <= 10500 THEN 'Senior'
-ELSE 'CEO'
-END AS Senioridade
- FROM
+    J.JOB_TITLE,
+    ROUND(AVG(E.SALARY), 2) AS 'Média salarial',
+    CASE
+        WHEN AVG(E.SALARY) <= 5800 THEN 'Júnior'
+        WHEN AVG(E.SALARY) <= 7800 THEN 'Pleno'
+        WHEN AVG(E.SALARY) <= 10500 THEN 'Senior'
+        ELSE 'CEO'
+    END AS Senioridade
+FROM
     jobs J
         JOIN
     employees E ON J.JOB_ID = E.JOB_ID
 GROUP BY JOB_TITLE
-ORDER BY `Média salarial`, J.JOB_TITLE; 
+ORDER BY `Média salarial` , J.JOB_TITLE; 
+
