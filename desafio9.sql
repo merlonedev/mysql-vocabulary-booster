c26c8561aa347e73ef743a1f6ce0bc8be261d4a0
Martin <escorpmartin97@gmail.com>
2021-09-21 19:16:46 -0300

    [desafio9 - corrige erro de digitação2]

--- desafio9.sql
+++ desafio9.sql
@@ -1,5 +1,5 @@
 SELECT 
-    CONCAT(e.LastName, ' ', e.FirstName) AS 'Nome completo',
+    CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
     COUNT(o.EmployeeID) 'Total de pedidos'
 FROM
     w3schools.employees AS e
