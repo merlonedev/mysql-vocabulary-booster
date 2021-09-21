3050bf217b981963d160e5722989503fa7b2173d
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-09-21 16:08:07 -0300

    code climate

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,7 @@ SELECT
     shi.ShipperName AS `Empresa que fez o envio`,
     o.OrderDate AS `Data do pedido`
 FROM w3schools.orders AS o
-	INNER JOIN w3schools.customers AS cu ON o.CustomerID = cu.CustomerID
-    INNER JOIN w3schools.shippers AS shi ON o.ShipperID = shi.ShipperID
+INNER JOIN w3schools.customers AS cu ON o.CustomerID = cu.CustomerID
+INNER JOIN w3schools.shippers AS shi ON o.ShipperID = shi.ShipperID
 WHERE o.ShipperID IN (1, 2)
 ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido`;
