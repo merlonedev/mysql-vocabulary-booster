31bae73df7b61e64d21c71d390d0d8284b1f36ab
MGSiqueira <marcos.siqueira98@gmail.com>
2021-09-21 14:44:57 -0300

    Mais uma arrumadinha

--- desafio16.sql
+++ desafio16.sql
@@ -7,7 +7,7 @@ BEGIN
 DECLARE jobs_quantity INT;
     SELECT COUNT(jh.job_id) FROM hr.job_history jh
     WHERE jh.employee_id IN (
-	SELECT e.employee_id FROM hr.employees e
+SELECT e.employee_id FROM hr.employees e
         WHERE e.email = employee_email)
         INTO jobs_quantity;
         RETURN jobs_quantity;
