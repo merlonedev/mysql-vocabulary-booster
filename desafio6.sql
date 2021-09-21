55cda029e1ba65938747533355966c9bbad2a088
Luciano Lanes <llaneslopes@gmail.com>
2021-09-21 17:31:01 -0300

    Refatorado desafio 6 por falta de assertividade no enunciado

--- desafio6.sql
+++ desafio6.sql
@@ -1,12 +1,14 @@
-SELECT
-    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome Completo`,
+SELECT 
+    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
     j.JOB_TITLE AS Cargo,
-    e.HIRE_DATE AS `Data de início do cargo`,
+    START_DATE AS `Data de início do cargo`,
     d.DEPARTMENT_NAME AS Departamento
 FROM
-    hr.employees AS e
+    hr.job_history AS jh
         INNER JOIN
-    hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+    hr.employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
         INNER JOIN
-    hr.departments AS d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
-ORDER BY `Nome Completo` DESC , Cargo;
+    hr.jobs AS j ON jh.JOB_ID = j.JOB_ID
+        INNER JOIN
+    hr.departments AS d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC , Cargo;
