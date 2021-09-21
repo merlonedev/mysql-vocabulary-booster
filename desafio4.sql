dec7f02812bf230180c6f5f33a50e0247a68928e
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-09-21 15:38:27 -0300

    corrige desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,24 +1,41 @@
 SELECT 
     JOB_TITLE AS Cargo,
-    ROUND((MAX_SALARY + MIN_SALARY) / 2, 2) AS 'Média salarial',
+    (SELECT 
+            ROUND(AVG(SALARY), 2)
+        FROM
+            hr.employees
+        WHERE
+            JOB_ID = j.JOB_ID) AS 'Média salarial',
     CASE
         WHEN
-            ROUND(ROUND((MAX_SALARY + MIN_SALARY) / 2, 2),
-                    2) BETWEEN 2000 AND 5800
+            (SELECT 
+                    ROUND(AVG(SALARY), 2)
+                FROM
+                    hr.employees
+                WHERE
+                    JOB_ID = j.JOB_ID) BETWEEN 2000 AND 5800
         THEN
             'Júnior'
         WHEN
-            ROUND(ROUND((MAX_SALARY + MIN_SALARY) / 2, 2),
-                    2) BETWEEN 5801 AND 7500
+            (SELECT 
+                    ROUND(AVG(SALARY), 2)
+                FROM
+                    hr.employees
+                WHERE
+                    JOB_ID = j.JOB_ID) BETWEEN 5801 AND 7500
         THEN
             'Pleno'
         WHEN
-            ROUND(ROUND((MAX_SALARY + MIN_SALARY) / 2, 2),
-                    2) BETWEEN 7501 AND 10500
+            (SELECT 
+                    ROUND(AVG(SALARY), 2)
+                FROM
+                    hr.employees
+                WHERE
+                    JOB_ID = j.JOB_ID) BETWEEN 7501 AND 10500
         THEN
             'Sênior'
         ELSE 'CEO'
     END AS Senioridade
 FROM
-    hr.jobs
+    hr.jobs AS j
 ORDER BY `Média salarial` , Cargo;
