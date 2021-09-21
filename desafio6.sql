5fdaa301506376e2314b8ccaf3631a0e58f34203
tiagoornelas <advtiagoornelas@gmail.com>
2021-09-21 18:53:46 -0300

    Pretende resolver o requisito 6 [2]

--- desafio6.sql
+++ desafio6.sql
@@ -2,10 +2,10 @@ SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
 J.JOB_TITLE AS `Cargo`,
 JH.START_DATE AS `Data de início do cargo`,
 D.DEPARTMENT_NAME AS `Departamento`
-FROM hr.employees E
-RIGHT JOIN hr.jobs J
-ON E.JOB_ID = J.JOB_ID
-RIGHT JOIN hr.job_history JH
+FROM hr.job_history JH
+LEFT JOIN hr.jobs J
+ON JH.JOB_ID = J.JOB_ID
+LEFT JOIN hr.employees E
 ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
 LEFT JOIN hr.departments D
 ON D.DEPARTMENT_ID = JH.DEPARTMENT_ID
