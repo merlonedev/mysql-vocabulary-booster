d76e52839e94bdb896b8c57736112940803d44a6
johnpligeiro <johnpligeiro@gmail.com>
2021-09-21 19:59:39 -0300

    Desafio 18

--- desafio18.sql
+++ desafio18.sql
@@ -3,7 +3,7 @@ SELECT
   CONCAT(e.first_name, ' ', e.last_name) AS `Nome completo`,
   DATE_FORMAT(j.start_date, '%d/%m/%Y') AS `Data de início`,
   DATE_FORMAT(j.end_date, '%d/%m/%Y') AS `Data de rescisão`,
-  DATEDIFF(j.end_date, j.start_date) / 365 AS `Anos trabalhados`
+  ROUND(DATEDIFF(j.end_date, j.start_date) / 365, 2) AS `Anos trabalhados`
 FROM
     employees AS e
         INNER JOIN
