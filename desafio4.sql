5c4336617b314c405d4bd252a50190547b15bdbf
Gabriel Takaki <gabitj@icloud.com>
2021-09-21 16:15:31 -0300

    corrige agora? 4 e 5

--- desafio4.sql
+++ desafio4.sql
@@ -1,12 +1,12 @@
 SELECT 
-    e.JOB_ID AS Cargo,
+    e.JOB_ID AS 'Cargo',
     TRUNCATE(AVG(e.SALARY), 2) AS 'Média salarial',
     CASE
         WHEN AVG(e.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
         WHEN AVG(e.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
         WHEN AVG(e.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
         WHEN AVG(e.SALARY) > 10500 THEN 'CEO'
-    END AS Senioridade
+    END AS 'Senioridade'
 FROM
     hr.employees AS e
         INNER JOIN
--- desafio5.sql
+++ desafio5.sql
@@ -1,11 +1,8 @@
 SELECT 
-    e.JOB_ID AS Cargo,
-    h.MAX_SALARY - h.MIN_SALARY AS 'Variação Salarial',
-    TRUNCATE(MIN(e.SALARY / 12), 2) AS 'Média mínima mensal',
-    TRUNCATE(MAX(e.SALARY / 12), 2) AS 'Média máxima mensal'
+    JOB_TITLE AS 'Cargo',
+    (MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
+    TRUNCATE(MIN_SALARY / 12, 2) AS 'Média mínima mensal',
+    TRUNCATE(MAX_SALARY / 12, 2) AS 'Média máxima mensal'
 FROM
-    hr.employees AS e
-        INNER JOIN
-    hr.jobs AS h ON e.JOB_ID = h.JOB_ID
-GROUP BY e.JOB_ID
-ORDER BY e.SALARY ASC , h.JOB_TITLE ASC;
+	hr.jobs
+ORDER BY (MAX_SALARY - MIN_SALARY) ASC , JOB_TITLE ASC;
