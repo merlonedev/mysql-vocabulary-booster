f3d0ec0b7334e5878621ad03137a8ce43662384c
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-09-21 18:22:25 -0300

    code climate

--- desafio9.sql
+++ desafio9.sql
@@ -1,6 +1,6 @@
 SELECT
-	CONCAT(e.FirstName, ' ', e.LastName) AS `Nome completo`,
-    COUNT(o.OrderID) AS `Total de pedidos`
+CONCAT(e.FirstName, ' ', e.LastName) AS `Nome completo`,
+COUNT(o.OrderID) AS `Total de pedidos`
 FROM w3schools.orders AS o
 INNER JOIN w3schools.employees AS e ON o.EmployeeID = e.EmployeeID
 GROUP BY o.EmployeeID
