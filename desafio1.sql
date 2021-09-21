7c50a33089ad9ef7409d6899ce855170bff7df52
Uriel <urielbochi@gmail.com>
2021-09-21 14:58:09 -0300

    foi um acento?

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,2 @@
-SELECT CTRY.COUNTRY_NAME AS 'País', IF(CTRY.REGION_ID = 1, 'incluido', 'não incluido') AS 'Status Inclusão' FROM hr.countries AS CTRY
+SELECT CTRY.COUNTRY_NAME AS 'País', IF(CTRY.REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão' FROM hr.countries AS CTRY
 ORDER BY `País`;
