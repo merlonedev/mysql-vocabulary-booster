c280c0220c8eaf7b85691ef1acf4a7845b0e0780
Elias Forte <eliasforte.dev@gmail.com>
2021-09-20 22:28:14 -0300

    desafio 8

--- desafio4.sql
+++ desafio4.sql
@@ -1,14 +1,15 @@
-SELECT j.JOB_TITLE AS `Cargo`, m.`Média salarial`,
-CASE 
-WHEN m.`Média salarial` BETWEEN 2000 AND 5800 THEN 'Júnior'
-WHEN m.`Média salarial` BETWEEN 5801 AND 7500 THEN 'Pleno'
-WHEN m.`Média salarial` BETWEEN 7501 AND 10500 THEN 'Sênior'
-ELSE 'CEO'
-END AS `Senioridade`
-FROM jobs AS j
-INNER JOIN (
-	SELECT JOB_ID, (
-		SELECT ROUND(AVG(SALARY), 2) FROM employees
-WHERE JOB_ID = jobs.JOB_ID) AS `Média salarial` FROM jobs) AS m 
-ON j.JOB_ID = m.JOB_ID
-ORDER BY m.`Média salarial`, Cargo;
+SELECT 
+    j.JOB_TITLE AS `Cargo`,
+    ROUND(AVG(i.SALARY), 2) AS `Média salarial`,
+    CASE
+        WHEN AVG(i.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN AVG(i.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN AVG(i.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        WHEN AVG(i.SALARY) > 10500 THEN 'CEO'
+    END AS `Senioridade`
+FROM
+    jobs AS j
+        INNER JOIN
+    hr.employees AS i ON i.JOB_ID = j.JOB_ID
+GROUP BY `Cargo`
+ORDER BY `Média salarial` , JOB_TITLE;
