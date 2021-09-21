975244c84df852613a3ee1cfbeffc92cffb1162e
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-09-21 15:21:52 -0300

    Desafios 1 ao 5 corrigidos

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,2 @@
-SELECT COUNTRY_NAME AS 'País', 
-	CASE 
-		WHEN REGION_ID = 1 THEN 'incluído'
-        ELSE 'não incluído'
-	END AS 'Status Inclusão'
-    FROM hr.countries
-    ORDER BY `País`;
+SELECT COUNTRY_NAME AS 'País', CASE WHEN REGION_ID = 1 THEN 'incluído' ELSE 'não incluído' END AS 'Status Inclusão' FROM hr.countries ORDER BY `País`;
     
