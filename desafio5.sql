c779feb112d9edb2d41f7504aabb363b35f2ff0a
camila-mp <ca.malvp@gmail.com>
2021-09-21 17:41:28 -0300

    resolve desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -5,3 +5,7 @@
 -- *A quarta deve possuir o alias "Média máxima mensal" e exibir o salário máximo mensal daquele cargo. Arredonde a média com uma precisão de duas casas decimais.
 -- Os resultados devem estar ordenados pela variação salarial em ordem crescente.
 -- Em caso de empate na variação, os resultados devem ser ordenados pelo nome do cargo em ordem alfabética.
+SELECT JOB_TITLE AS `Cargo`, (MAX_SALARY - MIN_SALARY) AS `Variação Salarial`,
+ROUND((MIN_SALARY/12), 2) AS `Média mínima mensal`, ROUND((MAX_SALARY/12), 2) AS `Média máxima mensal`
+FROM hr.jobs
+ORDER BY `Variação Salarial` ASC, `Cargo` ASC;
