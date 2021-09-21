6fd7a690a89d6f8e956440059112cf6f7c9e79d2
Wadson Almeida <wadson.dev92@gmail.com>
2021-09-21 16:32:40 -0300

    Resolve codeclimate desafio1

--- desafio1.sql
+++ desafio1.sql
@@ -1,9 +1,9 @@
 select countries.country_name as `País`,
 case
-	when regions.REGION_NAME = 'Europe' then 'incluído'
-    when countries.REGION_ID <> 'Europe' then 'não incluído'
+	when regions.region_name = 'Europe' then 'incluído'
+    else 'não incluído'
 end as `Status Inclusão`
 from hr.countries as countries
 inner join regions as regions
 on countries.region_id = regions.region_id
-order by COUNTRY_NAME;
+order by country_name;
