c59da1b739c1e709d0accc030a483850552967fc
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-09-21 20:19:47 -0300

    adiciona desafio 18 salvo agora :p

--- desafio18.sql
+++ desafio18.sql
@@ -1,4 +1,11 @@
 SELECT 
-    CONCAT(FIRST_NAME, ' ', LAST_NAME) AS 'Nome completo'
+    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+    DATE_FORMAT(jh.START_DATE, '%d/%m/%Y') AS 'Data de início',
+    DATE_FORMAT(jh.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
+    ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE) / 365.25,
+            2) AS 'Anos trabalhados'
 FROM
-    hr.employees;
+    hr.employees AS e
+        INNER JOIN
+    hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+ORDER BY `Nome completo` , `Anos trabalhados`;
