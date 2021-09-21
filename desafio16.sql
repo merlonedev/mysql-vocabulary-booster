1f71f6ffa32e9b140874a2a6040207c922333ce4
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-09-21 16:10:24 -0300

    muitos2

--- desafio16.sql
+++ desafio16.sql
@@ -2,8 +2,7 @@ CREATE FUNCTION `buscar_quantidade_de_empregos_por_funcionario`(email VARCHAR(50
     READS SQL DATA
 BEGIN
   DECLARE resultado INT;
-  SELECT COUNT(*) FROM job_history as job_history INNER JOIN employees as employees ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
-  WHERE employees.EMAIL = email
+  SELECT COUNT(*) FROM job_history as job_history INNER JOIN employees as employees ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID WHERE employees.EMAIL = email
   INTO resultado;
   RETURN resultado;
 END
--- desafio19.sql
+++ desafio19.sql
@@ -1,8 +1,8 @@
 CREATE FUNCTION `exibir_quantidade_pessoas_contratadas_por_mes_e_ano`(mes INT, ano INT) RETURNS int
-    READS SQL DATA
+  READS SQL DATA
 BEGIN
-	DECLARE result INT;
-	SELECT COUNT(*) FROM employees AS employees WHERE MONTH(HIRE_DATE) = mes AND YEAR(HIRE_DATE) = ano
-	INTO result;
-	RETURN result;
+DECLARE result INT;
+SELECT COUNT(*) FROM employees AS employees WHERE MONTH(HIRE_DATE) = mes AND YEAR(HIRE_DATE) = ano
+INTO result;
+RETURN result;
 END
