baed08eacd2e275f5908d9bb3b97344e3d46b428
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-09-21 18:21:10 -0300

    Envia desafio14

---
+++ desafio14.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT
+  customers.Country AS 'País'
+FROM w3schools.customers AS customers
+UNION 
+SELECT 
+  suppliers.Country AS 'País'
+FROM w3schools.suppliers AS suppliers
+ORDER BY País ASC
+LIMIT 5;
