5d838574cb7f50f678a1f944e7ef3a3cd9982223
gabrielfndz <gabriel.omercador@hotmail.com>
2021-09-21 14:46:08 -0300

    ajustes codeclimate

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,9 @@
 SELECT 
-	country_name AS 'País',
-CASE
-	WHEN region_id = 1 THEN 'incluído'
-    ELSE 'não incluído'
-END AS 'Status Inclusão'
-FROM countries
-ORDER BY country_name asc;
+    country_name AS 'País',
+    CASE
+        WHEN region_id = 1 THEN 'incluído'
+        ELSE 'não incluído'
+    END AS 'Status Inclusão'
+FROM
+    countries
+ORDER BY country_name ASC;
