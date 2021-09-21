57033ea74034358c0398cd7a7bd2f8c7a51e309e
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-09-21 14:58:52 -0300

    Resolvendo erro de CodeClimate

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,6 @@
 SELECT
-	c.COUNTRY_NAME AS 'País',
-    IF (r.REGION_NAME = 'Europe', 'incluido', 'não incluido') AS 'Status Inclusão'
+  c.COUNTRY_NAME AS 'País',
+  IF (r.REGION_NAME = 'Europe', 'incluido', 'não incluido') AS 'Status Inclusão'
 FROM countries c
 JOIN regions r ON c.REGION_ID = r.REGION_ID
 ORDER BY c.COUNTRY_NAME;
