42c6bfec17f364f1a3476b31e50dd5035e85522c
Uriel <urielbochi@gmail.com>
2021-09-21 11:10:43 -0300

    desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,2 @@
+SELECT JOB_TITLE AS 'Cargo', (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo' from hr.jobs
+ORDER BY `Diferença entre salários máximo e mínimo` ASC, JOB_TITLE;
