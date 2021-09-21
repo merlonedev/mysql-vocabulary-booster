2b99c6d7a882bb598aa530eb5b2fde129751a0b3
gabrielfndz <gabriel.omercador@hotmail.com>
2021-09-21 19:36:09 -0300

    Desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+SELECT 
+    c.ContactName AS 'Nome de contato',
+    s.ShipperName AS 'Empresa que fez o envio',
+    o.OrderDate AS 'Data do Pedido'
+FROM
+    orders AS o
+        INNER JOIN
+    shippers AS s ON o.ShipperID = s.ShipperID
+        INNER JOIN
+    customers AS c ON o.CustomerID = c.CustomerID
+WHERE
+    s.ShipperName IN ('Speedy Express' , 'United Package')
+ORDER BY c.ContactName , s.ShipperName , o.OrderDate ASC;
