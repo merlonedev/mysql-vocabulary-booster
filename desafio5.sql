d4362971de412749d57eec0ee1343e598d2244c7
matheustkaczyk <matheus111904@hotmail.com>
2021-09-21 14:47:36 -0300

    Corrigido requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
-SELECT job_title AS `Cargo`,
-ROUND(max_salary - min_salary, 2) AS `Variação Salarial`,
-ROUND(min_salary / 12, 2) AS `Média mínima mensal`,
-ROUND(max_salary / 12, 2) AS `Média máxima mensal`
+SELECT JOB_TITLE AS `Cargo`,
+MAX_SALARY - MIN_SALARY AS `Variação Salarial`,
+ROUND(MIN_SALARY / 12, 2) AS `Média mínima mensal`,
+ROUND(MAX_SALARY / 12, 2) AS `Média máxima mensal`
 FROM hr.jobs J
 INNER JOIN hr.employees E
 ON J.JOB_ID = E.JOB_ID
