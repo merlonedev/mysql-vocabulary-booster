cbe7d734cdca2137ff13f062ed4298d2e0faa4c1
julio11lp <julio11lp@hotmail.com>
2021-09-21 17:38:40 -0300

    Desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT 
+    prod.ProductName AS `Produto`,
+    MIN(Quantity) AS `Mínima`,
+    MAX(Quantity) AS `Máxima`,
+    ROUND(AVG(Quantity), 2) AS `Média`
+FROM
+    w3schools.products AS prod
+    INNER JOIN w3schools.order_details AS ords ON ords.ProductID = prod.ProductID
+GROUP BY `Produto`
+	HAVING ROUND(AVG(Quantity), 2) > 20.00
+ORDER BY `Média`, `Produto`;
