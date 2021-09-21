9d753a70a6b857b9461e5166b9ff59737dc112ce
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-09-21 19:02:30 -0300

    Desafios 11 ao 15 corrigidos III

--- desafio13.sql
+++ desafio13.sql
@@ -4,4 +4,3 @@ FROM w3schools.products
 WHERE ProductID 
 IN (SELECT ProductID FROM w3schools.order_details WHERE Quantity > 80)
 ORDER BY `Produto`;
-
--- desafio14.sql
+++ desafio14.sql
@@ -2,4 +2,3 @@ SELECT Sub.Country AS 'País' FROM
 (SELECT DISTINCT Country FROM w3schools.customers
 UNION ALL SELECT DISTINCT Country FROM w3schools.suppliers
 ORDER BY Country LIMIT 5) AS Sub;
-
