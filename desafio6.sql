317d0f194d0133a1bc74ab0ff582b3fd21240f28
thiagopcdev <thiagopc_dev@hotmail.com>
2021-09-21 17:09:30 -0300

    Def-6-fix-1

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
-	j.JOB_TITLE AS `Cargo`,
-  jh.START_DATE AS `Data de início do cargo`,
-  d.DEPARTMENT_NAME AS `Departamento`
+j.JOB_TITLE AS `Cargo`,
+jh.START_DATE AS `Data de início do cargo`,
+d.DEPARTMENT_NAME AS `Departamento`
 FROM
   hr.employees e
 JOIN hr.jobs j ON j.JOB_ID = e.JOB_ID
