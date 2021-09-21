36ca39c9615a1741675bf41ba3106492ad76f514
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-09-21 14:56:27 -0300

    add desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,11 @@
+SELECT * FROM hr.jobs;
+
+SELECT
+	JOB_TITLE AS Cargo, 
+	CASE
+		WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
+        WHEN MAX_SALARY > 20001 THEN 'Alto'
+        WHEN MAX_SALARY > 10001 THEN 'Médio'
+        WHEN MAX_SALARY >= 5000 THEN 'Baixo'
+	END AS Nível
+FROM hr.jobs;
