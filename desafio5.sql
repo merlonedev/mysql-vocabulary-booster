404f1b932d2678ba4c9f977c4605bb139a09f022
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-09-21 18:37:49 -0300

    Fazendo requisito 5

---
+++ desafio5.sql
@@ -0,0 +1,11 @@
+SELECT
+    Cargo.job_title AS 'Cargo',
+    ROUND(Cargo.MAX_SALARY - Cargo.MIN_SALARY, 2) AS 'Variação Salarial',
+    ROUND(Cargo.MIN_SALARY / 12, 2) AS 'Média mínima mensal',
+    ROUND(Cargo.MAX_SALARY / 12, 2) AS 'Média máxima mensal'
+FROM
+    hr.employees AS employee
+        INNER JOIN
+    hr.jobs AS Cargo ON employee.JOB_ID = Cargo.JOB_ID
+GROUP BY employee.JOB_ID
+ORDER BY Variação Salarial ASC , Cargo ASC;
