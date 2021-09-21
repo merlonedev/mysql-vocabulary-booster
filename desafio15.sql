cf4dc45600182448465eebc6f03cd4ddab553574
johnpligeiro <johnpligeiro@gmail.com>
2021-09-21 18:41:22 -0300

    Desafio 15 (procedure)

---
+++ desafio15.sql
@@ -0,0 +1,14 @@
+USE hr;
+DELIMITER $$;
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
+BEGIN
+SELECT
+ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
+FROM employees AS e
+JOIN jobs AS j
+ON e.JOB_ID = j.JOB_ID
+WHERE j.JOB_TITLE = cargo;
+END $$
+
+DELIMITER ;
