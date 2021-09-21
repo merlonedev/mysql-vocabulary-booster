145981b2b90926d13cf62d1f45489cc97a385cc9
Erick Silva <erick.silva6653@gmail.com>
2021-09-20 22:04:18 -0300

    desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,16 @@
+SELECT
+    CONCAT(employees.FIRST_NAME,
+            ' ',
+            employees.LAST_NAME) AS `Nome completo`,
+    jobs.JOB_TITLE AS Cargo,
+    j_history.START_DATE AS `Data de início do cargo`,
+    departments.DEPARTMENT_NAME AS Departamento
+FROM
+	hr.job_history AS j_history
+        JOIN
+	hr.jobs AS jobs ON jobs.JOB_ID = j_history.JOB_ID
+        JOIN
+	hr.employees AS employees ON employees.EMPLOYEE_ID = j_history.EMPLOYEE_ID
+		JOIN
+	hr.departments AS departments ON departments.DEPARTMENT_ID = j_history.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, Cargo;
