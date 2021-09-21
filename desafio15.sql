fe8022566855397356dd1c8acf981d6c77827e02
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-09-21 16:24:55 -0300

    muitos5

--- desafio15.sql
+++ desafio15.sql
@@ -2,6 +2,6 @@ USE hr
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(30))
 BEGIN
-	SELECT ROUND(AVG(employees.SALARY),2) AS `Média salarial` FROM employees AS employees INNER JOIN jobs as jobs ON jobs.JOB_ID = employees.JOB_ID WHERE jobs.JOB_TITLE = cargo;
+SELECT ROUND(AVG(employees.SALARY),2) AS `Média salarial` FROM employees AS employees INNER JOIN jobs as jobs ON jobs.JOB_ID = employees.JOB_ID WHERE jobs.JOB_TITLE = cargo;
 END $$
 DELIMITER
