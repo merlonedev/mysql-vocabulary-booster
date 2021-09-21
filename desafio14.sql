09ae1f90de5cf60316ef72ace4090bca7570ed07
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-09-21 20:27:04 -0300

    done 11-15

---
+++ desafio14.sql
@@ -0,0 +1,9 @@
+SELECT 
+    Country
+FROM
+    w3schools.customers 
+UNION SELECT 
+    Country
+FROM
+    w3schools.suppliers
+ORDER BY Country;
---
+++ desafio15.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(job_name VARCHAR(100))
+	BEGIN
+		SELECT
+			ROUND(AVG(e.SALARY), 2) AS `Média salarial`
+		FROM
+			hr.employees e,
+			hr.jobs j
+		WHERE j.JOB_TITLE = job_name
+			AND e.JOB_ID = j.JOB_ID;
+  END $$
+
+DELIMITER ;
