a2fd5a376dfc7d80e544da1102d196d386a0d93f
Martin <escorpmartin97@gmail.com>
2021-09-21 15:53:16 -0300

    [desafio2 e corrige desafio1]

--- desafio1.sql
+++ desafio1.sql
@@ -6,9 +6,9 @@ SELECT
     CASE
         WHEN r.region_name = 'Europe' THEN 'incluído'
         ELSE 'não incluído'
-    END 'Status Inclusão'
+    END AS 'Status Inclusão'
 FROM
     hr.countries AS c
         INNER JOIN
-    hr.regions AS r
+    hr.regions AS r ON c.region_id = r.region_id
 ORDER BY c.country_name;
