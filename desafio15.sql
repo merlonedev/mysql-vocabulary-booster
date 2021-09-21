04f4cb22e33569e3ad34d19bcc3d057dcb5869d1
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-09-21 16:56:01 -0300

    algo

--- desafio15.sql
+++ desafio15.sql
@@ -1,9 +1,6 @@
 DELIMITER $$ 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(30)) 
 BEGIN 
-SELECT 
-ROUND(AVG(employees.SALARY),2) AS `Média salarial` 
-FROM employees AS employees JOIN jobs as jobs ON jobs.JOB_ID = employees.JOB_ID 
-WHERE jobs.JOB_TITLE = cargo; 
+SELECT ROUND(AVG(employees.SALARY),2) AS `Média salarial` FROM employees AS employees JOIN jobs as jobs ON jobs.JOB_ID = employees.JOB_ID WHERE jobs.JOB_TITLE = cargo
 END $$ 
 DELIMITER ;
