70770859d93c47018dcc7e3693075bcb54c57fd8
Uriel <urielbochi@gmail.com>
2021-09-21 14:51:48 -0300

    tentando corrigir lint do desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,2 @@
-SELECT COUNTRY_NAME AS 'País', IF(REGION_ID = 1, 'incluido', 'não incluido') as 'Status Inclusão' from hr.countries
+SELECT CTRY.COUNTRY_NAME AS 'País', IF(CTRY.REGION_ID = 1, 'incluido', 'não incluido') AS 'Status Inclusão' FROM hr.countries AS CTRY
 ORDER BY `País`;
