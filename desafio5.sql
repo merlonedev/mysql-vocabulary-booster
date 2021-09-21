c0d0b8a185e0e501f84b8a0527e5b972d9789bc1
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-09-21 14:57:02 -0300

    desafio 05 - correcao

--- desafio5.sql
+++ desafio5.sql
@@ -7,4 +7,5 @@ FROM
     hr.employees AS e
         INNER JOIN
     hr.jobs AS j ON e.JOB_ID = j.JOB_ID
-ORDER BY `Variação Salarial`,`Cargo`;
+GROUP BY e.JOB_ID
+ORDER BY `Variação Salarial`, `Cargo`;
