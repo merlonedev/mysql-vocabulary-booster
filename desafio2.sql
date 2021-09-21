e191ac7006f35d7fc7afc39c3e3019b715993689
thiagopcdev <thiagopc_dev@hotmail.com>
2021-09-21 15:03:53 -0300

    Try fix climate def2

--- desafio2.sql
+++ desafio2.sql
@@ -1,11 +1,11 @@
 SELECT 
-    job_title AS Cargo,
-    CASE
+  job_title AS Cargo,
+  CASE
 		WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
-        WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
-        WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
-        ELSE 'Altíssimo'
+      WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
+      WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
+      ELSE 'Altíssimo'
 	END AS Nivel
 FROM
-    hr.jobs
+  hr.jobs
 ORDER BY Cargo;
