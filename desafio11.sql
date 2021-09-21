65e19ba565e09b619c609c04acd3f20fa7c2cb08
Uriel <urielbochi@gmail.com>
2021-09-21 18:10:09 -0300

    era o nome do arquivo?

--- desafio11.sql
+++ desafio11.sql
@@ -5,11 +5,10 @@ SELECT
 FROM
   w3schools.customers AS coutr1,
   w3schools.customers AS coutr2 
-WHERE
-  coutr1.Country = coutr2.Country 
+WHERE coutr2.Country = coutr1.Country AND coutr2.CustomerID != coutr1.CustomerID
 GROUP BY
   `Nome` 
 HAVING
   `Número de compatriotas` <> 0 
 ORDER BY
-   `Nome`;
+  `Nome`;
