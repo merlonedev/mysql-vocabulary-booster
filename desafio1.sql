c324145deae9e4e7f18f445c1ead12083d3e0b2a
Heder Waine <hederw@hotmail.com>
2021-09-21 20:21:24 -0300

    Desafio 1

---
+++ desafio1.sql
@@ -0,0 +1,9 @@
+-- select * from hr.countries;
+
+SELECT C.COUNTRY_NAME AS `Pais`,
+IF(r.REGION_NAME = "Europe", "incluido", "não incluido") AS `Status Inclusão`
+FROM 
+hr.countries AS C
+JOIN 
+hr.regions AS r ON r.REGION_ID = C.REGION_ID
+ORDER BY C.COUNTRY_NAME;
