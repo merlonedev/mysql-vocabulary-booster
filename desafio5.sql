967f40ce71a8b674cf232b969b8f281c706f7379
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-09-21 14:38:00 -0300

    code climate

---
+++ desafio5.sql
@@ -0,0 +1,10 @@
+SELECT 
+    j.JOB_TITLE AS `Cargo`,
+    (j.MAX_SALARY - j.MIN_SALARY) AS `Variação Salarial`,
+    ROUND((j.MIN_SALARY / 12), 2) AS `Média mínima mensal`,
+    ROUND((j.MAX_SALARY / 12), 2) AS `Média máxima mensal`
+FROM
+    hr.employees AS e
+        INNER JOIN
+    hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+ORDER BY `Variação Salarial` ASC,`Cargo` ASC;
