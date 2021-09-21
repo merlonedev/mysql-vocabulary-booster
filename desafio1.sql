fd9c2e91b56ee3781d1a0c49348d681a3404b383
Gabriel Takaki <gabitj@icloud.com>
2021-09-21 14:48:46 -0300

    desafio 2

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,9 @@
-SELECT COUNTRY_NAME,
-	CASE
-		WHEN REGION_ID <> 1 THEN 'não incluído'
-        ELSE "incluído"
-	END AS `Status Inclusão`
-FROM countries
-ORDER BY COUNTRY_NAME;
+SELECT 
+    COUNTRY_NAME,
+    CASE
+        WHEN REGION_ID <> 1 THEN 'não incluído'
+        ELSE 'incluído'
+    END AS `Status Inclusão`
+FROM
+    hr.countries
+ORDER BY COUNTRY_NAME
