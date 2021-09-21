4d671d745aab10ace7e5dcc6f93e440528a7f19b
fredmartins <fredweb22@gmail.com>
2021-09-21 16:48:40 -0300

    tentativa desafio1

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,8 @@
-SELECT
-    COUNTRY_NAME AS 'Pais',
-    IF(region_id = 1, 'incluido', 'não incluido') AS 'Status Inclusão'
+SELECT 
+    country_name AS 'País',
+    IF(region_id = 1,
+        'incluído',
+        'não incluído') AS 'Status Inclusão'
 FROM
     hr.countries
 ORDER BY COUNTRY_NAME;
