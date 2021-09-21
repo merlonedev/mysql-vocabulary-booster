efdba04b87c1cfb6e61687730ab83c41829efc93
José Reginaldo Da Silva Neto <netosilv44445@gmail.com>
2021-09-21 14:57:50 -0300

    fix 3

--- desafio1.sql
+++ desafio1.sql
@@ -2,4 +2,18 @@ SELECT
   COUNTRY_NAME AS 'País',
   IF(REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM
-  hr.countries;
+  hr.countries
+ORDER BY
+  COUNTRY_NAME;
+
+SELECT
+  c.COUNTRY_NAME AS 'País',
+  CASE
+    WHEN r.REGION_NAME = 'Europe' THEN 'incluído'
+    ELSE 'não incluído'
+  END AS 'Status Inclusão'
+FROM
+  countries c
+  INNER JOIN regions r ON c.REGION_ID = r.REGION_ID
+ORDER BY
+  c.COUNTRY_NAME;
