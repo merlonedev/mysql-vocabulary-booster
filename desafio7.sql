0abc69507c11660e80ca7f043c2195f738155a3a
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-09-21 12:09:21 -0300

    Requisitos corrigidos

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
     SELECT 
     UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
-    jh.START_DATE AS 'Data de início do cargo',
-    e.SALARY AS 'Departamento'
+    jh.START_DATE AS 'Data de início',
+    e.SALARY AS 'Salário'
 FROM
     hr.job_history AS jh
         INNER JOIN
