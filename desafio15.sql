0a9a38beedfe6b313fe1f09c1811dd79ff3605ea
Gabriel Takaki <gabitj@icloud.com>
2021-09-21 19:36:26 -0300

    Lint fake

--- desafio15.sql
+++ desafio15.sql
@@ -1,7 +1,7 @@
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
 BEGIN
-	SELECT ROUND(AVG(SALARY), 2) AS `Média salarial` FROM employees AS e
+	SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial` FROM employees AS e
     INNER JOIN jobs AS j ON j.JOB_ID = e.JOB_ID
     WHERE j.JOB_TITLE = cargo;
 END $$
