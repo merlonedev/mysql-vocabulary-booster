e84c7f3743ababeffbe1c1b7933c607db4bf8d13
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-09-21 16:27:45 -0300

    Corrigindo 5

--- desafio5.sql
+++ desafio5.sql
@@ -2,10 +2,10 @@
 -- considerando salários máximo e minímo como anuais
 
 SELECT 
-    JOB_TITLE AS `Cargo`,
-	(MAX_SALARY - MIN_SALARY) AS `Variação Salarial`,
-    ROUND((MIN_SALARY / 12), 2) AS `Média mínima mensal`,
-    ROUND((MAX_SALARY / 12), 2) AS `Média máxima mensal`
+  JOB_TITLE AS `Cargo`,
+(MAX_SALARY - MIN_SALARY) AS `Variação Salarial`,
+  ROUND((MIN_SALARY / 12), 2) AS `Média mínima mensal`,
+  ROUND((MAX_SALARY / 12), 2) AS `Média máxima mensal`
 FROM
-    jobs
+  jobs
 ORDER BY `Variação Salarial` ASC, `Cargo` ASC;
