32df766dd3967d5ad613aa08433f30a247378b6f
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-09-21 08:41:22 -0300

    Requisito 8 e 9

---
+++ desafio9.sql
@@ -0,0 +1,9 @@
+    SELECT 
+    CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome Completo',
+    COUNT(o.OrderID) AS 'Total de pedidos'
+FROM
+    w3schools.employees AS e
+        INNER JOIN
+    w3schools.orders AS o ON o.EmployeeID = e.EmployeeID
+GROUP BY CONCAT(e.FirstName, ' ', e.LastName)
+ORDER BY COUNT(o.OrderID) ASC;
