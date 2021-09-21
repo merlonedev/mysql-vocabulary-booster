0248552ae7db912ef48a873162872040180afcc3
Uriel <urielbochi@gmail.com>
2021-09-21 16:44:36 -0300

    tentativa 11 again

--- desafio 11.sql
+++ desafio 11.sql
@@ -1,8 +1,15 @@
-SELECT coutr1.ContactName AS 'Nome',
-coutr1.Country AS 'País',
-COUNT(coutr1.Country) - 1 AS 'Número de compatriotas'
-FROM w3schools.customers AS coutr1, w3schools.customers AS coutr2
-WHERE coutr1.Country = coutr2.Country
-GROUP BY `Nome`
-HAVING `Número de compatriotas` <> 0
-ORDER BY `Nome`;
+SELECT
+   coutr1.ContactName AS 'Nome',
+   coutr1.Country AS 'País',
+   COUNT(coutr1.Country) - 1 AS 'Número de compatriotas' 
+FROM
+   w3schools.customers AS coutr1,
+   w3schools.customers AS coutr2 
+WHERE
+   coutr1.Country = coutr2.Country 
+GROUP BY
+   `Nome` 
+HAVING
+   `Número de compatriotas` <> 0 
+ORDER BY
+   `Nome`;
