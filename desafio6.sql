b04376f17d4deb0b65bc58235b5f6ba7a792aa94
Guilherme Dias <gmd.delta@gmail.com>
2021-09-21 01:02:01 -0300

    [PT-BR]Resolvido requisito 6. [EN] Solved 6th requirement.

---
+++ desafio6.sql
@@ -0,0 +1,25 @@
+SELECT
+    (SELECT 
+            CONCAT(FIRST_NAME, ' ', LAST_NAME)
+        FROM
+            hr.employees
+        WHERE
+            hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID) AS 'Nome completo',
+    (SELECT 
+            JOB_TITLE
+        FROM
+            hr.jobs
+        WHERE
+            hr.job_history.JOB_ID = hr.jobs.JOB_ID) AS 'Cargo',
+    START_DATE AS 'Data de início do cargo',
+    (SELECT 
+            DEPARTMENT_NAME
+        FROM
+            hr.departments
+        WHERE
+            hr.job_history.DEPARTMENT_ID = hr.departments.DEPARTMENT_ID) AS 'Departamento'
+FROM
+    hr.job_history
+WHERE
+    EMPLOYEE_ID <> 0
+ORDER BY 1 DESC , 2;
