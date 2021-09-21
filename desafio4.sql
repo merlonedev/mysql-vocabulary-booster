2a8ef36b37155377dfb17dffaede85c2847b0d51
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-09-21 17:36:26 -0300

    Resolvendo requisito 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,12 +1,32 @@
-SELECT
-    job_title AS 'Cargo',
-    ROUND((MAX_SALARY + MIN_SALARY) / 2, 2) AS 'Média salarial',
+SELECT 
+    Cargo.job_title AS 'Cargo',
+    ROUND(SUM(employee.SALARY) / COUNT(employee.JOB_ID),
+            2) AS 'Média salarial',
     (CASE
-        WHEN ROUND((MAX_SALARY + MIN_SALARY) / 2, 2) BETWEEN 1999 AND 5801 THEN 'Júnior'
-        WHEN ROUND((MAX_SALARY + MIN_SALARY) / 2, 2) BETWEEN 5800 AND 7501 THEN 'Pleno'
-        WHEN ROUND((MAX_SALARY + MIN_SALARY) / 2, 2) BETWEEN 7500 AND 10501 THEN 'Sênior'
-        WHEN ROUND((MAX_SALARY + MIN_SALARY) / 2, 2) > 10500 THEN 'CEO'
+        WHEN
+            ROUND(SUM(employee.SALARY) / COUNT(employee.JOB_ID),
+                    2) BETWEEN 1999 AND 5801
+        THEN
+            'Júnior'
+        WHEN
+            ROUND(SUM(employee.SALARY) / COUNT(employee.JOB_ID),
+                    2) BETWEEN 5800 AND 7501
+        THEN
+            'Pleno'
+        WHEN
+            ROUND(SUM(employee.SALARY) / COUNT(employee.JOB_ID),
+                    2) BETWEEN 7500 AND 10501
+        THEN
+            'Sênior'
+        WHEN
+            ROUND(SUM(employee.SALARY) / COUNT(employee.JOB_ID),
+                    2) > 10500
+        THEN
+            'CEO'
     END) AS 'Senioridade'
 FROM
-    hr.jobs
-ORDER BY `Média salarial` ASC , job_title ASC;
+    hr.employees AS employee
+        INNER JOIN
+    hr.jobs AS Cargo ON employee.JOB_ID = Cargo.JOB_ID
+GROUP BY employee.JOB_ID
+ORDER BY `Média salarial` ASC , `Cargo` ASC;
