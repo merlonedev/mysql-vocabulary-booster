baef285ffee969958a266c399e1d4b35f1179575
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-09-21 16:40:42 -0300

    code climate desafio 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,6 +1,12 @@
 SELECT 
-	(SELECT CONCAT(FirstName, ' ', LastName) FROM w3schools.employees WHERE EmployeeID = o.EmployeeID) AS `Nome completo`,
+    (SELECT 
+            CONCAT(FirstName, ' ', LastName)
+        FROM
+            w3schools.employees
+        WHERE
+            EmployeeID = o.EmployeeID) AS `Nome completo`,
     COUNT(o.CustomerID) AS `Total de pedidos`
-FROM w3schools.orders AS o
+FROM
+    w3schools.orders AS o
 GROUP BY `Nome completo`
 ORDER BY `Total de pedidos`;
