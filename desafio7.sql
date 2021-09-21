bffee8f2a12fa08a34cf6026b369a209429f8df3
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-09-21 19:16:40 -0300

    Resolvendo desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+SELECT
+    UPPER(CONCAT(employee.FIRST_NAME,
+                    ' ',
+                    employee.LAST_NAME)) AS 'Nome completo',
+    jhistory.START_DATE AS 'Data de início',
+    employee.SALARY AS 'Salário'
+FROM
+    hr.employees AS employee
+        INNER JOIN
+    hr.job_history AS jhistory ON employee.EMPLOYEE_ID = jhistory.EMPLOYEE_ID
+WHERE
+    MONTH(jhistory.START_DATE) BETWEEN 01 AND 03
+ORDER BY `Nome completo` ASC , `Data de início` DESC;
