449dde1473da92fc5829d70c000021bfa5811dfb
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-09-21 14:38:28 -0300

    Requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+ SELECT 
+    A.ContactName AS 'Nome',
+    A.Country AS 'País',
+    COUNT(*) AS 'Número de compatriotas'
+FROM
+    customers A,
+    customers B
+WHERE
+    A.Country = B.Country
+        AND A.CustomerID <> B.CustomerID
+GROUP BY A.ContactName
+ORDER BY A.ContactName ASC;
