34b80722beabd2e8c7359de8eef0e971192dbed4
Eric Kreis <erickreis793@gmail.com>
2021-09-21 14:49:44 -0300

    [ Code Climate ]

--- desafio13.sql
+++ desafio13.sql
@@ -1,14 +1,10 @@
 SELECT
-	ProductName AS Produto,
-  Price AS Preço
-FROM
-	w3schools.products AS p
+ProductName AS Produto,
+Price AS Preço
+FROM w3schools.products AS p
 WHERE
-	EXISTS ( SELECT
-		*
-	FROM
-		w3schools.order_details AS od
-	WHERE
-		p.ProductID = od.ProductID
-    AND Quantity > 80)
+	EXISTS ( SELECT *
+	FROM w3schools.order_details AS od
+	WHERE p.ProductID = od.ProductID
+  AND Quantity > 80)
 ORDER BY Produto;
--- desafio18.sql
+++ desafio18.sql
@@ -1,10 +1,10 @@
 SELECT
-	CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-  DATE_FORMAT(START_DATE, '%d/%m/%Y') AS 'Data de início',
-  DATE_FORMAT(END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
-  ROUND(DATEDIFF(END_DATE, START_DATE) / 365, 2) AS 'Anos trabalhados'
+CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+DATE_FORMAT(START_DATE, '%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT(END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
+ROUND(DATEDIFF(END_DATE, START_DATE) / 365, 2) AS 'Anos trabalhados'
 FROM
-	hr.job_history AS jh
-		JOIN
-	hr.employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+hr.job_history AS jh
+JOIN
+hr.employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
 ORDER BY `Nome completo`, `Anos trabalhados`;
