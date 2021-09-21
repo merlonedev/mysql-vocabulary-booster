6adc50ea7cb1c7a8c11c35c3555011e6199da536
Flavio_Cordeiro <flavio_ribeirorc@hotmail.com>
2021-09-21 20:36:27 -0300

    80% pls

--- desafio7.sql
+++ desafio7.sql
@@ -5,9 +5,9 @@ SELECT
     job_history.START_DATE AS 'Data de início',
     employees.SALARY AS 'Salário'
 FROM
-     job_history AS job_history
+      job_history AS job_history
         JOIN
-     employees AS employees ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+      employees AS employees ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
 WHERE MONTH(job_history.START_DATE) = 01 
 OR MONTH(job_history.START_DATE) = 02 
 OR MONTH(job_history.START_DATE) = 03
