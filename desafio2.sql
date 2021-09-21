fdfa97fc773156c96684ecc9e809ef7e71237015
Martin <escorpmartin97@gmail.com>
2021-09-21 16:55:01 -0300

    [desafio5]

--- desafio2.sql
+++ desafio2.sql
@@ -1,10 +1,10 @@
 SELECT 
     job_title AS 'Cargo',
     CASE
-        WHEN max_salary BETWEEN 5000 AND 1000 THEN 'Baixo'
+        WHEN max_salary BETWEEN 1000 AND 5000 THEN 'Baixo'
         WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
         WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
-        ELSE Altíssimo
+        ELSE 'Altíssimo'
     END AS 'Nível'
 FROM
     hr.jobs
