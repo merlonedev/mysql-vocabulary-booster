364e88909dc06961143444b8f8f79b6c14e0401d
julio11lp <julio11lp@hotmail.com>
2021-09-21 15:04:51 -0300

    Mudanca do else para when no requisito 4

--- desafio4.sql
+++ desafio4.sql
@@ -5,10 +5,10 @@ SELECT
 WHEN ROUND(AVG(emp.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
 WHEN ROUND(AVG(emp.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
 WHEN ROUND(AVG(emp.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
-        ELSE 'CEO'
+WHEN ROUND(AVG(emp.SALARY), 2) > 10500 THEN 'CEO'
     END AS `Senioridade`
 FROM
     hr.employees AS emp
     INNER JOIN hr.jobs AS job ON job.JOB_ID = emp.JOB_ID
 GROUP BY JOB_TITLE
-ORDER BY `Média salarial` ASC, `Cargo`;
+ORDER BY `Média salarial`, `Cargo`;
