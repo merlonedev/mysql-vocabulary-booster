8f313f52a6b661c7c83c58148728470e042be9cc
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-09-21 19:19:25 -0300

    Requirement 16

---
+++ desafio16.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100))
+RETURNS INT READS SQL DATA
+BEGIN
+  RETURN (
+    SELECT
+      COUNT(*) AS total_empregos
+    FROM
+        hr.job_history AS jh
+            INNER JOIN
+        hr.employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+    WHERE e.EMAIL = email
+  );
+END $$
+
+DELIMITER ;
