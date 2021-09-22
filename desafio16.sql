53fdf5f7c3b09a50bdd4022e58c82fb13a55b99a
tiagoornelas <advtiagoornelas@gmail.com>
2021-09-22 17:24:42 -0300

    Pretende resolver o requisito 16 [2]

--- desafio16.sql
+++ desafio16.sql
@@ -1,8 +1,11 @@
 USE hr;
 
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario (EMAIL VARCHAR(150))
-RETURNS CHAR(4) DETERMINISTIC
-RETURN (SELECT COUNT(*) FROM hr.job_history JH
+RETURNS INT DETERMINISTIC
+RETURN (SELECT COUNT(*)
+FROM hr.job_history JH
 INNER JOIN hr.employees E
 ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
 WHERE E.EMAIL = EMAIL);
+
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR') AS `total_empregos`;
