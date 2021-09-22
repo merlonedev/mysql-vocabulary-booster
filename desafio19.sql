34ec8aef5a4a3854e5bf4a1420f05201f9974b88
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-09-22 09:23:36 -0300

    desafio 19

---
+++ desafio19.sql
@@ -0,0 +1,11 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(inMonth INT, inYear INT)
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE hireQty INT;
+    SELECT COUNT(*) FROM `employees` WHERE MONTH(HIRE_DATE) = inMonth AND YEAR(HIRE_DATE) = inYear INTO hireQty;
+    RETURN hireQty;
+END $$
+DELIMITER ;
