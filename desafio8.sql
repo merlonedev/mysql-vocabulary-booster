fd82d09959b4da9471e5da863a1547b03d1d6255
AndreHammel <hammel.engineer@gmail.com>
2021-09-21 21:07:07 -0300

    Update desafio8.sql

--- desafio8.sql
+++ desafio8.sql
@@ -1,17 +1,14 @@
 SELECT 
-	CU.ContactName AS `Nome de contato`,
+    CU.ContactName AS `Nome de contato`,
     S.ShipperName AS `Empresa que fez o envio`,
     O.OrderDate AS `Data do pedido`
 FROM
-	customers AS CU
-JOIN 
-	orders as O
-ON
-	 O.CustomerID = CU.CustomerID 
-JOIN
-	shippers as S
-ON
-	O.ShipperID = S.ShipperID
+    customers AS CU
+        JOIN
+    orders AS O ON O.CustomerID = CU.CustomerID
+        JOIN
+    shippers AS S ON O.ShipperID = S.ShipperID
 WHERE
-	S.ShipperName IN ('Speedy Express','United Package')
-ORDER BY CU.ContactName, S.ShipperName;
+    S.ShipperName IN ('Speedy Express' , 'United Package')
+ORDER BY CU.ContactName , S.ShipperName;
+
