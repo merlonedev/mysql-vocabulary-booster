5cb3e646be680987575c2d28f337c0e75685f47c
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-09-22 13:06:43 -0300

    completa req 14 - reidentação

--- desafio14.sql
+++ desafio14.sql
@@ -1,5 +1,10 @@
-SELECT Country AS `País` FROM suppliers
-UNION
-SELECT Country AS `País` FROM customers
-ORDER BY `País`
+SELECT 
+    Country AS `País`
+FROM
+    suppliers 
+UNION SELECT 
+    Country AS `País`
+FROM
+    customers
+ORDER BY País
 LIMIT 5;
