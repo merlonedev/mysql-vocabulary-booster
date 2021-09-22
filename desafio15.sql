384e081086b9efcbaa9893555cf7f67c8fe57481
Daniel <danielbped@gmail.com>
2021-09-21 23:35:45 -0300

    codeclimate

--- desafio15.sql
+++ desafio15.sql
@@ -2,10 +2,10 @@ DELIMITER &&
 
 CREATE PROCEDURE buscar_media_por_cargo(IN jobTitle VARCHAR(50))
 BEGIN
-	SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial`
-	FROM employees AS e
-    INNER JOIN jobs AS j
-    ON e.JOB_ID = j.JOB_ID
-    WHERE j.JOB_TITLE = jobTitle;
+SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial`
+FROM employees AS e
+INNER JOIN jobs AS j
+ON e.JOB_ID = j.JOB_ID
+WHERE j.JOB_TITLE = jobTitle;
 END &&
 DELIMITER ;
