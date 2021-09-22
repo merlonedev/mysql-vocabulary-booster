b43981aac2109f303f8eeca89cc8a30173c1b534
johnpligeiro <johnpligeiro@gmail.com>
2021-09-22 04:35:48 -0300

    Desafio 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,11 +1,10 @@
-USE hr;
 SELECT
   CONCAT(e.first_name, ' ', e.last_name) AS `Nome completo`,
   DATE_FORMAT(j.start_date, '%d/%m/%Y') AS `Data de início`,
   DATE_FORMAT(j.end_date, '%d/%m/%Y') AS `Data de rescisão`,
   ROUND(DATEDIFF(j.end_date, j.start_date) / 365, 2) AS `Anos trabalhados`
 FROM
-  employees AS e
+  hr.employees AS e
     JOIN
-  job_history AS j ON e.employee_ID = j.employee_ID
+  hr.job_history AS j ON e.employee_ID = j.employee_ID
 ORDER BY `Nome completo`, `Anos Trabalhados`;
