3da408483d6000737e03429a0a539d8bd527c7e1
Eder Santos <3derms@gmail.com>
2021-09-22 17:14:07 -0300

    Exibe todas as pessoas clientes que possuem compatriotas, mostrando a quantidade de compatriotas para cada pessoa cliente

---
+++ desafio11.sql
@@ -0,0 +1,11 @@
+SELECT 
+    c1.ContactName AS 'Nome',
+    c1.Country AS 'País',
+    COUNT(c1.Country) - 1 AS 'Número de compatriotas'
+FROM
+    w3schools.customers AS c1,
+    w3schools.customers AS c2
+WHERE
+    c1.Country = c2.Country
+GROUP BY c1.ContactName
+ORDER BY c1.Country;
