99b6529a7e34ee996d5d263530f5843318ee05b8
André Moreno <deco616@hotmail.com>
2021-09-21 21:46:28 -0300

    Desafio 6 AJUSTE

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,14 @@
 SELECT 
-	CONCAT(FIRST_NAME, ' ', LAST_NAME) AS 'Nome completo',
+    CONCAT(FIRST_NAME, ' ', LAST_NAME) AS 'Nome completo',
     JOB_TITLE AS 'Cargo',
-    START_DATE	 AS 'Data de início do cargo',
+    START_DATE AS 'Data de início do cargo',
     DEPARTMENT_NAME AS 'Departamento'
-FROM hr.job_history AS jh
-	INNER JOIN hr.employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
-	INNER JOIN hr.jobs AS j ON jh.JOB_ID = j.JOB_ID
-	INNER JOIN hr.departments AS d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
-ORDER BY `Nome completo` DESC, `Cargo`; 
+FROM
+    hr.job_history AS jh
+        INNER JOIN
+    hr.employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+        INNER JOIN
+    hr.jobs AS j ON jh.JOB_ID = j.JOB_ID
+        INNER JOIN
+    hr.departments AS d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC , `Cargo`; 
