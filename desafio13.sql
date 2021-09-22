03763ce2fd4d87d160e4b5ed8a1560e28907950b
Daniel Ceci <danielceci96@gmail.com>
2021-09-22 13:30:51 -0300

    tentando corrigir erro lint req 13

--- desafio13.sql
+++ desafio13.sql
@@ -3,4 +3,4 @@ FROM w3schools.products p INNER JOIN w3schools.order_details o
 ON p.ProductID = o.ProductID
 WHERE o.Quantity > 80
 ORDER BY ProductName ASC;
- -- quero nome do produto e seu preço(tabela products) somente onde a quantidade >80(aí é tabela order_details.
+-- quero nome do produto e seu preço(tabela products) somente onde a quantidade >80(aí é tabela order_details.
