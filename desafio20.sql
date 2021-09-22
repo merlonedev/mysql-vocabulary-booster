aa3db93b62c0534d084685d9f51d0260eaf0103f
pedro21medeiros <pedro21medeiros@gmail.com>
2021-09-22 18:22:12 -0300

    Desafio 20

---
+++ desafio20.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(50))
+BEGIN
+SELECT
+CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
+d.department_name AS 'Departamento',
+j.job_title AS 'Cargo'
+FROM hr.job_history AS jh
+JOIN hr.employees AS e ON jh.employee_id = e.employee_id
+JOIN hr.departments AS d ON jh.department_id = d.department_id
+JOIN hr.jobs AS j ON jh.job_id = j.job_id
+WHERE e.email = email
+ORDER BY d.department_name, j.job_title;
+END $$
+DELIMITER ;
+
+CALL exibir_historico_completo_por_funcionario('NKOCHHAR');
