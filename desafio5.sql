2cac48393c602e7e737b1ef0b3b7065c4affca1d
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-09-21 22:26:01 -0300

    code climate fix

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 SELECT 
-	j.JOB_TITLE AS `Cargo`,
-  j.MAX_SALARY - j.MIN_SALARY AS `Variação Salarial`,
+    j.JOB_TITLE AS `Cargo`,
+    j.MAX_SALARY - j.MIN_SALARY AS `Variação Salarial`,
   ROUND(j.MAX_SALARY/12, 2) AS `Média máxima mensal`,
   ROUND(j.MIN_SALARY/12, 2) AS `Média mínima mensal`
 FROM
-  hr.jobs j
+    hr.jobs j
 ORDER BY `Variação Salarial`, `Cargo`;
