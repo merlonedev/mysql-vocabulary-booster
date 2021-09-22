a1d4b2ce63747a95ea2522759c2ec1a31d680983
Abner <gedeaoabner@gmail.com>
2021-09-22 01:32:22 -0300

    Desafio 6: remove tabs na formataçao

--- desafio6.sql
+++ desafio6.sql
@@ -6,8 +6,8 @@ SELECT
 FROM
     hr.employees AS e
         JOIN
-	  hr.job_history as jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
-		    JOIN
+    hr.job_history as jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+        JOIN
     hr.jobs AS j ON jh.JOB_ID = j.JOB_ID
         JOIN
     hr.departments d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
