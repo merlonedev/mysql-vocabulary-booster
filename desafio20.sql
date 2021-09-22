729e88cef8324283e7c6746c3ee28ff2d9f274e2
AndreHammel <hammel.engineer@gmail.com>
2021-09-21 21:01:11 -0300

    Update desafio20.sql

--- desafio20.sql
+++ desafio20.sql
@@ -3,29 +3,24 @@ DELIMITER $$
 CREATE PROCEDURE
 	exibir_historico_completo_por_funcionario(email varchar(30))
 BEGIN
-	select
-	concat(emp.FIRST_NAME, ' ', emp.LAST_NAME) as `Nome completo`,
-    d.DEPARTMENT_NAME as Departamento,
-    j.JOB_TITLE as Cargo
-	from
-		employees as emp
-	join
-		job_history as jh
-	on
-		emp.EMPLOYEE_ID = jh.EMPLOYEE_ID
-	join
-		departments as d
-	on
-		jh.DEPARTMENT_ID = d.DEPARTMENT_ID
-	join
-		jobs as j
-	on
-		j.JOB_ID = jh.JOB_ID
-	WHERE 
-		emp.EMAIL =  email
-	ORDER BY Cargo;
+	SELECT 
+    CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS `Nome completo`,
+    d.DEPARTMENT_NAME AS Departamento,
+    j.JOB_TITLE AS Cargo
+FROM
+    employees AS emp
+        JOIN
+    job_history AS jh ON emp.EMPLOYEE_ID = jh.EMPLOYEE_ID
+        JOIN
+    departments AS d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
+        JOIN
+    jobs AS j ON j.JOB_ID = jh.JOB_ID
+WHERE
+    emp.EMAIL = email
+ORDER BY Cargo;
 END
 
 $$
 
 DELIMITER ;
+
