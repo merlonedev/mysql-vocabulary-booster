6e992474cd484f1035f936da64b4f6a00ec19bb1
Milton Castro <castro.milton07@gmail.com>
2021-09-22 00:29:46 -0300

    ADD-ADJUST-CHALLENGE-19-OF-TABS_CodeClimate

--- desafio19.sql
+++ desafio19.sql
@@ -4,12 +4,12 @@ DELIMITER $$
 CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT)
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE quantity INT;
-    SELECT
-	COUNT(EMPLOYEE_ID)
-	FROM hr.employees
-	WHERE DATE_FORMAT(HIRE_DATE, '%m/%Y') LIKE CONCAT('%', mes,'/', ano) INTO quantity;
-    RETURN quantity;
+DECLARE quantity INT;
+SELECT
+COUNT(EMPLOYEE_ID)
+FROM hr.employees
+WHERE DATE_FORMAT(HIRE_DATE, '%m/%Y') LIKE CONCAT('%', mes,'/', ano) INTO quantity;
+RETURN quantity;
 END $$
 
 DELIMITER ;
