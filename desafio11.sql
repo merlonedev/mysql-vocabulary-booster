8de89171ee9ff93edfb5982104cda7df635cb9b4
André Moreno <deco616@hotmail.com>
2021-09-22 13:42:17 -0300

    Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+SELECT 
+    c1.ContactName AS 'Nome',
+    c1.Country AS 'País',
+    COUNT(*) AS 'Número de compatriotas'
+FROM
+    w3schools.customers AS c1
+        INNER JOIN
+    w3schools.customers AS c2 ON c1.Country = c2.Country
+        AND c1.ContactName <> c2.ContactName
+GROUP BY c1.ContactName
+ORDER BY c1.ContactName;
+    
