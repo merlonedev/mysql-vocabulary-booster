fa4d46e97c33470e86507669886d649902344c07
Milton Castro <castro.milton07@gmail.com>
2021-09-21 23:31:20 -0300

    ADD-ADJUST-CHALLENGE-17-OF-TABS_CodeClimate

--- desafio17.sql
+++ desafio17.sql
@@ -2,9 +2,9 @@ USE w3schools;
 
 DELIMITER $$
 CREATE TRIGGER trigger_order_insert
-	BEFORE INSERT ON w3schools.orders
-    FOR EACH ROW
+BEFORE INSERT ON w3schools.orders
+FOR EACH ROW
 BEGIN
-	SET NEW.OrderDate = NOW();
+SET NEW.OrderDate = NOW();
 END $$
 DELIMITER ;
