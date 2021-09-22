003fbdea5eb2443a8f9f400ae94359934edd3ecb
Luciano Lanes <llaneslopes@gmail.com>
2021-09-22 19:33:14 -0300

    FIX Desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,12 @@
 SELECT 
     t1.ContactName AS Nome,
-    t1.Country AS `País`,
+    t1.Country AS País,
     (COUNT(t1.Country) - 1) AS `Número de compatriotas`
 FROM
     w3schools.customers AS t1,
     w3schools.customers AS t2
 WHERE
     t1.country = t2.country
-GROUP BY `País`, Nome
+GROUP BY País , Nome
+HAVING `Número de compatriotas` > 1
 ORDER BY Nome;
