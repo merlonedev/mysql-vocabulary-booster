d514a40685207385ab1bff21df47f847798bf7a4
André Moreno <deco616@hotmail.com>
2021-09-21 23:11:27 -0300

    Desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,12 @@
+SELECT
+	ProductName AS 'Produto',
+    MIN(Quantity) AS 'Mínima',
+    MAX(Quantity) AS 'Máxima',
+    ROUND(AVG(Quantity), 2) AS 'Média'
+FROM 
+	w3schools.order_details AS od
+		INNER JOIN
+	w3schools.products AS p ON od.ProductID = p.ProductID
+GROUP BY ProductName
+HAVING Média > 20
+ORDER BY Média, ProductName;
