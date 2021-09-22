f117369556bed6fee2aec3478270203fc5be675f
RyanLaiber <ryanlaiber@gmail.com>
2021-09-22 10:11:21 -0300

    corrige estilo do req 16

--- desafio16.sql
+++ desafio16.sql
@@ -4,18 +4,18 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE total_empregos INT;
-	SELECT 
-		COUNT(jh.EMPLOYEE_ID)
-	FROM
-		job_history AS jh
-	WHERE
-		EMPLOYEE_ID IN (SELECT 
-				EMPLOYEE_ID
-			FROM
-				employees AS e
-			WHERE
-				e.EMAIL = email) INTO total_empregos;
-	RETURN total_empregos;
+DECLARE total_empregos INT;
+SELECT 
+	COUNT(jh.EMPLOYEE_ID)
+FROM
+	job_history AS jh
+WHERE
+	EMPLOYEE_ID IN (SELECT 
+			EMPLOYEE_ID
+		FROM
+			employees AS e
+		WHERE
+			e.EMAIL = email) INTO total_empregos;
+RETURN total_empregos;
 END $$
 DELIMITER ;
