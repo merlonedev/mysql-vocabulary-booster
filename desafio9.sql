207e0cf5e4d2ba99046fead315ec6580c445bb2c
Eder Santos <3derms@gmail.com>
2021-09-22 10:24:34 -0300

    Exibe todos as pessoas funcionárias que já realizaram algum pedido, mostrando também seu total de pedidos feitos

---
+++ desafio9.sql
@@ -0,0 +1,11 @@
+SELECT 
+    CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
+    od.Quantity AS 'Total de pedidos'
+FROM
+    w3schools.employees AS e
+        INNER JOIN
+    w3schools.order_details AS od
+        INNER JOIN
+    w3schools.orders AS o ON od.OrderID = o.OrderID
+        AND e.EmployeeID = o.EmployeeID
+ORDER BY od.Quantity;
