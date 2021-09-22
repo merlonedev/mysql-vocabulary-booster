7c71f0dd61022d6c93e0bb244e71401de49e30ab
AndreHammel <hammel.engineer@gmail.com>
2021-09-21 21:05:11 -0300

    Update desafio6.sql

--- desafio6.sql
+++ desafio6.sql
@@ -1,20 +1,14 @@
 SELECT 
-	CONCAT(EMP.FIRST_NAME, ' ', EMP.LAST_NAME) AS `Nome completo`,
-	J.JOB_TITLE AS Cargo,
+    CONCAT(EMP.FIRST_NAME, ' ', EMP.LAST_NAME) AS `Nome completo`,
+    J.JOB_TITLE AS Cargo,
     JH.START_DATE AS `Data de início do cargo`,
     DEP.DEPARTMENT_NAME AS Departamento
 FROM
-	job_history as JH
-JOIN
-    employees AS EMP
-ON
-	 JH.EMPLOYEE_ID = EMP.EMPLOYEE_ID
-JOIN
-		jobs AS J
-ON
-	JH.JOB_ID = J.JOB_ID
-JOIN
-	departments AS DEP
-ON 
-	JH.DEPARTMENT_ID = DEP.DEPARTMENT_ID
-ORDER BY `Nome completo` DESC, Cargo ASC;
+    job_history AS JH
+        JOIN
+    employees AS EMP ON JH.EMPLOYEE_ID = EMP.EMPLOYEE_ID
+        JOIN
+    jobs AS J ON JH.JOB_ID = J.JOB_ID
+        JOIN
+    departments AS DEP ON JH.DEPARTMENT_ID = DEP.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC , Cargo ASC;
