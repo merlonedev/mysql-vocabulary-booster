8d99d96adcdc9a7b7268f707be8d47691bdc2cdb
Erick Silva <erick.silva6653@gmail.com>
2021-09-22 00:29:50 -0300

    desafio 18

---
+++ desafio19.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes TINYINT, ano INT)
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE quantity INT;
+	SELECT COUNT(*) FROM hr.employees
+    WHERE HIRE_DATE LIKE CONCAT(ano, '-_', mes, '%')
+    INTO quantity;
+    RETURN quantity;
+END $$
+
+DELIMITER ;
