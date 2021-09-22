360a9b94191e4fbb1f79004311509ecef4e4e2ea
tiagoornelas <advtiagoornelas@gmail.com>
2021-09-22 16:33:05 -0300

    Pretende resolver o requisito 15

---
+++ desafio15.sql
@@ -0,0 +1,10 @@
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo (IN JOB VARCHAR(250))
+BEGIN
+SELECT ROUND(AVG(e.SALARY),2) AS `Média salarial`
+FROM hr.employees e
+RIGHT JOIN hr.jobs j
+ON j.JOB_ID = e.JOB_ID
+GROUP BY j.JOB_TITLE
+HAVING j.JOB_TITLE = @JOB;
+END$$
