a99fe717ef2cbea9a4e122d4f7a2797b74d18c58
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-09-22 11:12:02 -0300

    requisito 9

---
+++ desafio9.sql
@@ -0,0 +1,12 @@
+SELECT 
+    (SELECT 
+            CONCAT(e.firstName, ' ', e.lastName)
+        FROM
+            employees AS e
+        WHERE
+            e.employeeID = o.employeeID) AS 'Nome completo',
+    COUNT(EmployeeID) AS 'Total de pedidos'
+FROM
+    orders AS o
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos` ASC;
