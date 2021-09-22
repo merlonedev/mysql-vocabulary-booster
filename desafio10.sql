62dd7eecf14fd7cda0ceda6b208c0e13d04513f1
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-09-21 22:35:04 -0300

    code climate fix

--- desafio10.sql
+++ desafio10.sql
@@ -6,7 +6,8 @@ SELECT
 FROM
 	w3schools.order_details od
 INNER JOIN
-  w3schools.products p ON od.ProductID = p.ProductID
+  w3schools.products p
+ON od.ProductID = p.ProductID
 GROUP BY od.ProductID
 HAVING `Média` > 20
 ORDER BY `Média`, `Produto`;
