3104b07201c9ec0f55b557cbed173fd929c41285
Ramon Paschoal <rpaschoal90@gmail.com>
2021-09-22 12:47:25 -0300

    Requisito 15 e 16

---
+++ desafio16.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE qtd_jobs INT;
+	SELECT COUNT(jh.EMPLOYEE_ID) FROM job_history AS jh
+    INNER JOIN employees AS e
+    ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+    WHERE e.EMAIL = email INTO qtd_jobs;
+    RETURN qtd_jobs;
+END $$
+
+DELIMITER ;
