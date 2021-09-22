35d1cffafabd5ec6272936951458e518206747cf
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-09-22 09:55:19 -0300

    Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,15 @@
+--11 - Exibe todas as pessoas clientes **que possuem compatriotas**, 
+-- mostrando a quantidade de compatriotas para cada pessoa cliente
+
+SELECT 
+    A.ContactName AS 'Nome',
+    A.Country AS 'País',
+    COUNT(*) AS 'Número de compatriotas'
+FROM
+    customers A,
+    customers B
+WHERE
+    A.Country = B.Country
+        AND A.CustomerID <> B.CustomerID
+GROUP BY A.ContactName
+ORDER BY A.ContactName ASC;
