0aad374da52489788621fa0dd470b5498594783c
Patrick Dack <patrickdack89@gmail.com>
2021-09-22 03:55:25 -0300

    code climate

--- desafio15.sql
+++ desafio15.sql
@@ -1,9 +1,9 @@
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
 BEGIN
-	  SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
+	  SELECT ROUND(AVG(SALARY), 2) AS 'Media salarial'
     FROM employees AS e
-		  INNER JOIN
+		INNER JOIN
         jobs AS j ON e.JOB_ID = j.JOB_ID
         WHERE j.JOB_TITLE LIKE CONCAT('%', cargo, '%');
 END $$
