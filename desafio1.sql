9e123473f19ae2371e42bc6481361eec0f599f0a
Nalu <aluziapantoja@gmail.com>
2021-09-22 11:57:26 -0300

    Testando

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,3 @@
 USE hr;
-SELECT COUNTRY_NAME AS 'País', IF(REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão' FROM countries
+SELECT COUNTRY_NAME AS 'País', IF(REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão' FROM hr.countries
 ORDER BY 'País';
