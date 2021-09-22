95a5224e0fafd2ae8708d2c6226eeab38f9f2a4e
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-09-21 22:09:42 -0300

    code climate fix

--- desafio15.sql
+++ desafio15.sql
@@ -1,14 +1,14 @@
 DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(job_name VARCHAR(100))
-BEGIN
-SELECT
-	ROUND(AVG(e.SALARY), 2) AS `Média salarial`
-FROM
-	hr.employees e,
-	hr.jobs j
-WHERE j.JOB_TITLE = job_name
-	AND e.JOB_ID = j.JOB_ID;
-END $$
+  BEGIN
+    SELECT
+      ROUND(AVG(e.SALARY), 2) AS `Média salarial`
+    FROM
+      hr.employees e,
+      hr.jobs j
+    WHERE j.JOB_TITLE = job_name
+      AND e.JOB_ID = j.JOB_ID;
+  END $$
 
 DELIMITER ;
