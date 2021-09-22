b839c6fe0902b3ada307da7f1d6c2dde817c8892
camila-mp <ca.malvp@gmail.com>
2021-09-22 11:03:28 -0300

    corrige desafio 8 e resolve desafio 9

--- desafio8.sql
+++ desafio8.sql
@@ -10,4 +10,5 @@ SELECT c.ContactName AS `Nome de contato`, s.ShipperName AS `Empresa que fez o e
 FROM w3schools.customers AS c
 INNER JOIN w3schools.orders AS o ON c.CustomerID = o.CustomerID
 INNER JOIN w3schools.shippers AS s ON o.ShipperID = s.ShipperID
+WHERE s.ShipperName IN ('Speedy Express', 'United Package')
 ORDER BY c.ContactName ASC, s.ShipperName ASC, o.OrderDate ASC;
--- desafio9.sql
+++ desafio9.sql
@@ -3,3 +3,8 @@
 --   *A primeira deve possuir o alias "Nome completo" e exibir o nome completo da pessoa funcionária (não se esqueça do espaço entre o nome e o sobrenome).
 --   *A segunda deve possuir o alias "Total de pedidos" e exibir a quantidade total de pedidos feitos pela pessoa.
 -- Ordene seus resultados pelo total de pedidos em ordem crescente.
+SELECT CONCAT(e.LastName, ' ', e.FirstName) AS `Nome completo`, COUNT(o.EmployeeID) AS `Total de pedidos`
+FROM w3schools.employees AS e
+INNER JOIN w3schools.orders as o ON e.EmployeeID = o.EmployeeID
+GROUP BY CONCAT(e.LastName, ' ', e.FirstName)
+ORDER BY COUNT(o.EmployeeID) ASC;
