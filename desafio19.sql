2fc8891dd1f7330bb6f5b620f34af7a9c3fc6ef5
RyanLaiber <ryanlaiber@gmail.com>
2021-09-22 11:44:13 -0300

    add req 19

---
+++ desafio19.sql
@@ -0,0 +1,17 @@
+USE hr;
+
+DELIMITER $$
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE pessoas_contratadas INT;
+SELECT 
+COUNT(*)
+FROM
+employees
+WHERE
+MONTH(HIRE_DATE) = mes
+AND YEAR(HIRE_DATE) = ano INTO pessoas_contratadas;
+RETURN pessoas_contratadas;
+END $$
+DELIMITER ;
