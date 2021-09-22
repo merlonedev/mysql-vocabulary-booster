5d537385a049cac877ed03e13d866620b85c3cc0
Erick Silva <erick.silva6653@gmail.com>
2021-09-22 00:32:42 -0300

    code climate

--- desafio16.sql
+++ desafio16.sql
@@ -3,13 +3,13 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(40))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE JobQuantity TINYINT;
-	SELECT COUNT(emp.FIRST_NAME)
+DECLARE JobQuantity TINYINT;
+SELECT COUNT(emp.FIRST_NAME)
     FROM hr.job_history AS jh
-	JOIN 
-		hr.employees as emp ON emp.EMPLOYEE_ID = jh.EMPLOYEE_ID
-	WHERE emp.EMAIL = 'NKOCHHAR'
-	GROUP BY jh.EMPLOYEE_ID
+JOIN 
+	hr.employees as emp ON emp.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE emp.EMAIL = 'NKOCHHAR'
+GROUP BY jh.EMPLOYEE_ID
     INTO JobQuantity;
     RETURN JobQuantity;
 END $$
