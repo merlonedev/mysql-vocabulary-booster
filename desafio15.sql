bc365c0d6be26e6363f61ab6575f12e0baaa9f07
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2021-09-22 20:34:50 -0300

    Resolve desafio 15

--- desafio15.sql
+++ desafio15.sql
@@ -2,12 +2,11 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
 BEGIN
-	SELECT 
-	ROUND((SUM(e.SALARY) / COUNT(e.SALARY)), 2) AS `Média salarial`
-	FROM hr.employees AS e
-	INNER JOIN hr.jobs AS j
-	WHERE e.JOB_ID = j.JOB_ID
-	AND j.JOB_TITLE LIKE CONCAT('%', cargo, '%');
+SELECT 
+ROUND((SUM(e.SALARY) / COUNT(e.SALARY)), 2) AS `Média salarial`
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+WHERE j.JOB_TITLE LIKE CONCAT('%', cargo, '%');
 END $$
 
 DELIMITER ;
