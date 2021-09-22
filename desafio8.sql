384fc6aad7ea3d77d275f75c3fef1ae043f26913
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-09-21 22:36:06 -0300

    code climate fix

--- desafio8.sql
+++ desafio8.sql
@@ -1,14 +1,14 @@
 SELECT
-  c.ContactName AS `Nome de contato`,
-  s.ShipperName AS `Empresa que fez o envio`,
-  o.OrderDate AS `Data do pedido`
+    c.ContactName AS `Nome de contato`,
+    s.ShipperName AS `Empresa que fez o envio`,
+    o.OrderDate AS `Data do pedido`
 FROM
-	  w3schools.orders o
+	w3schools.orders o
 INNER JOIN
-	  w3schools.customers c
+	w3schools.customers c
 ON o.CustomerID = c.CustomerID
 INNER JOIN
-	  w3schools.shippers s
+	w3schools.shippers s
 ON o.ShipperID = s.ShipperID
 WHERE s.ShipperName
 IN ('Speedy Express', 'United Package')
