1fc0d0bea7db2c3fef9968904829d89318917607
tiagoornelas <advtiagoornelas@gmail.com>
2021-09-21 22:08:12 -0300

    Pretende resolver o requisito 11 [2]

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,6 @@
 SELECT ContactName AS `Nome`,
 Country AS `País`,
-(SELECT COUNT(Country)
+(SELECT COUNT(Country) FROM w3schools.customers WHERE Country = `País`) - 1 AS `Número de compatriotas`
 FROM w3schools.customers
-WHERE Country = `País`) - 1 AS `Número de compatriotas`
-FROM w3schools.customers
-ORDER BY ContactName;
+HAVING `Número de compatriotas` > 0
+ORDER BY `Nome`;
