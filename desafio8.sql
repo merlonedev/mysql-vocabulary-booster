6579bfa585960fa2c955c5e5259c2ca671b344fa
tiagoornelas <advtiagoornelas@gmail.com>
2021-09-21 21:19:43 -0300

    Pretende resolver o requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+SELECT C.ContactName AS `Nome do contato`,
+S.ShipperName AS `Empresa que fez o envio`,
+O.OrderDate AS `Data do pedido`
+FROM w3schools.orders O
+RIGHT JOIN w3schools.customers C
+ON O.CustomerID = C.CustomerID
+RIGHT JOIN w3schools.shippers S
+ON O.ShipperID = S.ShipperID
+WHERE O.ShipperID IN (1, 2)
+ORDER BY `Nome do contato`, `Empresa que fez o envio`, `Data do pedido`;
