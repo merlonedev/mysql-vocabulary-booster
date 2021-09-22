e0201aafa43d967dcbae55839b03e9e3445289a6
jorda <jordan.rachid@gmail.com>
2021-09-22 17:14:10 -0300

    desafio 7 e 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,10 @@
 SELECT 
-  c.CustomerName AS "Nome de contato",
+  c.ContactName AS "Nome de contato",
   s.ShipperName AS "Empresa que fez o envio",
   o.OrderDate AS "Data do pedido"
- FROM w3schools.customers AS c
- INNER JOIN w3schools.orders AS o ON c.CustomerID = o.CustomerID
- INNER JOIN w3schools.shippers AS s ON s.ShipperID = o.ShipperID
- WHERE s.ShipperName ="United Package" OR s.ShipperName ="Speedy Express"
- ORDER BY c.CustomerName, s.ShipperName, o.OrderDate;
+FROM w3schools.customers AS c
+INNER JOIN w3schools.orders AS o ON c.CustomerID = o.CustomerID
+INNER JOIN w3schools.shippers AS s ON s.ShipperID = o.ShipperID
+WHERE s.ShipperName ="United Package" OR s.ShipperName ="Speedy Express"
+ORDER BY c.CustomerName, s.ShipperName, o.OrderDate;
+ 
