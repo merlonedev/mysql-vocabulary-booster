c2ae14a80ccbdc9efb04a3b26a693e5747884747
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-09-21 22:21:15 -0300

    conclui desafio13 - Exibe todos produtos que já tiveram um pedido associado requerindo uma quantidade desse produto maior que 80

---
+++ desafio13.sql
@@ -0,0 +1,13 @@
+SELECT
+  P.ProductName AS `Produto`,
+  P.Price AS `Preço`
+FROM
+  w3schools.products AS P
+JOIN
+  w3schools.order_details AS OD
+ON
+  P.ProductID = OD.ProductID
+WHERE
+  OD.Quantity > 80
+ORDER BY
+  `Produto`;
