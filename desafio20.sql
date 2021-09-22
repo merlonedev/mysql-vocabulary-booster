0e4bb69d1271573e2fbb59e9473600ca0decf4b9
alestori7@gmail.com <alestori7@gmail.com>
2021-09-22 01:22:15 -0300

    fix: code climate Desafio 20 [2]

--- desafio20.sql
+++ desafio20.sql
@@ -7,19 +7,19 @@ DELIMITER $$
 CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(50))
 BEGIN
 SELECT 
-    CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome completo',
-    dep.DEPARTMENT_NAME AS 'Departamento',
-    job.JOB_TITLE AS 'Cargo'
+CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome completo',
+dep.DEPARTMENT_NAME AS 'Departamento',
+job.JOB_TITLE AS 'Cargo'
 FROM
 hr.job_history jh
-	JOIN
+JOIN
 hr.employees emp ON emp.EMPLOYEE_ID = jh.EMPLOYEE_ID
-	JOIN
+JOIN
 hr.departments dep ON dep.DEPARTMENT_ID = jh.DEPARTMENT_ID
-	JOIN
+JOIN
 hr.jobs job ON job.JOB_ID = jh.JOB_ID
 WHERE
-    emp.EMAIL LIKE concat(email, '%')
+emp.EMAIL LIKE concat(email, '%')
 ORDER BY 2 , 3;
 END $$
 
