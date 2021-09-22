876618b0c6a999763d9ae6826c0c18414d245218
RyanLaiber <ryanlaiber@gmail.com>
2021-09-22 10:13:42 -0300

    corrige estilo do req 16

--- desafio16.sql
+++ desafio16.sql
@@ -6,16 +6,16 @@ RETURNS INT READS SQL DATA
 BEGIN
 DECLARE total_empregos INT;
 SELECT 
-	COUNT(jh.EMPLOYEE_ID)
+COUNT(jh.EMPLOYEE_ID)
 FROM
-	job_history AS jh
+job_history AS jh
 WHERE
-	EMPLOYEE_ID IN (SELECT 
-			EMPLOYEE_ID
-		FROM
-			employees AS e
-		WHERE
-			e.EMAIL = email) INTO total_empregos;
+EMPLOYEE_ID IN (SELECT 
+EMPLOYEE_ID
+FROM
+employees AS e
+WHERE
+e.EMAIL = email) INTO total_empregos;
 RETURN total_empregos;
 END $$
 DELIMITER ;
