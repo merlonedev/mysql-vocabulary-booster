042c52e2b4ed552842834dab5149ef14df70474c
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-09-22 09:38:05 -0300

    Requisito 15.5

--- desafio15.sql
+++ desafio15.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 
 Create Procedure buscar_media_por_cargo (in cargo VARCHAR(50))
 BEGIN
-select ROUND(AVG(e.SALARY), 00) AS 'Média salarial' from hr.employees as e
+select ROUND(AVG(SALARY), 2) AS 'Média salarial' from hr.employees as e
 INNER JOIN hr.jobs as j on j.JOB_ID = e.JOB_ID
 WHERE JOB_TITLE = cargo;
 END $$
