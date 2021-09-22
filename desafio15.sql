a59fb494182afd578049c51421884021f3d4f3ea
Patrick Dack <patrickdack89@gmail.com>
2021-09-22 03:52:06 -0300

    Corrige code climate

--- desafio15.sql
+++ desafio15.sql
@@ -1,9 +1,9 @@
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
 BEGIN
-	SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
+	  SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
     FROM employees AS e
-		INNER JOIN
+		  INNER JOIN
         jobs AS j ON e.JOB_ID = j.JOB_ID
         WHERE j.JOB_TITLE LIKE CONCAT('%', cargo, '%');
 END $$
--- desafio16.sql
+++ desafio16.sql
@@ -2,15 +2,15 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario (email VARCHAR(100))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE result INT;
-	SELECT
-    COUNT(j.JOB_ID)
+    DECLARE result INT;
+    SELECT
+      COUNT(j.JOB_ID)
 FROM
-    job_history AS j
-        INNER JOIN
-    employees AS e ON e.EMPLOYEE_ID = j.EMPLOYEE_ID
+      job_history AS j
+          INNER JOIN
+      employees AS e ON e.EMPLOYEE_ID = j.EMPLOYEE_ID
 WHERE
-    e.EMAIL LIKE CONCAT('%', email, '%') INTO result;
+      e.EMAIL LIKE CONCAT('%', email, '%') INTO result;
 RETURN result;
 END $$
 DELIMITER ;
