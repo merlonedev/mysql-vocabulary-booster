a658c25765b5f4b6ab4c80a17d4a45eb11b0c2dc
ArturAlvaro <artur.alvaro73@gmail.com>
2021-09-22 11:17:39 -0300

    [Requisito 15]

---
+++ desafio15.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargoNome VARCHAR(100))
+BEGIN
+	SELECT 
+		ROUND(AVG(e.salary), 2) 'Média salarial'
+	FROM
+		hr.jobs j
+			INNER JOIN
+		hr.employees e ON e.JOB_ID = j.JOB_ID
+	WHERE
+		j.JOB_TITLE = cargoNome;
+END $$
+
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');
