3e55c9eda8c73b841941d9f3bcecff2cd45f0249
Eder Santos <3derms@gmail.com>
2021-09-22 12:49:43 -0300

    Corrige total de pedidos

--- desafio9.sql
+++ desafio9.sql
@@ -1,12 +1,9 @@
 SELECT 
     CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
-    od.Quantity AS 'Total de pedidos'
+    COUNT(o.OrderID) AS 'Total de pedidos'
 FROM
     w3schools.employees AS e
         INNER JOIN
-    w3schools.order_details AS od
-        INNER JOIN
-    w3schools.orders AS o ON od.OrderID = o.OrderID
-        AND e.EmployeeID = o.EmployeeID
+    w3schools.orders AS o ON e.EmployeeID = o.EmployeeID
 GROUP BY CONCAT(e.FirstName, ' ', e.LastName)
-ORDER BY od.Quantity;
+ORDER BY COUNT(o.OrderID);
