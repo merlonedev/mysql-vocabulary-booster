2e2719a04e2091aa30937f59cc9e9ba80ff29af7
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-09-21 22:33:57 -0300

    code climate fix

--- desafio13.sql
+++ desafio13.sql
@@ -1,10 +1,10 @@
 SELECT
-  p.ProductName AS `Produto`,
-  p.Price AS `Preço`
+    p.ProductName AS `Produto`,
+    p.Price AS `Preço`
 FROM
-	  w3schools.products p,
-    w3schools.order_details od
+  w3schools.products p,
+  w3schools.order_details od
 WHERE
-	  p.ProductID = od.ProductID
+  p.ProductID = od.ProductID
 AND od.Quantity > 80
 ORDER BY `Produto`;
