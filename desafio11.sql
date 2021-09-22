01238ebae2d6d16c11567bed4c226d7c0fa8cb71
Josimar Souza <contact.josimarsouza@gmail.com>
2021-09-22 11:52:25 -0300

    Corrigi o desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,7 @@
 SELECT
 ContactName AS `Nome`,
 Country AS `País`,
-(SELECT COUNT(Country) - 1 FROM w3schools.customers WHERE Country = `País`) AS `Número de compatriotas`
+(SELECT COUNT(CustomerID) - 1 FROM w3schools.customers GROUP BY Country HAVING Country = `País`) AS `Número de compatriotas`
 FROM w3schools.customers
+HAVING `Número de compatriotas` > 0
 ORDER BY `Nome`;
