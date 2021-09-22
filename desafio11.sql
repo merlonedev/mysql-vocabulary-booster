5b8dd8ba5dac574cbebacd95c0cede1f7e1a810c
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-09-22 10:13:42 -0300

    Tentando resover erro no desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,3 @@
--- 11 - Exibe todas as pessoas clientes que possuem compatriotas, 
--- mostrando a quantidade de compatriotas para cada pessoa cliente
-
 SELECT 
     A.ContactName AS 'Nome',
     A.Country AS 'País',
