c9a48e0dcb8e29171a1ec19a87472d45a5342954
AndreHammel <hammel.engineer@gmail.com>
2021-09-21 21:59:36 -0300

    Update desafio8.sql

--- desafio8.sql
+++ desafio8.sql
@@ -1,13 +1,14 @@
 SELECT 
-c.ContactName AS 'Nome de contato',
-s.ShipperName AS 'Empresa que fez o envio',
-o.OrderDate AS 'Data do pedido'
+    c.ContactName AS 'Nome de contato',
+    s.ShipperName AS 'Empresa que fez o envio',
+    o.OrderDate AS 'Data do pedido'
 FROM
-customers AS c
-JOIN
-orders AS o ON o.CustomerID = c.CustomerID
-JOIN
-shippers AS s ON o.ShipperID = s.ShipperID
+    w3schools.customers AS c
+        INNER JOIN
+    w3schools.orders AS o ON o.CustomerID = c.CustomerID
+        INNER JOIN
+    w3schools.shippers AS s ON s.ShipperID = o.ShipperID
 WHERE
-s.ShipperName IN ('Speedy Express' , 'United Package')
-ORDER BY c.ContactName , s.ShipperName;
+    s.ShipperName = 'Speedy Express'
+        OR s.ShipperName = 'United Package'
+ORDER BY c.ContactName , s.ShipperName , o.OrderDate;
