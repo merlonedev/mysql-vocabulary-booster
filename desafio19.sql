e76a763a605f07d50ae4a1ed1c1a0e10a609a221
alestori7@gmail.com <alestori7@gmail.com>
2021-09-22 01:05:14 -0300

    fix: code climate Desafio 19

--- desafio19.sql
+++ desafio19.sql
@@ -7,7 +7,7 @@ DELIMITER $$
 CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(hire_month INT, hire_year INT)
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE qty_emp INT;
+DECLARE qty_emp INT;
 SELECT 
     COUNT(*)
 FROM
