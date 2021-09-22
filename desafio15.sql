4f9e4a516f623d928971d534cdf95fb335f568c4
Patrick Dack <patrickdack89@gmail.com>
2021-09-22 03:26:19 -0300

    Requisito 15

---
+++ desafio15.sql
@@ -0,0 +1,10 @@
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
+BEGIN
+	SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
+    FROM employees AS e
+		INNER JOIN
+        jobs AS j ON e.JOB_ID = j.JOB_ID
+        WHERE j.JOB_TITLE LIKE CONCAT('%', cargo, '%');
+END $$
+DELIMITER ;
