6a2ee2f27e954a73dc37c5e46276f80847a407e7
Guilherme Cesconeto <cesconeto@gmail.com>
2021-09-22 09:58:48 -0300

    Fazendo a 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,11 @@
 SELECT 
-    COUNTRY_NAME AS País,
-    IF(REGION_ID = 1,
-        'incluído',
-        'não incluído') AS 'Status Inclusão'
+    JOB_TITLE AS 'Cargo',
+	CASE
+        WHEN MAX_SALARY < 10001 THEN 'Baixo'
+        WHEN MAX_SALARY < 20001 THEN 'Médio'
+        WHEN MAX_SALARY < 30001 THEN 'Alto'
+        ELSE 'Altíssimo'
+    END AS 'Nível'
 FROM
-    hr.countries
-ORDER BY País;
+    hr.jobs
+ORDER BY 'Cargo';
