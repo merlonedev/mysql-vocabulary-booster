983339419e0caeb05cecf677d9cc347c504b026c
Heder Waine <hederw@hotmail.com>
2021-09-22 20:01:02 -0300

    Desafio 1 correção3

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,5 @@
-SELECT C.COUNTRY_NAME AS `País`,
-IF(r.REGION_NAME = "Europe", "incluido", "não incluido") AS `Status Inclusão`
+SELECT COUNTRY_NAME AS `País`,
+IF(REGION_ID = 1, 'incluído', 'não incluído') AS `Status Inclusão`
 FROM 
-hr.countries AS C
-JOIN 
-hr.regions AS r ON r.REGION_ID = C.REGION_ID
-ORDER BY C.COUNTRY_NAME;
+hr.countries
+ORDER BY `País`;
