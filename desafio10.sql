6c11ddeccd7ddb2f51e18e04d5d422cd94081e7a
Zevid-Lucas <zevid.dev@outlook.com>
2021-09-22 01:33:45 -0300

    Done challenge10, use MIN, MAX, ROUND, AVG, INNER JOIN, GROUP BY, HAVING, ORDER BY

---
+++ desafio10.sql
@@ -0,0 +1,12 @@
+SELECT 
+  ProductName AS 'Produto',
+  MIN(Quantity) AS 'Mínima',
+  MAX(Quantity) AS 'Máxima',
+  ROUND(AVG(Quantity),2) AS 'Média' 
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS od
+ON 
+  p.ProductID = od.ProductID
+GROUP BY p.ProductID
+HAVING `Média`> 20
+ORDER BY `Média`, `Produto`;
