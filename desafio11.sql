a40ec007bde3ad50e4cd377d47fbb23b03002593
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-09-22 08:41:23 -0300

    desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+SELECT 
+    ContactName AS `Nome`,
+    Country AS `País`,
+    (
+		SELECT COUNT(*) FROM w3schools.customers WHERE Country = `País` AND ContactName <> `Nome`
+    ) AS `Número de compatriotas`
+FROM
+    w3schools.customers as cu
+WHERE EXISTS (
+	SELECT * FROM w3schools.customers WHERE Country = cu.Country AND CustomerID <> cu.CustomerID
+)
+ORDER BY ContactName ASC;
