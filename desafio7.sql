343164065d94df2154f189e6b1b424ed168c20dc
jorda <jordan.rachid@gmail.com>
2021-09-22 13:02:00 -0300

    desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,7 @@
+SELECT
+  UCASE(CONCAT (e.first_name," ", e.last_name)) AS  "Nome completo",
+   h.START_DATE AS "Data de início",
+   e.SALARY AS "Salário"
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS h ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
+ORDER BY e.FIRST_NAME, h.START_DATE;
