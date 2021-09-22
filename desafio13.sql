36288d1d6ec11c4581997c72463a192348ec0580
Daniel Ceci <danielceci96@gmail.com>
2021-09-22 13:27:59 -0300

    requisito 13

---
+++ desafio13.sql
@@ -0,0 +1,6 @@
+SELECT ProductName AS "Produto", Price AS "Preço"  
+FROM w3schools.products p INNER JOIN w3schools.order_details o
+ON p.ProductID = o.ProductID
+WHERE o.Quantity > 80
+ORDER BY ProductName ASC;
+ -- quero nome do produto e seu preço(tabela products) somente onde a quantidade >80(aí é tabela order_details.
