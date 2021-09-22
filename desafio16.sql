1da24ac5ff983d65c0b8bdc94d9adb3cac52f70d
tiagoornelas <advtiagoornelas@gmail.com>
2021-09-22 17:17:16 -0300

    Pretende resolver o requisito 16

---
+++ desafio16.sql
@@ -0,0 +1,8 @@
+USE hr;
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario (EMAIL VARCHAR(150))
+RETURNS CHAR(4) DETERMINISTIC
+RETURN (SELECT COUNT(*) FROM hr.job_history JH
+INNER JOIN hr.employees E
+ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+WHERE E.EMAIL = EMAIL);
