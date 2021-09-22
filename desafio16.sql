d228b97a78bf111269d043b050a0c9cb0f24e3a6
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-09-21 23:56:41 -0300

    requisito 16

---
+++ desafio16.sql
@@ -0,0 +1,13 @@
+USE hr;
+DELIMITER $$ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(
+  email VARCHAR(30)
+) RETURNS INT READS SQL DATA BEGIN DECLARE total_empregos INT;
+SELECT 
+  COUNT(*) 
+FROM 
+  hr.job_history AS jh 
+  INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID 
+WHERE 
+  e.EMAIL = email INTO total_empregos;
+RETURN total_empregos;
+END $$ DELIMITER;
