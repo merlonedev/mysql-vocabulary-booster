1087d79b947c5b302041e8defdcc740b4d8b0a1d
Renan Ferreira <renansantiago2013@hotmail.com>
2021-09-22 11:43:09 -0300

    Resolve os requisitos 15, 16 e 17(ajusted no code climate)

--- desafio15.sql
+++ desafio15.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
 BEGIN
-	SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
+SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
     FROM hr.employees AS e 
     INNER JOIN 
     hr.jobs AS j ON j.JOB_ID = e.JOB_ID
