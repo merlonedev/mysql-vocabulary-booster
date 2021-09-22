35b82ee172fc6c298ce10a1b9e64c9878f4c3681
Guilherme Cesconeto <cesconeto@gmail.com>
2021-09-22 10:54:50 -0300

    Fazendo a 4 FIX 2

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,6 @@
 SELECT 
   j.JOB_TITLE AS `Cargo`,
-  ROUND(AVG(e.SALARY), 2) AS `Média Salarial`,
+  ROUND(AVG(e.SALARY), 2) AS `Média salarial`,
   CASE
   WHEN ROUND(AVG(e.SALARY), 2) <= 5800 THEN 'Júnior'
   WHEN ROUND(AVG(e.SALARY), 2) <= 7500 THEN 'Pleno'
@@ -10,4 +10,4 @@ SELECT
 FROM hr.jobs AS j
 JOIN hr.employees AS e ON j.JOB_ID = e.JOB_ID
 GROUP BY `Cargo`
-ORDER BY `Média Salarial`, `Cargo`;
+ORDER BY `Média salarial`, `Cargo`;
