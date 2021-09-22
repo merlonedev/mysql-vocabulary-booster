4aa87254f03cb9b3de637fe8fb914c8073ca5a38
Elisa-Libanio <elisalibanio1@gmail.com>
2021-09-22 19:17:04 -0300

    oi

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,8 @@
-SELECT COUNTRY_NAME AS País  from hr.countries;
-
-SELECT COUNTRY_NAME AS País  from hr.countries 
-case 
-when 
+SELECT 
+  COUNTRY_NAME AS 'País',
+  IF(REGION_ID = 1,
+    'incluído',
+    'não incluído') AS 'Status Inclusão'
+FROM
+  hr.countries
+ORDER BY COUNTRY_NAME;
