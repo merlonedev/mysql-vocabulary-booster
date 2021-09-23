f401278e20b8c770f7b9ac539a6699a56e1cce1d
Eder Santos <3derms@gmail.com>
2021-09-22 21:21:00 -0300

    Corrige ISSUE TAB

--- desafio15.sql
+++ desafio15.sql
@@ -4,7 +4,7 @@ DELIMITER $$
 CREATE PROCEDURE buscar_mdia_por_cargo(IN cargo VARCHAR(30))
 BEGIN SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial' 
 FROM hr.employees AS e
-	INNER JOIN hr.jobs AS j ON j.JOB_ID = e.JOB_ID
+INNER JOIN hr.jobs AS j ON j.JOB_ID = e.JOB_ID
 WHERE j.JOB_TITLE LIKE (cargo);
 END $$
 
