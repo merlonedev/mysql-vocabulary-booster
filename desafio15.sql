15705bce178171a52572b5e365b3653a8e5ba275
Guilherme Cesconeto <cesconeto@gmail.com>
2021-09-23 11:58:24 -0300

    Fazendo a 15 FIX

--- desafio15.sql
+++ desafio15.sql
@@ -3,8 +3,11 @@ CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(64))
 BEGIN
   SELECT 
     ROUND(AVG(e.salary), 2) AS `Média salarial`
-  FROM hr.employees AS e
-  JOIN hr.jobs AS j ON j.JOB_ID = e.JOB_ID
-  WHERE j.JOB_TITLE = job
+  FROM 
+    hr.employees AS e
+  JOIN 
+    hr.jobs AS j 
+  ON j.JOB_ID = e.JOB_ID
+  WHERE j.JOB_TITLE = job;
 END $$
 DELIMITER ;
