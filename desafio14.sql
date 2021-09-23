6ded375080c2a81b803cf98c571a5744c2448bf0
André Moreno <deco616@hotmail.com>
2021-09-23 04:48:17 -0300

    Desafio 13 e 14

---
+++ desafio14.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT
+    *
+FROM
+    ((SELECT 
+        Country
+    FROM
+        w3schools.customers) UNION (SELECT 
+        Country
+    FROM
+        w3schools.suppliers)) AS total_countries
+ORDER BY Country
+LIMIT 5;
