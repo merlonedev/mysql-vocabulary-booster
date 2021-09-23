26c21503438a9e967f22563699e07c893e989b80
Daniel Ceci <danielceci96@gmail.com>
2021-09-23 16:10:44 -0300

    tirando AFTER e colocando BEFORE

--- desafio17.sql
+++ desafio17.sql
@@ -2,7 +2,7 @@ USE w3schools;
 
 DELIMITER $$
 CREATE TRIGGER triggerReq15 -- cria um gatilho que bota a data atual na order_date
-AFTER INSERT ON w3schools.orders
+BEFORE INSERT ON w3schools.orders
 FOR EACH ROW
 BEGIN
 SET NEW.OrderDate=current_date; -- acho que podia ser NOW()
