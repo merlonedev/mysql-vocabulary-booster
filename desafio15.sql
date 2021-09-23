2c63056375a8966fb9be0b51e379033835c83d50
Jossany Moura <mjossany@gmail.com>
2021-09-23 18:09:05 -0300

    Corrige coldclimate attempt II

--- desafio15.sql
+++ desafio15.sql
@@ -3,18 +3,18 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo (in job varchar(100))
 BEGIN
-	SELECT 
-		round(AVG(salary), 2) AS 'Média salarial'
-    FROM 
-		hr.employees
-    WHERE job_id =
-	(SELECT 
-			job_id 
-		FROM 
-			hr.jobs 
-		WHERE 
-			job_title = job
-	);
+SELECT 
+round(AVG(salary), 2) AS 'Média salarial'
+FROM 
+hr.employees
+WHERE job_id =
+(SELECT 
+  job_id 
+FROM 
+  hr.jobs 
+WHERE 
+  job_title = job
+);
 END $$
 DELIMITER ;
 
