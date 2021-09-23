5720d5817aaeef5bc3cacb9573dd7bd27f277f7f
Josimar Souza <contact.josimarsouza@gmail.com>
2021-09-23 11:20:15 -0300

    Refatora desafio 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,7 +1,7 @@
 SELECT 
 CONCAT(e1.FIRST_NAME, ' ', e1.LAST_NAME) AS `Nome completo funcionário 1`,
 e1.SALARY AS `Salário funcionário 1`,
-e1.PHONE_NUMBER AS `Telefone funcionario 1`,
+e1.PHONE_NUMBER AS `Telefone funcionário 1`,
 CONCAT(e2.FIRST_NAME, ' ', e2.LAST_NAME) AS `Nome completo funcionário 2`,
 e2.SALARY AS `Salário funcionário 2`,
 e2.PHONE_NUMBER AS `Telefone funcionário 2`
