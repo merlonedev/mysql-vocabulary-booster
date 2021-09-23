f8fc615c2267bac9ed7b5ab64c17129367a88fc5
Abner <gedeaoabner@gmail.com>
2021-09-23 01:30:09 -0300

    Desafios 10 ao 12

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+SELECT 
+    c1.ContactName AS `Nome`,
+    c1.Country AS `País`,
+    (SELECT 
+            (COUNT(*) - 1)
+        FROM
+            w3schools.customers AS c2
+        WHERE
+            c1.Country = c2.Country) AS `Número de compatriotas`
+FROM
+    w3schools.customers AS c1
+ORDER BY `Nome`;
