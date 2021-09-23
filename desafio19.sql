97c6c587b924d9354fa24186b9f1ab9bb28d4a06
Patrick Dack <patrickdack89@gmail.com>
2021-09-23 04:17:00 -0300

    Requisito 19

---
+++ desafio19.sql
@@ -0,0 +1,19 @@
+DELIMITER $$
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(month VARCHAR(2), year VARCHAR(4))
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE result  INT;
+	SELECT
+    COUNT(*)
+FROM
+    employees
+WHERE
+    HIRE_DATE LIKE CONCAT(year,
+            '-',
+            IF(LENGTH(month) = 1,
+                CONCAT(0, month),
+                month), '-',
+            '%') INTO result;
+    RETURN result;
+END $$
+DELIMITER ;
