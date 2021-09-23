4367cb017fb33817c4d1133fee9f4af2db069446
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-09-23 16:18:19 -0300

    Tentando arrumar o codeclimate de novo

--- desafio10.sql
+++ desafio10.sql
@@ -1,12 +1,12 @@
-SELECT
+SELECT 
     p.ProductName AS 'Produto',
     MIN(od.Quantity) AS 'Mínima',
     MAX(od.Quantity) AS 'Máxima',
-    ROUND(AVG(od.Quantity),2) AS 'Média'
+    ROUND(AVG(od.Quantity), 2) AS 'Média'
 FROM
-	w3schools.products AS p
-		INNER JOIN
-	w3schools.order_details AS od ON od.ProductID = p.ProductID
+    w3schools.products AS p
+        INNER JOIN
+    w3schools.order_details AS od ON od.ProductID = p.ProductID
 GROUP BY p.ProductName
 HAVING `Média` > 20.00
-ORDER BY `Média` ASC, p.ProductName ASC;
+ORDER BY `Média` ASC , p.ProductName ASC;
