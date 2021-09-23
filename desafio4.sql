1c4554e724d4a7398caa3a31aefdb17d2d7c5715
Wadson Almeida <wadson.dev92@gmail.com>
2021-09-23 19:49:25 -0300

    Resolve desafio4

---
+++ desafio4.sql
@@ -0,0 +1,12 @@
+select job.job_title as Cargo, round(avg(employee.salary), 2) as `Média salarial`,
+case
+when round(avg(employee.salary), 2) between 2000 and 5800 then 'Júnior'
+when round(avg(employee.salary), 2) between 5801 and 7500 then 'Pleno'
+when round(avg(employee.salary), 2) between 7501 and 10500 then 'Sênior'
+else 'CEO'
+end as Senioridade
+from hr.jobs as job
+inner join hr.employees as employee
+on job.JOB_ID = employee.JOB_ID
+group by employee.JOB_ID
+order by avg(employee.salary);
