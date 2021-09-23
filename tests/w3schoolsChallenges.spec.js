a953f9a9f24c4390f78a36a17189fac5e8531354
jorda <jordan.rachid@gmail.com>
2021-09-23 01:12:07 -0300

    desafio 10

--- tests/w3schoolsChallenges.spec.js
+++ tests/w3schoolsChallenges.spec.js
@@ -47,7 +47,7 @@ describe('Desafios iniciais', () => {
   });
 
   describe('9 - Exibe todos as pessoas funcionárias que já realizaram algum pedido, mostrando também seu total de pedidos feitos', () => {
-    it.only('Verifica o desafio 9', async () => {
+    it('Verifica o desafio 9', async () => {
       const challengeQuery = readFileSync('desafio9.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult9');
 
@@ -56,7 +56,7 @@ describe('Desafios iniciais', () => {
   });
 
   describe('10 - Exibe todos os produtos que já foram pedidos, que possuem uma média de quantidade nos pedidos registrados acima de `20.00`', () => {
-    it('Verifica o desafio 10', async () => {
+    it.only('Verifica o desafio 10', async () => {
       const challengeQuery = readFileSync('desafio10.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult10');
 
