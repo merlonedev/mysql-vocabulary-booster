aecb00e05c793383756099d568a6b637357abaa2
Daniel Ceci <danielceci96@gmail.com>
2021-09-23 16:08:02 -0300

    requisito 17

---
+++ desafio17.sql
@@ -0,0 +1,11 @@
+USE w3schools;
+
+DELIMITER $$
+CREATE TRIGGER triggerReq15 -- cria um gatilho que bota a data atual na order_date
+AFTER INSERT ON w3schools.orders
+FOR EACH ROW
+BEGIN
+SET NEW.OrderDate=current_date; -- acho que podia ser NOW()
+END $$ -- quando inserir algo na tabela orders, vai inserir também a data atual na coluna order_date.
+
+DELIMITER ;
