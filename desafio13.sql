a8d4c1c39c709d6bf51fc2c36230bffc94c3a128
Victor Emmanuel <victorpassos2012@gmail.com>
2021-09-23 18:27:58 -0300

    Desario 13 feito

---
+++ desafio13.sql
@@ -0,0 +1,9 @@
+SELECT 
+    ProductName AS 'Produto', Price AS 'Preço'
+FROM
+    w3schools.products AS p
+        JOIN
+    order_details AS od ON p.ProductID = od.ProductID
+WHERE
+    od.Quantity > 80
+ORDER BY ProductName;
