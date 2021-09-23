ad69cb920e2796264c9dde13137777b416dda412
thales <thalesdaniel2013@gmail.com>
2021-09-23 05:45:54 -0300

    reformulada etapa 6 com joins

--- desafio6.sql
+++ desafio6.sql
@@ -1,18 +1,14 @@
 SELECT 
-    CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
-    (SELECT 
-            JOB_TITLE
-        FROM
-            jobs AS job
-        WHERE
-            job.JOB_ID = E.JOB_ID) AS Cargo,
-    E.HIRE_DATE AS `Data de início do cargo`,
-    (SELECT 
-            DEPARTMENT_NAME
-        FROM
-            departments AS dpt
-        WHERE
-            dpt.DEPARTMENT_ID = E.DEPARTMENT_ID) AS Departamento
+    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
+    j.JOB_TITLE AS Cargo,
+    jh.START_DATE AS `Data de início do cargo`,
+    DEPARTMENT_NAME AS Departamento
 FROM
-    employees AS E
-ORDER BY `Nome completo` DESC, `Cargo`;
+    employees AS e
+        JOIN
+    job_history jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+        JOIN
+    jobs AS j ON jh.JOB_ID = j.JOB_ID
+        JOIN
+    departments AS dt ON dt.DEPARTMENT_ID = jh.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, Cargo;
