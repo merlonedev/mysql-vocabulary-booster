22ff9b33d9213df014354b07ea2ade4c18f79868
camila-mp <ca.malvp@gmail.com>
2021-09-23 11:57:22 -0300

    resolve desafio 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,2 +1,8 @@
 -- Exiba os 05 primeiros países distintos dentre as pessoas consumidoras e as empresas fornecedoras, em ordem alfabética (w3schools).
 -- Monte uma query que exiba 01 coluna com o alias "País" contendo o nome do país.
+-- select * from w3schools.customers;
+-- select * from w3schools.suppliers;
+(SELECT DISTINCT Country AS `País` FROM w3schools.customers)
+UNION ALL
+(SELECT DISTINCT Country AS `País` FROM w3schools.suppliers) 
+ORDER BY `País` LIMIT 5;
