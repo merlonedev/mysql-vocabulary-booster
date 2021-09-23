844e70b08da9a93de91e08411b20ddb10948db73
Guilherme Cesconeto <cesconeto@gmail.com>
2021-09-23 11:04:41 -0300

    Fazendo a 12 FIX

--- desafio12.sql
+++ desafio12.sql
@@ -8,4 +8,4 @@ SELECT
 FROM
   hr.employees AS e1
 JOIN hr.employees AS e2 ON e1.JOB_ID = e2.JOB_ID AND e1.EMPLOYEE_ID != e2.EMPLOYEE_ID
-ORDER BY `Nome completo funcionário 1`;
+ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
