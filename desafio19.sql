8ee0ed71f0a7c580e32126b81bdcf8f22c681297
Patrick Dack <patrickdack89@gmail.com>
2021-09-23 04:20:56 -0300

    Corrige code climate requisito 19

--- desafio19.sql
+++ desafio19.sql
@@ -2,8 +2,8 @@ DELIMITER $$
 CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(month VARCHAR(2), year VARCHAR(4))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE result  INT;
-	SELECT
+DECLARE result  INT;
+SELECT
     COUNT(*)
 FROM
     employees
