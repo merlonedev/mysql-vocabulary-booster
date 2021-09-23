148668f96050475273fa9dac3b93f9ab4a66e8e2
Isaac Cruz <isaac.clm1@gmail.com>
2021-09-23 19:15:02 -0300

    Add: Requisito 15

---
+++ desafio15.sql
@@ -0,0 +1,12 @@
+-- Esse Código foi baseado no código do colega MATHEUS TKACZYK RIBEIRO, turma 12
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
+BEGIN
+  SELECT ROUND(AVG(SALARY), 2) AS `Média salarial` FROM hr.employees AS e
+  WHERE EXISTS (
+	SELECT * FROM hr.jobs AS j
+    WHERE j.JOB_TITLE LIKE (cargo)
+    AND e.JOB_ID = j.JOB_ID
+  );
+END $$
+DELIMITER ;
