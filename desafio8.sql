0e12dee0acaab7009bfdf63910d9610081fd6353
thales <thalesdaniel2013@gmail.com>
2021-09-23 09:19:16 -0300

    feito correção da 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,16 +1,13 @@
 SELECT 
     ContactName AS `Nome de contato`,
-    (SELECT 
-            sp.ShipperName
-        FROM
-            shippers AS sp
-        WHERE
-            sp.ShipperID = o.ShipperID) AS `Empresa que fez o envio`,
+    SP.ShipperName AS `Empresa que fez o envio`,
     o.OrderDate AS `Data do pedido`
 FROM
     customers AS c
         JOIN
     orders AS o ON c.CustomerID = o.CustomerID
+    JOIN
+    shippers AS SP ON SP.ShipperID = o.ShipperID
 WHERE
-    o.ShipperID = 1 OR 2
+    o.ShipperID IN(1,2)
 ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
