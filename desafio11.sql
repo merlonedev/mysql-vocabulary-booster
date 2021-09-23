ae3eaf77b871efefedc0ed15204e62c68d7f62ac
Marcela Silva <marcela.ops@hotmail.br>
2021-09-23 17:59:22 -0300

    resolve desafio11

---
+++ desafio11.sql
@@ -0,0 +1,10 @@
+SELECT 
+    ContactName AS 'Nome',
+    Country AS 'País',
+    (select count(*) - 1 from customers as c2 where c1.Country = c2.Country) as 'Número de compatriotas'
+FROM
+    customers as c1 
+having `Número de compatriotas` > 0
+ORDER BY ContactName;
+
+
