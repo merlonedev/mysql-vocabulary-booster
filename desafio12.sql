1155e19f4cff49019b1500cb590eb479b2c44fd3
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-09-23 07:56:34 -0300

    Resolvendo erro desafio 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,16 +1,16 @@
 -- 12 - Faça um relatório que lista todas as pessoas funcionárias que possuem o mesmo cargo
 
 SELECT 
-    CONCAT(A.FIRST_NAME, ' ', A.LAST_NAME) AS `Nome completo funcionário 1`,
-    A.SALARY AS 'Salário funcionário 1',
-    A.PHONE_NUMBER AS 'Telefone funcionário 1',
-	CONCAT(B.FIRST_NAME, ' ', B.LAST_NAME) AS `Nome completo funcionário 2`,
-    B.SALARY AS 'Salário funcionário 2',
-    B.PHONE_NUMBER AS 'Telefone funcionário 2'
+  CONCAT(A.FIRST_NAME, ' ', A.LAST_NAME) AS `Nome completo funcionário 1`,
+  A.SALARY AS 'Salário funcionário 1',
+  A.PHONE_NUMBER AS 'Telefone funcionário 1',
+  CONCAT(B.FIRST_NAME, ' ', B.LAST_NAME) AS `Nome completo funcionário 2`,
+  B.SALARY AS 'Salário funcionário 2',
+  B.PHONE_NUMBER AS 'Telefone funcionário 2'
 FROM
-    employees A,
-    employees B
+  employees A,
+  employees B
 WHERE
-    A.JOB_ID = B.JOB_ID
-        AND A.EMPLOYEE_ID <> B.EMPLOYEE_ID
+  A.JOB_ID = B.JOB_ID
+      AND A.EMPLOYEE_ID <> B.EMPLOYEE_ID
 ORDER BY `Nome completo funcionário 1` ASC, `Nome completo funcionário 2` ASC;
