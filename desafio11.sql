5fbe6755491fc3e20796693266337d0f32e82616
Isaac Cruz <isaac.clm1@gmail.com>
2021-09-23 11:58:17 -0300

    Add: Requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,7 @@
+SELECT
+c1.ContactName AS Nome,
+c1.Country AS País,
+(SELECT COUNT(*) - 1 FROM w3schools.customers AS c2 WHERE c1.Country = c2.Country) AS `Número de compatriotas`
+FROM w3schools.customers AS c1
+WHERE (SELECT COUNT(*) - 1 FROM w3schools.customers AS c2 WHERE c1.Country = c2.Country) > 0
+ORDER BY Nome;
