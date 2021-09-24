cb57ad4659a05ce6cec9b1414b41ea5a19cb5d55
Eder Santos <3derms@gmail.com>
2021-09-24 11:29:53 -0300

    Faz um relatório que lista todas as pessoas funcionárias que possuem o mesmo cargo

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,8 @@
 SELECT 
     j.JOB_TITLE AS 'Cargo',
     (j.MAX_SALARY - j.MIN_SALARY) AS 'Variação Salarial',
-    ROUND(AVG(j.MIN_SALARY)/12, 2) AS 'Média mínima mensal',
-    ROUND(AVG(j.MAX_SALARY)/12, 2) AS 'Média máxima mensal'
+    ROUND((j.MIN_SALARY)/12, 2) AS 'Média mínima mensal',
+    ROUND((j.MAX_SALARY)/12, 2) AS 'Média máxima mensal'
 FROM
     hr.jobs AS j
-GROUP BY j.JOB_TITLE
 ORDER BY j.MAX_SALARY - j.MIN_SALARY, j.JOB_TITLE;
