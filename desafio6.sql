528bb51c2cd728094a10e494d66c6cf422d76429
Nalu <aluziapantoja@gmail.com>
2021-09-24 18:52:36 -0300

    Corrigindo reqs 6 e 7

--- desafio6.sql
+++ desafio6.sql
@@ -1,11 +1,13 @@
 SELECT
   CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome completo',
   job.JOB_TITLE AS 'Cargo',
-  emp.HIRE_DATE AS 'Data de início do cargo',
+  jobH.START_DATE AS 'Data de início do cargo',
   dep.DEPARTMENT_NAME AS 'Departamento'
-FROM hr.employees AS emp
+FROM hr.job_history AS jobH
 INNER JOIN hr.jobs AS job
-ON job.JOB_ID = emp.JOB_ID
+ON jobH.JOB_ID = job.JOB_ID
 INNER JOIN hr.departments AS dep
-ON dep.DEPARTMENT_ID = emp.DEPARTMENT_ID
+ON jobH.DEPARTMENT_ID = dep.DEPARTMENT_ID
+INNER JOIN hr.employees AS emp
+ON jobH.EMPLOYEE_ID = emp.EMPLOYEE_ID
 ORDER BY `Nome completo` DESC, `Cargo`;
--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,9 @@
 SELECT
-  UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS 'Nome completo',
-  HIRE_DATE AS 'Data de início',
-  SALARY AS 'Salário'
-FROM hr.employees
-ORDER BY `Nome completo`, HIRE_DATE;
+  UCASE(CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME)) AS 'Nome completo',
+  jobH.START_DATE AS 'Data de início',
+  emp.SALARY AS 'Salário'
+FROM hr.employees AS emp
+INNER JOIN hr.job_history AS jobH
+ON jobH.EMPLOYEE_ID = emp.EMPLOYEE_ID
+WHERE MONTH(jobH.START_DATE) BETWEEN 1 AND 3
+ORDER BY `Nome completo`, `Data de início`;
