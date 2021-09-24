74382c6e8e30e8ead784955d0b18afc78df1157e
Paulo Henrique de Paula Bomfim <paulopbomfim@gmail.com>
2021-09-24 18:22:32 -0300

    Requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,18 @@
+SELECT 
+    CONCAT(FIRST_NAME, ' ', LAST_NAME) AS 'Nome completo',
+    (SELECT 
+            JOB_TITLE
+        FROM
+            hr.jobs
+        WHERE
+            hr.jobs.JOB_ID = hr.employees.JOB_ID) AS 'Cargo',
+    HIRE_DATE AS 'Data de início do cargo',
+    (SELECT 
+            DEPARTMENT_NAME
+        FROM
+            hr.departments
+        WHERE
+            hr.departments.DEPARTMENT_ID = hr.employees.DEPARTMENT_ID) AS 'Departamento'
+FROM
+    hr.employees
+ORDER BY CONCAT(FIRST_NAME, ' ', LAST_NAME) DESC , 'Cargo';
