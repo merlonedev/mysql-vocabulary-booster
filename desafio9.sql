2d3db8a29658633d260ca42c5156e06765feb786
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-09-24 02:32:29 -0300

    Desafio 9 completo, desafio 10 incompleto

---
+++ desafio9.sql
@@ -0,0 +1,10 @@
+SELECT 
+    CONCAT(e.FirstName, ' ', e.LastName) AS `Nome completo`,
+    COUNT(o.EmployeeID) as `Total de pedidos`
+FROM
+    employees e
+JOIN
+	orders o
+    on e.EmployeeID = o.EmployeeID
+GROUP BY o.EmployeeID
+ORDER BY `Total de pedidos`;
