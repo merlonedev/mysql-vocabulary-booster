bf66571849c99ec139235af07e7ffd2adf0f946e
lucasnahas <lucasahas@gmail.com>
2021-09-24 15:22:10 +0100

    muda de aspas pra backtick

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,8 @@
 SELECT 
-    COUNTRY_NAME AS 'País',
+    COUNTRY_NAME AS `País`,
     IF(REGION_ID = 1,
-        'incluído',
-        'não incluído') AS 'Status Inclusão'
+        `incluído`,
+        `não incluído`) AS `Status Inclusão`
 FROM
     hr.countries
-ORDER BY 'País';
+ORDER BY `País`;
