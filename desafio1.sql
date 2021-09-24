617bc25ff27276996c805a374d7f2b090ba9e9a1
lucasnahas <lucasahas@gmail.com>
2021-09-24 15:41:04 +0100

    corrige requisito 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,6 @@
-SELECT 
-    COUNTRY_NAME AS `País`,
-    IF(REGION_ID = 1,
-        `incluído`,
-        `não incluído`) AS `Status Inclusão`
-FROM
-    hr.countries
+SELECT c.COUNTRY_NAME AS `País`,
+IF(r.REGION_NAME = `Europe`, `incluído`, `não incluído`) AS `Status Inclusão`
+FROM hr.countries AS c
+JOIN hr.regions AS r
+ON c.REGION_ID = r.REGION_ID
 ORDER BY `País`;
