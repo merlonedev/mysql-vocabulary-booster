5dfaf224051365d2f2bb28c6b80274457dcdab46
jorda <jordan.rachid@gmail.com>
2021-09-23 22:27:23 -0300

    desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,7 @@
 SELECT
-  ContactName AS "Nome",
-  Country  AS "País",
-  (SELECT COUNT(ContactName) FROM w3schools.customers)
-FROM w3schools.customers
-GROUP BY Country
-HAVING COUNT(ContactName) > 1
-ORDER BY Country;
+  c.ContactName AS "Nome",
+  c.Country  AS "País",
+  (SELECT COUNT(*) FROM w3schools.customers WHERE c.Country = Country) -1  AS 'Número de compatriotas'
+FROM w3schools.customers AS c
+WHERE (SELECT COUNT(*) FROM w3schools.customers WHERE c.Country = Country) > 1
+ORDER BY c.ContactName
--- tests/w3schoolsChallenges.spec.js
+++ tests/w3schoolsChallenges.spec.js
@@ -65,7 +65,7 @@ describe('Desafios iniciais', () => {
   });
 
   describe('11 - Exibe todas as pessoas clientes **que possuem compatriotas**, mostrando a quantidade de compatriotas para cada pessoa cliente', () => {
-    it('Verifica o desafio 11', async () => {
+    it.only('Verifica o desafio 11', async () => {
       const challengeQuery = readFileSync('desafio11.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult11');
 
@@ -74,7 +74,7 @@ describe('Desafios iniciais', () => {
   });
 
   describe('13 - Exibe todos produtos **que já tiveram um pedido associado requerindo uma quantidade desse produto maior que 80**', () => {
-    it.only('Verifica o desafio 13', async () => {
+    it('Verifica o desafio 13', async () => {
       const challengeQuery = readFileSync('desafio13.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult13');
 
