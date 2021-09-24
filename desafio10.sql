4bab4faaed66ed8b86c4605f1e310e9dfad0ce7d
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-09-24 03:07:49 -0300

    Desafio 10 completo

---
+++ desafio10.sql
@@ -0,0 +1,12 @@
+SELECT 
+    p.ProductName AS Produto,
+    MIN(od.Quantity) AS Mínima,
+    MAX(od.Quantity) AS Máximo,
+    ROUND(AVG(od.Quantity), 2) AS Média
+FROM
+    products p
+        JOIN
+    order_details od ON p.ProductID = od.ProductID
+GROUP BY p.ProductID
+HAVING Média > 20
+ORDER BY Média , Produto;
