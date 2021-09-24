5bbb46418c7f320760bb7c22ff4b318aacdbcf02
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-09-24 03:13:11 -0300

    Corrige requisito 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,7 +1,7 @@
 SELECT 
     p.ProductName AS Produto,
     MIN(od.Quantity) AS Mínima,
-    MAX(od.Quantity) AS Máximo,
+    MAX(od.Quantity) AS Máxima,
     ROUND(AVG(od.Quantity), 2) AS Média
 FROM
     products p
