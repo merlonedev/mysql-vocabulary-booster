d00cf0862d473b3a591b876611e80a73797cbc53
Isaac Cruz <isaac.clm1@gmail.com>
2021-09-24 15:04:09 -0300

    Corrige requisitos 9 e 16

--- desafio16.sql
+++ desafio16.sql
@@ -1,11 +1,15 @@
 DELIMITER $$
-CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(IN email VARCHAR(50))
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
+RETURNS INT READS SQL DATA
 BEGIN
+DECLARE total INT;
 SELECT COUNT(*) AS total_empregos FROM hr.job_history AS h
 WHERE EXISTS (
 SELECT * FROM hr.employees AS e
 WHERE e.EMAIL LIKE (email)
 AND e.EMPLOYEE_ID = h.EMPLOYEE_ID
-);
+)
+INTO total;
+RETURN total;
 END $$
 DELIMITER ;
--- desafio9.sql
+++ desafio9.sql
@@ -1,5 +1,5 @@
 SELECT
-CONCAT(e.FirstName, ' ', e.LastName) AS `Nome Completo`,
+CONCAT(e.FirstName, ' ', e.LastName) AS `Nome completo`,
 COUNT(o.OrderID) AS `Total de pedidos`
 FROM w3schools.employees AS e
 INNER JOIN w3schools.orders AS o
