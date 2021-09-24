ea05fdecd5b8a7b3ad47e15d0dc5f2e134e6b0cf
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-09-24 17:44:03 -0300

    Conserta codeclimate

--- desafio16.sql
+++ desafio16.sql
@@ -5,9 +5,14 @@ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(10))
 RETURNS INT READS SQL DATA
 BEGIN
 	DECLARE empregos_total INT;
-    SELECT COUNT(*)
-    FROM hr.job_history AS JH INNER JOIN hr.employees AS E ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
-    WHERE E.EMAIL = email into empregos_total;
+SELECT 
+    COUNT(*)
+FROM
+    hr.job_history AS JH
+        INNER JOIN
+    hr.employees AS E ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+WHERE
+    E.EMAIL = email INTO empregos_total;
 	RETURN empregos_total;
 END $$
 
--- desafio19.sql
+++ desafio19.sql
@@ -5,8 +5,13 @@ CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano
 RETURNS INT READS SQL DATA
 BEGIN
 	DECLARE total_contratadas INT;
-    SELECT COUNT(*) FROM hr.employees
-    WHERE MONTH(HIRE_DATE) = mes AND YEAR(HIRE_DATE) = ano into total_contratadas;
+SELECT 
+    COUNT(*)
+FROM
+    hr.employees
+WHERE
+    MONTH(HIRE_DATE) = mes
+        AND YEAR(HIRE_DATE) = ano INTO total_contratadas;
     RETURN total_contratadas;
 END $$
 
