1a0380590e4432be610de7282b424abdee54ab94
Raph2ll <raphael.ph2ll@gmail.com>
2021-09-24 11:26:02 -0300

    Resolve 13 - > 80

--- desafio12.sql
+++ desafio12.sql
@@ -1,14 +1,14 @@
 SELECT 
-    CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) 'Nome completo funcionário 1',
+    CONCAT(E1.FIRST_NAME, ' ', E1.LAST_NAME) 'Nome completo funcionário 1',
     CONCAT(E2.FIRST_NAME, ' ', E2.LAST_NAME) 'Nome completo funcionário 2',
-    E.SALARY 'Salário funcionário 1',
+    E1.SALARY 'Salário funcionário 1',
     E2.SALARY 'Salário funcionário 2',
-    E.PHONE_NUMBER 'Telefone funcionário 1',
+    E1.PHONE_NUMBER 'Telefone funcionário 1',
     E2.PHONE_NUMBER 'Telefone funcionário 2'
 FROM
-    hr.employees AS E,
+    hr.employees AS E1,
     hr.employees AS E2
 WHERE
-    E.JOB_ID = E2.JOB_ID
-        AND E.EMPLOYEE_ID <> E2.EMPLOYEE_ID
+    E1.JOB_ID = E2.JOB_ID
+        AND E1.EMPLOYEE_ID <> E2.EMPLOYEE_ID
 ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
