cae9d18d9ccacc97291902073791899244e85457
thales <thalesdaniel2013@gmail.com>
2021-09-24 08:34:23 -0300

    feito etapa 11

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT
+    ct1.ContactName AS `Nome`,
+    ct1.Country AS País,
+    COUNT(ct1.ContactName) - 1 AS `Número de compatriotas`
+FROM
+    customers AS ct1,
+    customers AS ct2
+WHERE
+    ct1.Country = ct2.Country
+        AND ct1.ContactName <> ct2.ContactName
+GROUP BY ct1.Country , ct1.ContactName
+ORDER BY ct1.ContactName;
