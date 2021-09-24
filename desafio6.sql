0ba6a138b701aecec206d49fe9ef7feb45e4bf0f
Paulo Henrique de Paula Bomfim <paulopbomfim@gmail.com>
2021-09-24 18:35:44 -0300

    Requisito 6 - tabela alterada

--- desafio6.sql
+++ desafio6.sql
@@ -1,18 +1,23 @@
 SELECT 
-    CONCAT(FIRST_NAME, ' ', LAST_NAME) AS 'Nome completo',
+    (SELECT 
+            CONCAT(FIRST_NAME, ' ', LAST_NAME)
+        FROM
+            hr.employees
+        WHERE
+            hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID) AS 'Nome completo',
     (SELECT 
             JOB_TITLE
         FROM
             hr.jobs
         WHERE
-            hr.jobs.JOB_ID = hr.employees.JOB_ID) AS 'Cargo',
-    HIRE_DATE AS 'Data de início do cargo',
+            hr.jobs.JOB_ID = hr.job_history.JOB_ID) AS 'Cargo',
+    START_DATE AS 'Data de início do cargo',
     (SELECT 
             DEPARTMENT_NAME
         FROM
             hr.departments
         WHERE
-            hr.departments.DEPARTMENT_ID = hr.employees.DEPARTMENT_ID) AS 'Departamento'
+            hr.departments.DEPARTMENT_ID = hr.job_history.DEPARTMENT_ID) AS 'Departamento'
 FROM
-    hr.employees
-ORDER BY CONCAT(FIRST_NAME, ' ', LAST_NAME) DESC , 'Cargo';
+    hr.job_history
+ORDER BY 1 DESC , 2;
