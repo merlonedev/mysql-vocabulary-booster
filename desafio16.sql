9c6bdad66c7325e7ff52b9e99d7f2bd8171790dc
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-09-24 11:17:25 -0300

    climate req 16

--- desafio16.sql
+++ desafio16.sql
@@ -3,16 +3,16 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario (emailInput varchar(50))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE total INT;
-	SELECT 
-    COUNT(*)
+DECLARE total INT;
+SELECT 
+COUNT(*)
 FROM
-    job_history AS jh
-        INNER JOIN
-    employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+job_history AS jh
+INNER JOIN
+employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 WHERE
-    e.EMAIL LIKE CONCAT('%', emailInput, '%') INTO total;
-    RETURN total;
+e.EMAIL LIKE CONCAT('%', emailInput, '%') INTO total;
+RETURN total;
 END $$
 
 DELIMITER ;
