0c265d3c0fa9071e15e144ba391942c976943b7c
Wadson Almeida <wadson.dev92@gmail.com>
2021-09-24 15:55:36 -0300

    Resolve desafio10

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+select p.ProductName as Produto,
+min(od.Quantity) as `Mínima`,
+max(od.Quantity) as `Máxima`,
+round(avg(od.Quantity), 2) as `Média`
+from w3schools.products as p
+inner join w3schools.order_details as od
+on p.ProductID = od.ProductID
+group by 1
+having avg(od.Quantity) > 20.00
+order by 4 asc, 1 asc;
