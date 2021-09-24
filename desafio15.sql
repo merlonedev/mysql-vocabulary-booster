415f34f1d34372d9ab56693c40e69c410fdc4b0f
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-09-24 11:01:42 -0300

    climate req 15

--- desafio15.sql
+++ desafio15.sql
@@ -2,11 +2,11 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(15))
 BEGIN
-	SELECT ROUND(AVG(SALARY), 2) AS `Média salarial`
-    FROM employees AS e
-    INNER JOIN jobs as j
-    ON e.JOB_id = j.JOB_ID
-    WHERE j.JOB_TITLE LIKE CONCAT('%', cargo, '%');
+SELECT ROUND(AVG(SALARY), 2) AS `Média salarial`
+FROM employees AS e
+INNER JOIN jobs as j
+ON e.JOB_id = j.JOB_ID
+WHERE j.JOB_TITLE LIKE CONCAT('%', cargo, '%');
 END $$
 
 DELIMITER ;
