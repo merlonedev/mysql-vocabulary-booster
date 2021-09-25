2a90c80c923333e2b177f9b687e88252f5aeec9a
Armando <a.guerra58@hotmail.com>
2021-09-25 11:31:40 -0300

    desafio15

---
+++ desafio15.sql
@@ -0,0 +1,14 @@
+USE hr;
+
+DELIMITER $$
+  CREATE PROCEDURE buscar_media_por_cargo(IN CARGO VARCHAR(100))
+  BEGIN
+  SELECT ROUND(AVG(E.SALARY),2) AS 'Média salarial'
+FROM hr.employees AS E
+INNER JOIN hr.jobs AS J
+ON E.JOB_ID = J.JOB_ID
+WHERE J.JOB_TITLE = CARGO;
+  END $$
+  DELIMITER ;
+  
+  CALL buscar_media_por_cargo('Programmer');
