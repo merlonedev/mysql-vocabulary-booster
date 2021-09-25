fa5a4ad8bac801e5423c9f5e19a26c99f0ffc8ea
lucasnahas <lucasahas@gmail.com>
2021-09-25 22:55:20 +0100

    requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,13 @@
+SELECT 
+    customers1.ContactName AS 'Nome',
+    customers1.Country AS 'País',
+    (SELECT 
+            COUNT(*) - 1
+        FROM
+            w3schools.customers AS customers2
+        WHERE
+            customers2.Country = customers1.Country) AS 'Número de compatriotas'
+FROM
+    w3schools.customers AS customers1
+HAVING `Número de compatriotas` > 0
+ORDER BY `Nome`;
