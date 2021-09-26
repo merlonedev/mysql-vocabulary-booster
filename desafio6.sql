0637224ba92567ed495f8937c7043dab7e335738
Álvaro Palma <palma.alvaro@outlook.com>
2021-09-26 12:58:46 -0300

    Fix challeng 6

--- desafio6.sql
+++ desafio6.sql
@@ -9,4 +9,4 @@ INNER JOIN hr.jobs t2
 ON t1.job_id = t2.job_id
 LEFT JOIN hr.departments t3
 ON t1.department_id = t3.department_id
-ORDER BY `Nome completo`, `Cargo`;
+ORDER BY `Nome completo` DESC, `Cargo`;
