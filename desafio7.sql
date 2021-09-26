11695738816881f536efb6deeaf8a6309089508c
Rodrigo <rodrigo.facury14@gmail.com>
2021-09-26 16:23:11 -0300

    desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+SELECT 
+    UPPER(CONCAT(t1.FIRST_NAME, ' ', t1.LAST_NAME)) AS `Nome completo`,
+    t2.START_DATE AS `Data de início`,
+    t1.SALARY AS `Salário`
+FROM
+    hr.employees AS t1
+        INNER JOIN
+    hr.job_history AS t2 ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
+WHERE
+    MONTH(t2.START_DATE) IN (01 , 02, 03)
+ORDER BY `Nome completo` ASC , `Data de início` ASC;
+    
