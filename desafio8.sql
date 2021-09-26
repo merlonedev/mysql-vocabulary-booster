6b0f27e7872d8488c5e0b4a84125bba587c76cbe
Rodrigo <rodrigo.facury14@gmail.com>
2021-09-26 16:53:05 -0300

    corrige desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -11,10 +11,11 @@ FROM
     w3schools.orders AS t1
         INNER JOIN
     w3schools.customers AS t2 ON t1.CustomerID = t2.CustomerID
-    WHERE (SELECT 
+WHERE
+    (SELECT 
             ShipperName
         FROM
             w3schools.shippers
         WHERE
-            ShipperID = t1.ShipperID) IN ("Speedy Express", "United Package")
-	ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;
+            ShipperID = t1.ShipperID) IN ('Speedy Express' , 'United Package')
+ORDER BY `Nome de contato` ASC , `Empresa que fez o envio` ASC , `Data do pedido` ASC;
