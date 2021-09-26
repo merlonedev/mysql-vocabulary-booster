261716051053d583bf12f7402f1c94b63ec1c118
Adriana Biberg <adribiberg@hotmail.com>
2021-09-26 17:11:27 -0300

    Code climate

--- desafio7.sql
+++ desafio7.sql
@@ -1,11 +1,11 @@
 SELECT 
-   UPPER(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
-   jh.START_DATE AS 'Data de início do cargo',
+    UPPER(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+    jh.START_DATE AS 'Data de início do cargo',
     e.SALARY AS Salário
 FROM
     hr.employees AS e
         INNER JOIN
-    hr.job_history AS jh
-    ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
-where month(start_date) <= 03
-ORDER BY `Nome completo`, `Data de início do cargo`;
+    hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE
+    MONTH(start_date) <= 03
+ORDER BY `Nome completo` , `Data de início do cargo`;
