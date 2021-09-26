6fcc44200260bd237b8ee74a5819e62c9ed8ddaf
jmlinger <jmlinger10@gmail.com>
2021-09-26 19:11:11 -0300

    Resolve requisito 8: corrige erros code climate - tentativa 1.

--- desafio8.sql
+++ desafio8.sql
@@ -3,11 +3,11 @@ SELECT
     S.ShipperName AS `Empresa que fez o envio`,
     O.OrderDate AS `Data do pedido`
 FROM
-	w3schools.customers AS C
+w3schools.customers AS C
         INNER JOIN
-	w3schools.orders AS O ON O.CustomerID = C.CustomerID
-       INNER JOIN
+w3schools.orders AS O ON O.CustomerID = C.CustomerID
+        INNER JOIN
     w3schools.shippers AS S ON S.ShipperID = O.ShipperID
 WHERE 
-	O.ShipperID = 1 OR O.ShipperID = 2
+O.ShipperID = 1 OR O.ShipperID = 2
 ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
