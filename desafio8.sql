72af67ce1ef63598d0c6cb75f890b26b0287aa55
Adriana Biberg <adribiberg@hotmail.com>
2021-09-26 18:22:19 -0300

    Desafio 8 completo

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,15 @@
 SELECT 
-    ContactName AS 'Nome de contato',
-    (SELECT shipperName from w3schools.shippers WHERE w3schools.oders.OrderID = w3schools.shippers.ShipperID )
+    C.ContactName AS 'Nome de contato',
+    (SELECT 
+            shipperName
+        FROM
+            w3schools.shippers
+        WHERE
+            ShipperID = O.ShipperID) AS 'Empresa que fez o envio',
+    O.OrderDate AS 'Data do pedido'
 FROM
-    w3schools.customers;
+    w3schools.customers AS C
+        INNER JOIN
+    w3schools.orders AS O ON C.CustomerID = O.CustomerID
+HAVING `Empresa que fez o envio` IN ('Speedy Express' , 'United Package')
+ORDER BY `Nome de contato` , `Empresa que fez o envio` , `Data do pedido`;
