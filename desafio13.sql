0b3d28fba5bfb7fa79ffca875ffa28baef58151e
walttoncoelho <walltton@outlook.com>
2021-09-26 15:17:18 -0300

    Desafio13

---
+++ desafio13.sql
@@ -0,0 +1,8 @@
+SELECT 
+    ProductName AS 'Produto',
+	Price As 'Preço'
+FROM
+    products AS `p`
+        JOIN
+    w3schools.order_details d ON d.ProductID = p.ProductID and d.Quantity > 80
+ORDER BY 1;
