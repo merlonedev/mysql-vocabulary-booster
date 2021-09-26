28340b4499c02052b488041256dbf871d6849345
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-09-26 11:26:06 -0300

    exercicio 6

---
+++ desafio6.sql
@@ -0,0 +1,12 @@
+SELECT 
+    CONCAT(a.first_name, ' ', a.last_name) AS `Nome Completo`,
+    b.job_title AS `Cargo`,
+    a.hire_date AS `Data de início do cargo`,
+    c.department_name AS Departamento
+FROM
+    employees AS a
+        INNER JOIN
+    jobs AS b ON a.job_id = b.job_id
+        INNER JOIN
+    departments AS c ON a.department_id = c.department_id
+ORDER BY `Nome Completo` DESC, `Cargo`;
