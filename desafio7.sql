90bd762f9c124dbfffef8851864a20306f64a176
Vinicius <vini_bodra@hotmail.com>
2021-09-26 10:50:05 -0300

    Desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+SELECT 
+    UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+    jh.START_DATE AS 'Data de início',
+    e.SALARY AS 'Salário'
+FROM
+    hr.employees AS e
+INNER JOIN
+	hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE MONTH(START_DATE) = 1 OR MONTH(START_DATE) = 2 OR MONTH(START_DATE) = 3
+ORDER BY `Nome completo` ASC, `Data de início` ASC
