e079ae17700e079fe9ae8f0710e51a4092dc5838
Marcela Silva <marcela.ops@hotmail.br>
2021-09-26 10:30:22 -0300

    resolve desafio16

---
+++ desafio16.sql
@@ -0,0 +1,17 @@
+USE hr;
+DELIMITER $$ 
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(200))
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE jobs_quantity INT;
+    SELECT COUNT(*)
+	FROM job_history AS jh
+	JOIN employees AS e ON jh.employee_id = e.employee_id
+	WHERE e.email = email INTO jobs_quantity;
+    RETURN jobs_quantity;
+END $$
+
+DELIMITER ;
+
+-- SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
