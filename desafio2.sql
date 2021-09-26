3f4faa3d5ec3d27534d1e009066ffd4ee83823b7
Eurides <euridesalves@yahoo.com.br>
2021-09-26 08:33:35 -0300

    Corrigido requisito 5

--- desafio2.sql
+++ desafio2.sql
@@ -1,11 +1,11 @@
 SELECT 
     JOB_TITLE AS Cargo,
     CASE
-        WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
-        WHEN MAX_SALARY > 20001 THEN 'Alto'
-        WHEN MAX_SALARY > 10001 THEN 'Médio'
-        ELSE 'Baixo'
-    END AS 'Nivel'
+        WHEN MAX_SALARY between 5000 and 10000 THEN 'Baixo'
+        WHEN MAX_SALARY between 10001 and 20000 THEN 'Médio'
+        WHEN MAX_SALARY between 20001 and 30000  THEN 'Alto'
+        ELSE 'Altíssimo'
+    END AS 'Nível'
 FROM
     hr.jobs
     order by JOB_TITLE;
--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,10 @@
 SELECT 
     j.job_title AS Cargo, j.MAX_SALARY - j.MIN_SALARY as "Variação Salarial",
-    round(min(e.SALARY),2) as "Média mínima mensal", round(max(e.SALARY),2) as "Média máxima mensal"
+    round(avg(j.MIN_SALARY/12),2) as "Média mínima mensal", round(avg(j.MAX_SALARY/12),2) as "Média máxima mensal"
 FROM
     hr.employees AS e
         INNER JOIN
     hr.jobs AS j ON j.job_id = e.job_id
 GROUP BY e.job_id
 ORDER BY `Variação Salarial` , Cargo;
+
