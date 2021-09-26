d1446b76402c191a4857918c53077c9cf285af4f
Marcela Silva <marcela.ops@hotmail.br>
2021-09-26 12:14:58 -0300

    fix desafio20

---
+++ desafio20.sql
@@ -0,0 +1,24 @@
+SELECT 'Nome completo', 'Departamento', 'Cargo';
+
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(100))
+BEGIN
+	SELECT 
+		CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
+		d.department_name AS  'Departamento',
+		j.job_title AS 'Cargo'
+	FROM job_history AS jh
+		JOIN employees AS e ON jh.employee_id = e.employee_id
+		JOIN departments AS d ON d.department_id = jh.department_id
+		JOIN jobs AS j ON j.job_id = jh.job_id
+		WHERE e.email like 'NKOCHHAR'
+		ORDER BY `Departamento`, `Cargo`;
+END $$
+
+DELIMITER ;
+
+
+
+ 
