388d826121d9ebf5c76ffb7d5c7fda0788d8d8d7
thales <thalesdaniel2013@gmail.com>
2021-09-26 16:00:38 -0300

    corrigido etapa 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,4 +1,4 @@
-(SELECT DISTINCT Country FROM suppliers)
-UNION
-(SELECT DISTINCT Country FROM customers)
-ORDER BY Country LIMIT 5;
+(SELECT DISTINCT Country AS País FROM suppliers)
+UNION 
+(SELECT DISTINCT Country AS País FROM customers) 
+ORDER BY País LIMIT 5;
