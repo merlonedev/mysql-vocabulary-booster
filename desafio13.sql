bd3095cd2be65111fd41e57d3094b9c5407e8478
lucasnahas <lucasahas@gmail.com>
2021-09-26 16:20:19 +0100

    requisito 14

--- desafio13.sql
+++ desafio13.sql
@@ -1,11 +1,10 @@
 SELECT 
-    products.ProductName AS Produto, 
-    products.Price AS Preço
+    Country AS País
 FROM
-    w3schools.order_details AS orderDetails
-        JOIN
-    w3schools.products AS products ON orderDetails.ProductID = products.ProductID
-WHERE
-    orderDetails.Quantity > 80
-GROUP BY Produto , Preço
-ORDER BY Produto;
+    w3schools.customers 
+UNION SELECT 
+    Country AS País
+FROM
+    w3schools.suppliers
+ORDER BY País
+LIMIT 5;
