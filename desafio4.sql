f99b2d04e38abddf6d627d97a1391e67211523bd
Adriana Biberg <adribiberg@hotmail.com>
2021-09-26 15:02:14 -0300

    Desafio 4 - Troca o ceil por round

--- desafio4.sql
+++ desafio4.sql
@@ -1,23 +1,23 @@
 SELECT 
     j.JOB_TITLE AS Cargo,
-    CEIL(AVG(e.SALARY)) AS 'Média salarial',
+	ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
     CASE
         WHEN
-            CEIL(AVG(e.SALARY)) >= 2000
-                AND CEIL(AVG(e.SALARY)) <= 5800
+            ROUND(AVG(e.SALARY), 2)  >= 2000
+                AND ROUND(AVG(e.SALARY), 2)  <= 5800
         THEN
             'Júnior'
         WHEN
-            CEIL(AVG(e.SALARY)) >= 5801
-                AND CEIL(AVG(e.SALARY)) <= 7500
+            ROUND(AVG(e.SALARY), 2)  >= 5801
+                AND ROUND(AVG(e.SALARY), 2)  <= 7500
         THEN
             'Pleno'
         WHEN
-            CEIL(AVG(e.SALARY)) >= 7501
-                AND CEIL(AVG(e.SALARY)) <= 10500
+            ROUND(AVG(e.SALARY), 2)  >= 7501
+                AND ROUND(AVG(e.SALARY), 2)  <= 10500
         THEN
             'Sênior'
-        WHEN CEIL(AVG(e.SALARY)) > 10501 THEN 'CEO'
+        WHEN ROUND(AVG(e.SALARY), 2)  > 10501 THEN 'CEO'
     END AS Senioridade
 FROM
     hr.employees AS e
