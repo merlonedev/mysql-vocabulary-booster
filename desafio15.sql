0892cefdd7eb338cc1493129226963458ef709c2
Martin <escorpmartin97@gmail.com>
2021-09-26 16:16:26 -0300

    [desafio15 - Corrige erro de organizaçao]

--- desafio15.sql
+++ desafio15.sql
@@ -3,12 +3,12 @@ DELIMITER $$
 CREATE PROCEDURE
 	buscar_media_por_cargo(IN JOB_TITLE VARCHAR(50))
 BEGIN
-	SELECT ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
-    FROM
-		hr.employees AS e
-	INNER JOIN
-		hr.jobs AS j ON e.JOB_ID = j.JOB_ID
-	WHERE
+SELECT ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
+FROM
+	hr.employees AS e
+INNER JOIN
+	hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+WHERE
     j.JOB_TITLE = JOB_TITLE;
 END $$
 DELIMITER ;
