61799df6fd79ce8c005fd4c91bedf239b555df89
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-09-26 14:00:14 -0300

    completa req 18

---
+++ desafio18.sql
@@ -0,0 +1,18 @@
+SELECT
+    CONCAT(em.FIRST_NAME, ' ', em.LAST_NAME) AS `Nome completo`,
+    CONCAT(IF(DAY(jh.START_DATE) < 10, CONCAT(0, DAY(jh.START_DATE)), DAY(jh.START_DATE)),
+    '/',
+    IF(MONTH(jh.START_DATE) < 10, CONCAT(0, MONTH(jh.START_DATE)), MONTH(jh.START_DATE)),
+    '/',
+    YEAR(jh.START_DATE)) AS `Data de início`,
+    CONCAT(IF(DAY(jh.END_DATE) < 10, CONCAT(0, DAY(jh.END_DATE)), DAY(jh.END_DATE)),
+    '/',
+    IF(MONTH(jh.END_DATE) < 10,CONCAT(0, MONTH(jh.END_DATE)), MONTH(jh.END_DATE)),
+    '/',
+    YEAR(jh.END_DATE)) AS `Data de rescisão`,
+    ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE) / 365, 2) AS `Anos trabalhados`
+FROM
+    employees AS `em`
+INNER JOIN job_history AS `jh`
+ON em.EMPLOYEE_ID = jh.EMPLOYEE_ID
+ORDER BY `Nome completo`, `Anos trabalhados`;
