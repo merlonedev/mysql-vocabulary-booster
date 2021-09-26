f879cbda4005139c6e76526cf0ccf218d65d86c3
Matheus Bodra <matheusbodra@gmail.com>
2021-09-26 15:27:19 -0300

    desafios 11 a 14

--- desafio11.sql
+++ desafio11.sql
@@ -16,13 +16,13 @@
 -- Os resultados devem estar ordenados pelo nome de contato da pessoa cliente em ordem alfabética.
 SELECT 
     customers1.ContactName AS 'Nome',
-    customers1.Country AS 'Páis',
+    customers1.Country AS 'País',
     COUNT(*) AS 'Número de compatriotas'
 FROM
     w3schools.customers AS customers1,
     w3schools.customers AS customers2
 WHERE
     customers1.Country = customers2.Country
-        AND customers1.CustomerID <> customers2.CustomerID
+        AND customers1.CustomerID <> customers2.customerID
 GROUP BY customers1.ContactName , customers1.Country
-ORDER BY customers1.ContactName ASC;
+ORDER BY Nome ASC;
