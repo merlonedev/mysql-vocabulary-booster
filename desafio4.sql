cdbaa0de0e45628f3cbde2985a1a3cc409bb331b
MIkaela Braga <bragamikaela@gmail.com>
2021-09-26 01:58:04 -0300

    Desafio 5 completo

--- desafio4.sql
+++ desafio4.sql
@@ -8,4 +8,4 @@ CASE
 END AS `Senioridade`
 FROM hr.jobs AS j
 	INNER JOIN hr.employees AS e
-ON j.JOB_ID = e.JOB_ID GROUP BY j.JOB_TITLE ORDER BY AVG(e.salary), j.JOB_TITLE;
+ON j.JOB_ID = e.JOB_ID GROUP BY j.JOB_TITLE ORDER BY AVG(e.salary), j.JOB_TITLE ASC;
