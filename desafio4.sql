fc9287522001f3647b2d1318f6cac4eb925f0f28
Adriana Biberg <adribiberg@hotmail.com>
2021-09-26 15:06:25 -0300

    code climate

--- desafio4.sql
+++ desafio4.sql
@@ -1,23 +1,23 @@
 SELECT 
     j.JOB_TITLE AS Cargo,
-	ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
+    ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
     CASE
         WHEN
-            ROUND(AVG(e.SALARY), 2)  >= 2000
-                AND ROUND(AVG(e.SALARY), 2)  <= 5800
+            ROUND(AVG(e.SALARY), 2) >= 2000
+                AND ROUND(AVG(e.SALARY), 2) <= 5800
         THEN
             'Júnior'
         WHEN
-            ROUND(AVG(e.SALARY), 2)  >= 5801
-                AND ROUND(AVG(e.SALARY), 2)  <= 7500
+            ROUND(AVG(e.SALARY), 2) >= 5801
+                AND ROUND(AVG(e.SALARY), 2) <= 7500
         THEN
             'Pleno'
         WHEN
-            ROUND(AVG(e.SALARY), 2)  >= 7501
-                AND ROUND(AVG(e.SALARY), 2)  <= 10500
+            ROUND(AVG(e.SALARY), 2) >= 7501
+                AND ROUND(AVG(e.SALARY), 2) <= 10500
         THEN
             'Sênior'
-        WHEN ROUND(AVG(e.SALARY), 2)  > 10501 THEN 'CEO'
+        WHEN ROUND(AVG(e.SALARY), 2) > 10501 THEN 'CEO'
     END AS Senioridade
 FROM
     hr.employees AS e
