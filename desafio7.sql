bcc2c702c66d4a50719e4a50aafaacfb2fbf6141
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-09-26 08:47:59 -0300

    Correçao Requisito 7

--- desafio7.sql
+++ desafio7.sql
@@ -4,4 +4,5 @@ T2.SALARY AS 'Salário'
 FROM hr.job_history AS T1
 INNER JOIN hr.employees AS T2
 ON T1.employee_id = T2.employee_id
+WHERE T1.START_DATE LIKE '%-01-%' OR T1.START_DATE LIKE '%-02-%' OR T1.START_DATE LIKE '%-03-%'
 ORDER BY CONCAT (T2.first_name,' ',T2.last_name) ASC, T1.START_DATE ASC;
