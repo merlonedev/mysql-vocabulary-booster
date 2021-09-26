91fb364a19423860e17f2f6a44059133f7325a81
camila-mp <ca.malvp@gmail.com>
2021-09-26 16:14:25 -0300

    resolve desafio 15

---
+++ desafio15.sql
@@ -0,0 +1,12 @@
+-- Crie uma procedure chamada buscar_media_por_cargo que recebe como parâmetro o nome de um cargo e em retorno deve mostrar
+-- a média salarial de todas as pessoas que possuem esse cargo;
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
+BEGIN
+	SELECT AVG(SALARY) FROM employees
+    WHERE JOB_ID IN (SELECT JOB_ID FROM hr.jobs WHERE JOB_TITLE = cargo);
+END $$
+
+DELIMITER ;
