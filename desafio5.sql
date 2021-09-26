2d1322fba9b36ca9b5a1ed1af9c2f139fed721d6
MaxAlves0035 <maxalves2@hotmail.com>
2021-09-26 17:07:03 -0300

    desafio05 - correcao

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,11 @@
 SELECT 
-    JOB_TITLE AS 'Cargo',
-    (MAX_SALARY - MIN_SALARY) * 12 AS 'Variação Salarial',
-    ROUND(AVG(MIN_SALARY), 2) AS 'Média mínima mensal',
-    ROUND(AVG(MAX_SALARY), 2) AS 'Média máxima mensal'
+    j.JOB_TITLE AS 'Cargo',
+    ROUND(AVG(MAX_SALARY) - AVG(MIN_SALARY)) AS 'Variação Salarial',
+    ROUND(AVG(j.MIN_SALARY / 12), 2) AS 'Média mínima mensal',
+    ROUND(AVG(j.MAX_SALARY / 12), 2) AS 'Média máxima mensal'
 FROM
-    hr.jobs
-GROUP BY MIN_SALARY , MAX_SALARY
-ORDER BY (MAX_SALARY - MIN_SALARY) * 12 ASC , JOB_TITLE ASC;
+    hr.jobs AS j
+        JOIN
+    hr.employees AS e ON e.JOB_ID = j.JOB_ID
+GROUP BY e.JOB_ID
+ORDER BY ROUND(AVG(j.MAX_SALARY) - AVG(j.MIN_SALARY)) ASC , j.JOB_TITLE ASC;
