b375702b351674d59582036423744943021280ed
Vinicius <vini_bodra@hotmail.com>
2021-09-26 09:39:44 -0300

    Desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,10 @@
+SELECT
+	JOB_TITLE AS Cargo,
+    MAX_SALARY - MIN_SALARY AS 'Variação salarial',
+    ROUND(AVG(MIN_SALARY)/12, 2) AS 'Média mínima mensal',
+	ROUND(AVG(MAX_SALARY)/12, 2) AS 'Média máxima mensal'
+FROM
+	hr.jobs
+GROUP BY JOB_ID, `Variação salarial`
+ORDER BY `Variação salarial`
+    
