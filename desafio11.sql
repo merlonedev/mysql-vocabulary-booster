49cb8df1aafa3bc6924b03f72b67707b9d0897aa
lucasnahas <lucasahas@gmail.com>
2021-09-26 14:47:44 +0100

    requisito 11 altera logica

--- desafio11.sql
+++ desafio11.sql
@@ -9,5 +9,11 @@ SELECT
             customers2.Country = customers1.Country) AS 'Número de compatriotas'
 FROM
     w3schools.customers AS customers1
-HAVING `Número de compatriotas` > 0
-ORDER BY `Nome`;
+WHERE
+    (SELECT 
+            COUNT(*) - 1
+        FROM
+            w3schools.customers AS customers2
+        WHERE
+            customers2.Country = customers1.Country) > 0
+ORDER BY Nome;
