3b8d2f8815b963a8079e3cd5a69b84abd631522e
MaxAlves0035 <maxalves2@hotmail.com>
2021-09-26 16:38:21 -0300

    desafio04 - correcao

--- desafio4.sql
+++ desafio4.sql
@@ -1,12 +1,15 @@
 SELECT 
-    JOB_TITLE AS 'Cargo',
-    ROUND((MIN_SALARY + MAX_SALARY) / 2, 2) AS 'Média salarial',
+    J.JOB_TITLE AS 'Cargo',
+    ROUND(AVG(EMP.salary), 2) AS 'Média salarial',
     CASE
-        WHEN ROUND((MIN_SALARY + MAX_SALARY) / 2, 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
-        WHEN ROUND((MIN_SALARY + MAX_SALARY) / 2, 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
-        WHEN ROUND((MIN_SALARY + MAX_SALARY) / 2, 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        WHEN ROUND(AVG(EMP.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN ROUND(AVG(EMP.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN ROUND(AVG(EMP.salary), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
         ELSE 'CEO'
     END AS 'Senioridade'
 FROM
-    hr.jobs
-ORDER BY ROUND((MIN_SALARY + MAX_SALARY) / 2, 2) ASC , JOB_TITLE ASC;
+    hr.employees AS EMP
+        JOIN
+    hr.jobs AS J ON EMP.JOB_ID = J.JOB_ID
+GROUP BY EMP.JOB_ID
+ORDER BY ROUND(AVG(EMP.salary), 2) ASC , J.JOB_TITLE ASC;
