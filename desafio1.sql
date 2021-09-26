011be78bd4b3b98fdedbefe7a34af11e17987595
MaxAlves0035 <maxalves2@hotmail.com>
2021-09-26 15:18:23 -0300

    desafio01 - correcao

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,8 @@
 SELECT 
     COUNTRY_NAME AS 'País',
     IF(REGION_ID = 1,
-        'incluido',
-        'não incluido') AS 'Status de Inclusão'
+        'incluído',
+        'não incluído') AS 'Status Inclusão'
 FROM
     hr.countries
 ORDER BY COUNTRY_NAME ASC;
