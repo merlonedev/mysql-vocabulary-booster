cd8708f8d8ee2528e31afec1924820e6a158d560
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-09-25 21:04:50 -0300

    Requirement 20

---
+++ desafio20.sql
@@ -0,0 +1,21 @@
+DELIMITER $$
+
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(100))
+BEGIN
+    SELECT
+    CONCAT(e.FIRST_NAME , ' ', LAST_NAME) AS `Nome completo`,
+    d.DEPARTMENT_NAME AS Departamento,
+    j.JOB_TITLE AS Cargo
+    FROM
+        hr.job_history AS jh
+            INNER JOIN
+        hr.employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+        AND e.EMAIL = email
+            INNER JOIN
+        hr.jobs AS j ON j.JOB_ID = jh.JOB_ID
+            INNER JOIN
+        hr.departments AS d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
+    ORDER BY Departamento ASC, Cargo ASC;
+END $$
+
+DELIMITER ;
