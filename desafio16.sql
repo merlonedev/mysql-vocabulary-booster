fcad79865e2b4a5a8482791a790bd3d7c18c0372
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-09-26 13:13:14 -0300

    completa req 16

---
+++ desafio16.sql
@@ -0,0 +1,20 @@
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE qty_jobs INT;
+SELECT 
+    COUNT(*)
+FROM
+    employees AS `em`
+        INNER JOIN
+    job_history AS `jh` ON em.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE
+    em.EMAIL = email INTO qty_jobs;
+    RETURN qty_jobs;
+END $$
+
+DELIMITER ;
+
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
