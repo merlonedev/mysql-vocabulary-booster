8cc639984cc82d0bd785a358958175bf89f9ce8c
Gustavo-Mourao <gcmbh1@gmail.com>
2021-09-27 18:27:43 -0300

    Q7

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
-SELECT Ucase( concat(employees.FIRST_NAME, ' ' ,employees.LAST_NAME)) AS 'Nome completo',
-s.START_DATE AS 'Data de início',
-employees.SALARY AS Sálario
+SELECT Ucase( concat(employees.FIRST_NAME , ' ' , employees.LAST_NAME)) AS 'Nome completo',
+s.START_DATE AS `Data de início`,
+employees.SALARY AS `Salário`
 FROM hr.job_history AS s
 INNER JOIN hr.employees AS employees ON s.employee_id = employees.employee_id
 WHERE MONTH(s.START_DATE) IN (1 , 2, 3)
-ORDER BY `Nome completo` ASC , `Data de início` ASC;
+ORDER BY `Nome completo` , `Data de início`;
