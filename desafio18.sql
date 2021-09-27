6d3b387323980621dcadf7efebeb2b09b545020f
AlyssonSene <alysson_silva@outlook.com>
2021-09-27 15:43:16 -0300

    desafio 20 implementado

--- desafio18.sql
+++ desafio18.sql
@@ -1,13 +1,10 @@
-DELIMITER $$
-CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT)
-RETURNS INT READS SQL DATA
-BEGIN
-RETURN (SELECT
-COUNT(*)
-FROM employees
-WHERE
-MONTH(HIRE_DATE) = mes
-AND YEAR(HIRE_DATE) = ano
-);
-END $$
-DELIMITER ;
+SELECT
+CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+DATE_FORMAT(START_DATE, '%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT(END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
+ROUND(DATEDIFF(END_DATE, START_DATE) / 365, 2) AS 'Anos trabalhados'
+FROM
+hr.job_history AS jh
+JOIN
+hr.employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+ORDER BY `Nome completo`, `Anos trabalhados`;
