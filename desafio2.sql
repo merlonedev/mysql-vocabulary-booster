168bb599aba1d1d17c0f19c1abc0f127cce56703
Luiz Otavio <luiz.otavio273@gmail.com>
2021-09-26 23:00:14 -0300

    fix req 02

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,7 @@
-SELECT job_title AS 'Cargo', max_salary,
+SELECT job_title AS 'Cargo',
 (IF(max_salary <= 10000, 'Baixo',
 IF(max_salary <= 20000, 'Médio',
 IF(max_salary <= 30000, 'Alto', 'Altíssimo')))) AS 'Nível'
 FROM hr.jobs
 ORDER BY job_title;
+
