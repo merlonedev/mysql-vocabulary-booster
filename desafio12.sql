cfad33569880f66e58b8af7e71f682632685f6ce
jmlinger <jmlinger10@gmail.com>
2021-09-26 22:19:45 -0300

    Resolve requisito 12: Faz um relatório que lista todas as pessoas funcionárias que possuem o mesmo cargo.

--- desafio12.sql
+++ desafio12.sql
@@ -9,6 +9,6 @@ FROM
     hr.employees AS E1,
     hr.employees AS E2
 WHERE 
-	E1.JOB_ID = E2.JOB_ID 
-		AND E1.EMPLOYEE_ID <> E2.EMPLOYEE_ID
+E1.JOB_ID = E2.JOB_ID 
+AND E1.EMPLOYEE_ID <> E2.EMPLOYEE_ID
 ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
