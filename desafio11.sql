9674c267ebd08df03a5a257732a3f087bea82c0e
Adriana Biberg <adribiberg@hotmail.com>
2021-09-27 10:59:43 -0300

    Desafio 11 - corrigido

--- desafio11.sql
+++ desafio11.sql
@@ -4,10 +4,10 @@ SELECT
     (SELECT 
             COUNT(*)
         FROM
-            customers
+            w3schools.customers
         WHERE
             c.country = country) - 1 AS 'Número de compatriotas'
 FROM
-    w3schools.customers AS c1
+    w3schools.customers AS c
 HAVING `Número de compatriotas` > 0
 ORDER BY ContactName;
