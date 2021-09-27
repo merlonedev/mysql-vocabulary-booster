5d68e38b1d6b177a36a4ad0de70366f2060ad548
Adriana Biberg <adribiberg@hotmail.com>
2021-09-27 11:17:43 -0300

    Desafio 12 - completo

--- desafio12.sql
+++ desafio12.sql
@@ -1,8 +1,8 @@
 SELECT 
-    e1.FIRST_NAME AS 'Nome completo funcionário 1',
+    CONCAT(e1.FIRST_NAME, ' ', e1.LAST_NAME) AS 'Nome completo funcionário 1',
     e1.SALARY AS 'Salário funcionário 1',
     e1.PHONE_NUMBER AS 'Telefone funcionário 1',
-    e2.FIRST_NAME AS 'Nome completo funcionário 2',
+    CONCAT(e2.FIRST_NAME, ' ', e2.LAST_NAME) AS 'Nome completo funcionário 2',
     e2.SALARY AS 'Salário funcionário 2',
     e2.PHONE_NUMBER AS 'Telefone funcionário 2'
 FROM
@@ -11,4 +11,4 @@ FROM
 WHERE
     e1.JOB_ID = e2.JOB_ID
         AND e1.FIRST_NAME <> e2.FIRST_NAME
-ORDER BY e1.FIRST_NAME , e2.FIRST_NAME;
+ORDER BY `Nome completo funcionário 1` , `Nome completo funcionário 2`;
