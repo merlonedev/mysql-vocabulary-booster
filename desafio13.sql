8684e110660f96e9ab9527290860d6a90789a99f
MaxAlves0035 <maxalves2@hotmail.com>
2021-09-27 17:50:57 -0300

    desafio13 - correcao

--- desafio13.sql
+++ desafio13.sql
@@ -3,7 +3,7 @@ SELECT DISTINCT
 FROM
     w3schools.order_details AS D
         JOIN
-    w3schools.products AS P
+    w3schools.products AS P ON D.ProductID = P.ProductID
 WHERE
     Quantity > 80
 ORDER BY `Produto` ASC;
