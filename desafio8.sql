d144820b587a30fc6427b661a952fc66e7af8cd2
Paulo Henrique de Paula Bomfim <paulopbomfim@gmail.com>
2021-09-27 10:59:02 -0300

    Requisito 8 - coluna de nome alterada

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,5 @@
 SELECT 
-    customer.CustomerName AS 'Nome de contato',
+    customer.ContactName AS 'Nome de contato',
     shipper.ShipperName AS 'Empresa que fez o envio',
     orders.OrderDate AS 'Data do pedido'
 FROM
@@ -10,4 +10,4 @@ FROM
     w3schools.shippers AS shipper ON shipper.ShipperID = orders.ShipperID
 WHERE
     shipper.ShipperID < 3
-ORDER BY 1 , 2 , 3 ASC;
+ORDER BY 1 , 2 , 3;
