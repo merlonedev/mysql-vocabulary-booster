04d9c55ae07de056546ecee148dcacc65e4bd4d6
Augusto <augustocm2002@yahoo.com.br>
2021-09-27 17:12:29 -0300

    Atualiza requisitos 4 e 5 e resolve requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+SELECT 
+	UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+    s.START_DATE AS 'Data de início',
+    e.SALARY AS 'Salário'
+FROM
+	hr.employees AS e
+    INNER JOIN
+    hr.job_history AS s ON e.EMPLOYEE_ID = s.EMPLOYEE_ID
+HAVING MONTH(s.START_DATE) BETWEEN 1 AND 3
+	
+ORDER BY UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)), s.START_DATE;
