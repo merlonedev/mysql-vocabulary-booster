4abd1ffb984ae12cedd41c94ae065664894bedd9
CamilaDamasio <camiladamasioef@hotmail.com>
2021-09-27 10:01:12 -0300

    desafio 16 e desafio 15_code_climate

---
+++ desafio16.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
+RETURNS INT READS SQL DATA
+BEGIN	
+DECLARE quantity_jobs INT;
+SELECT
+COUNT(job_history.EMPLOYEE_ID)
+FROM 
+job_history INNER JOIN employees ON
+employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+WHERE 
+employees.EMAIL = email INTO quantity_jobs;
+RETURN quantity_jobs;
+END $$
+DELIMITER ;
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
