4011b11b3c25a879567fcd391964c426d635c842
Nalu <aluziapantoja@gmail.com>
2021-09-27 17:23:23 -0300

    Req 12

--- desafio11.sql
+++ desafio11.sql
@@ -1,14 +1,14 @@
-SELECT
-  cs1.ContactName AS 'Nome',
-  cs1.Country AS 'País',
-  COUNT(cs1.Country) - 1 AS 'Número de compatriotas'
-FROM
-  w3schools.customers AS cs1,
-  w3schools.customers AS cs2
-WHERE cs2.Country = cs1.Country AND cs2.CustomerID != cs1.CustomerID
-GROUP BY
-  `Nome`
-HAVING
-  `Número de compatriotas` <> 0
-ORDER BY
-  `Nome`;
+SELECT 
+    cs1.ContactName 'Nome',
+    cs1.Country AS 'País',
+    (SELECT COUNT(*) - 1
+      FROM w3schools.customers AS cs2
+      WHERE cs1.Country = cs2.Country
+      GROUP BY cs2.Country) AS 'Número de compatriotas'
+FROM w3schools.customers AS cs1
+WHERE
+    (SELECT COUNT(*) - 1
+      FROM w3schools.customers AS cs2
+      WHERE cs2.Country = cs1.Country
+      GROUP BY cs2.Country) > 0
+ORDER BY cs1.ContactName;
