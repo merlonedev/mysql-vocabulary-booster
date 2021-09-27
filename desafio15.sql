8617288bdf1627ad24d0ac6f52f1a6fb879aa7c8
Victor Emmanuel <victorpassos2012@gmail.com>
2021-09-27 19:29:08 -0300

    tentando resolver o codeclimate do desafio 15 segunda tentativa

--- desafio15.sql
+++ desafio15.sql
@@ -3,12 +3,13 @@ DELIMITER $$;
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
 BEGIN
 	SELECT 
-    ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
-FROM
-    hr.employees AS e
-        JOIN
-    hr.jobs AS j ON j.JOB_ID = e.JOB_ID
-    WHERE j.JOB_TITLE = cargo;
+		ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
+	FROM
+		hr.employees AS e
+	JOIN
+		hr.jobs AS j ON j.JOB_ID = e.JOB_ID
+    WHERE
+		j.JOB_TITLE = cargo;
 END $$;
 
 DELIMITER ;
