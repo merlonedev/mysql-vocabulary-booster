f535039f8589bc0b545700685587a657a78c03f5
Paulo Henrique de Paula Bomfim <paulopbomfim@gmail.com>
2021-09-27 10:36:23 -0300

    Requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+SELECT 
+    customer.CustomerName AS 'Nome de contato',
+    shipper.ShipperName AS 'Empresa que fez o envio',
+    orders.OrderDate AS 'Data do pedido'
+FROM
+    w3schools.orders AS orders
+        INNER JOIN
+    w3schools.customers AS customer ON customer.CustomerID = orders.CustomerID
+        INNER JOIN
+    w3schools.shippers AS shipper ON shipper.ShipperID = orders.ShipperID
+WHERE
+    shipper.ShipperID < 3
+ORDER BY 1 , 2 , 3 ASC;
