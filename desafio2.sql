e41b6db681bcb5f07a70c51a4be6362ce823796d
martinsph <ph.mmartins@gmail.com>
2021-09-27 16:52:12 -0300

    add req 3

--- desafio2.sql
+++ desafio2.sql
@@ -1,11 +1,11 @@
 SELECT 
     JOB_TITLE AS Cargo,
     CASE
-		WHEN MAX_SALARY between 5000 and 10000 then 'Baixo'
+		    WHEN MAX_SALARY between 5000 and 10000 then 'Baixo'
         WHEN MAX_SALARY between 10001 and 20000 then 'Médio'
         WHEN MAX_SALARY between 20001 and 30000 then 'Alto'
         WHEN MAX_SALARY > 30000 then 'Altíssimo'
-	END AS Nível
+	  END AS Nível
 FROM
     jobs
 ORDER BY Cargo;
