d4a8ca6148c6d26e9fb880e3fcac05bbc1483f79
igor fernandes <igorsife80@gmail.com>
2021-09-27 14:03:07 -0300

    da os devidos creditos a david gonzaga

--- desafio18.sql
+++ desafio18.sql
@@ -10,3 +10,4 @@ FROM
     hr.employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) , ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE) / 365,
         2);
+#todos os requisitos foram inspirados no pull request do david gonzaga  e sou totalmente capaz de explicar os codigos https://github.com/tryber/sd-012-mysql-vocabulary-booster/pull/99
