a1287d07edf485479f11aeb0ba19f8e31a23aa05
Paulo Henrique de Paula Bomfim <paulopbomfim@gmail.com>
2021-09-27 10:12:48 -0300

    Requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+SELECT 
+    UCASE(CONCAT(employees.FIRST_NAME,
+                    ' ',
+                    employees.LAST_NAME)) AS 'Nome completo',
+    job_history.START_DATE AS 'Data de início',
+    employees.SALARY AS 'Salário'
+FROM
+    hr.job_history AS job_history
+        INNER JOIN
+    hr.employees AS employees ON job_history.EMPLOYEE_ID = employees.EMPLOYEE_ID
+WHERE
+    MONTH(START_DATE) IN (1 , 2, 3)
+ORDER BY 1 , 2 ASC;
