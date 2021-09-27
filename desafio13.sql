b7a5b211de95efb346feccacf7aa3d8bee5dd28f
martinsph <ph.mmartins@gmail.com>
2021-09-27 20:56:39 -0300

    fix req 13

--- desafio13.sql
+++ desafio13.sql
@@ -1,9 +1,9 @@
-SELECT
-	p.ProductName AS Produto,
-    p.Price AS `Preço`
+SELECT 
+    p.ProductName AS Produto, p.Price AS Preço
 FROM
-	products AS p
-		INNER JOIN
-	order_details AS od ON p.ProductID = od.ProductID
-WHERE od.Quantity > 80
-ORDER BY Produto;
+    products AS p
+        INNER JOIN
+    order_details AS od ON p.ProductID = od.ProductID
+WHERE
+    od.Quantity > 80
+ORDER BY Produto
