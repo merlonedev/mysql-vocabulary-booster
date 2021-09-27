807211a739f2ae338886566bfba2fe90d34b46fc
Paulo Henrique de Paula Bomfim <paulopbomfim@gmail.com>
2021-09-27 17:08:13 -0300

    Requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,12 @@
+SELECT 
+    products.ProductName AS 'Produto',
+    MIN(order_details.Quantity) AS 'Mínima',
+    MAX(order_details.Quantity) AS 'Máxima',
+    ROUND(AVG(order_details.Quantity), 2) AS 'Média'
+FROM
+    w3schools.order_details AS order_details
+        INNER JOIN
+    w3schools.products AS products ON products.ProductID = order_details.ProductID
+GROUP BY products.ProductID
+HAVING AVG(order_details.Quantity) > 20.00
+ORDER BY 4 ASC , 1;
