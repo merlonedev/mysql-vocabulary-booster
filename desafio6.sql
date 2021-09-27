f15f54308a6e80e27627b049565f047dbf9147ee
Álvaro Palma <palma.alvaro@outlook.com>
2021-09-26 22:05:15 -0300

    Change join order in challenge 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,14 +1,14 @@
 SELECT
-CONCAT(t1.first_name, ' ', t1.last_name) AS `Nome completo`,
-t2.job_title AS `Cargo`,
-t3.start_date AS `Data de início do cargo`,
+CONCAT(t2.first_name, ' ', t2.last_name) AS `Nome completo`,
+t3.job_title AS `Cargo`,
+t1.start_date AS `Data de início do cargo`,
 t4.department_name AS `Departamento`
 FROM
-hr.employees t1
-INNER JOIN hr.jobs t2
-ON t1.job_id = t2.job_id
-INNER JOIN hr.job_history t3
-ON t1.employee_id = t3.employee_id
+hr.job_history t1
+INNER JOIN hr.employees t2
+ON t1.employee_id = t2.employee_id
+INNER JOIN hr.jobs t3
+ON t1.job_id = t3.job_id
 INNER JOIN hr.departments t4
 ON t1.department_id = t4.department_id
 ORDER BY `Nome completo` DESC, `Cargo`;
