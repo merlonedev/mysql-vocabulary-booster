c1cc7c58ce6e4916c88638144448c59be576e3f3
Augusto <augustocm2002@yahoo.com.br>
2021-09-27 19:03:18 -0300

    Resolve requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,12 @@
+SELECT
+p.ProductName AS 'Produto',
+    MIN(o.Quantity) AS 'Mínima',
+    MAX(o.Quantity) AS 'Máxima',
+    ROUND(AVG(o.Quantity), 2) AS 'Média'
+FROM
+w3schools.products AS p
+	INNER JOIN
+    w3schools.order_details AS o ON o.ProductID = p.ProductID
+GROUP BY p.ProductName
+HAVING ROUND(AVG(o.Quantity), 2) > 20
+ORDER BY ROUND(AVG(o.Quantity), 2), p.ProductName;
