83939e69500d72806a2ac5c952b894cbf19f3ab7
igor fernandes <igorsife80@gmail.com>
2021-09-27 11:43:17 -0300

    tenta corrigir os linters

--- desafio8.sql
+++ desafio8.sql
@@ -1,13 +1,5 @@
-SELECT
-	wc.ContactName AS `Nome de contato`,
-    ws.ShipperName AS `Empresa que fez o envio`,
-    wo.OrderDate AS `Data do pedodo`
-FROM 
-	w3schools.customers as wc
-		INNER JOIN 
-	w3schools.orders AS wo ON wo.CustomerID = wc.CustomerID
-		INNER JOIN 
-	w3schools.shippers AS ws ON ws.ShipperID = wo.ShipperID
-WHERE
-	ws.ShipperName = 'Speedy Express'
-		OR ws.ShipperName = 'United Package'
+SELECT wc.ContactName AS `Nome de contato` ,ws.ShipperName AS `Empresa que fez o envio`,  wo.OrderDate AS `Data do pedodo`
+FROM w3schools.customers as wc
+INNER JOIN w3schools.orders AS wo ON wo.CustomerID = wc.CustomerID
+INNER JOIN w3schools.shippers AS ws ON ws.ShipperID = wo.ShipperID
+WHERE ws.ShipperName = 'Speedy Express' OR ws.ShipperName = 'United Package'
