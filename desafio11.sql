da9a40cd110c73d33551ae66dfbf12f3abba1923
martinsph <ph.mmartins@gmail.com>
2021-09-27 20:27:50 -0300

    add req 11

---
+++ desafio11.sql
@@ -0,0 +1,9 @@
+SELECT 
+    t1.CustomerName AS `Nome`,
+    t1.Country AS `País`,
+    COUNT(t2.CustomerID) AS `Número de compatriotas`
+FROM
+    customers AS t1, customers AS t2
+WHERE t1.Country = t2. Country AND t1.CustomerID <> t2.CustomerID
+GROUP BY `Nome`
+ORDER BY t1.ContactName;
