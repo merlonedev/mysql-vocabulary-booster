4dd39056b983aac859ee87e7d18c68b57cab683c
MaxAlves0035 <maxalves2@hotmail.com>
2021-09-27 16:06:11 -0300

    desafio10

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT 
+    P.ProductName AS 'Produto',
+    MIN(D.Quantity) AS 'Mínima',
+    MAX(D.Quantity) AS 'Máxima',
+    ROUND(AVG(D.Quantity), 2) AS 'Média'
+FROM
+    w3schools.products AS P
+        JOIN
+    w3schools.order_details AS D ON D.ProductID = P.ProductID
+GROUP BY D.ProductID
+ORDER BY `Média` ASC , `Produto` ASC;
