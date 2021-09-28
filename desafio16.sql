d17f998a71fab459fc7c345de7a0f21df79b8a95
Victor Emmanuel <victorpassos2012@gmail.com>
2021-09-27 21:30:49 -0300

    Desafio 16 feito

---
+++ desafio16.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario (userEmail VARCHAR(200))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE emailcount INT;
+SELECT COUNT(jh.EMPLOYEE_ID) FROM hr.employees AS e JOIN hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE EMAIL = userEmail
+INTO emailcount;
+RETURN emailcount;
+END $$ ;
+
+DELIMITER ;
+
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR')
