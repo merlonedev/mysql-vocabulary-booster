f01fc851a9215a2c7d2d43b294417cb3ed779d53
Ana-Jacomassi <anajacomassip@gmail.com>
2021-09-28 13:50:38 -0300

    +tentativa de correçao desafio 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,16 +1,14 @@
-SELECT 
-    DISTINCT CONCAT(emA.FIRST_NAME, ' ', emA.LAST_NAME) AS 'Nome completo funcionário 1',
+SELECT DISTINCT
+    CONCAT(emA.FIRST_NAME, ' ', emA.LAST_NAME) AS 'Nome completo funcionário 1',
     emA.SALARY AS 'Salário funcionário 1',
     emA.PHONE_NUMBER AS 'Telefone funcionário 1',
     CONCAT(emB.FIRST_NAME, ' ', emB.LAST_NAME) AS 'Nome completo funcionário 2',
     emB.SALARY AS 'Salário funcionário 2',
     emB.PHONE_NUMBER AS 'Telefone funcionário 2'
 FROM
-	hr.employees emA
-RIGHT JOIN
-	hr.employees emB
-ON 
-    emA.JOB_ID = emB.JOB_ID
+    hr.employees emA
+        RIGHT JOIN
+    hr.employees emB ON emA.JOB_ID = emB.JOB_ID
 WHERE
     emA.EMPLOYEE_ID <> emB.EMPLOYEE_ID
-ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
+ORDER BY `Nome completo funcionário 1` , `Nome completo funcionário 2`;
