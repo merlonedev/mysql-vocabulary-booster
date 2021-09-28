2811f8f616d7db555eba6add9580b2235c6ec0e5
josimarschneider <josimarschneider@gmail.com>
2021-09-28 09:19:12 -0400

    requisito 11 - Exibe todas as pessoas clientes que possuem...

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+SELECT 
+    C1.ContactName AS `Nome`,
+    C1.Country AS `País`,
+    COUNT(*) -1 AS `Número de compatriotas`
+FROM
+    w3schools.customers AS C1, 
+    customers AS C2
+WHERE C1.Country = C2.Country
+GROUP BY C1.ContactName, C2.Country
+having `Número de compatriotas` > 0
+ORDER BY `Nome`;
+
