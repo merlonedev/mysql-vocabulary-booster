6aa3f015f98b28f0b11f81c182fbb8cef43b549a
Antonio Malato <malatoneto@gmail.com>
2021-09-28 01:53:05 -0300

    Requisito 3 corrigido - add order by

--- desafio3.sql
+++ desafio3.sql
@@ -2,4 +2,5 @@ SELECT
     JOB_TITLE AS 'Cargo',
     MAX_SALARY - MIN_SALARY AS 'Diferença entra salários máximo e mínimo'
 FROM
-    hr.jobs;
+    hr.jobs
+ORDER BY `Diferença entra salários máximo e mínimo`, Cargo;
