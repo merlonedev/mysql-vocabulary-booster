076140490dda97974119defe76c8a3f2a94625e2
Antonio Malato <malatoneto@gmail.com>
2021-09-28 02:21:00 -0300

    requisito 2- colocado else

--- desafio2.sql
+++ desafio2.sql
@@ -4,8 +4,10 @@ SELECT
         WHEN MAX_SALARY BETWEEN 5000 AND 10000 THEN 'Baixo'
         WHEN MAX_SALARY BETWEEN 10001 AND 20000 THEN 'Médio'
         WHEN MAX_SALARY BETWEEN 20001 AND 30000 THEN 'Alto'
-        WHEN MAX_SALARY > 300000 THEN 'Altíssimo'
+        ELSE 'Altíssimo'
     END AS 'Nivel'
 FROM
     hr.jobs
-ORDER BY `Cargo` ASC;
+ORDER BY Cargo ASC;
+
+
