c16c95fba64621d7dfbad1f9a74255690c82b7a7
Adryan Rosa <adryanrosa374@gmail.com>
2021-09-27 21:38:38 -0300

    Corrige codeclimate

--- desafio2.sql
+++ desafio2.sql
@@ -1,15 +1,15 @@
 SELECT
-    j.JOB_TITLE AS 'Cargo',
+    JOB_TITLE AS 'Cargo',
     CASE
-        WHEN j.MAX_SALARY BETWEEN 5000
+        WHEN MAX_SALARY BETWEEN 5000
         AND 10000 THEN 'Baixo'
-        WHEN j.MAX_SALARY BETWEEN 10001
+        WHEN MAX_SALARY BETWEEN 10001
         AND 20000 THEN 'Médio'
-        WHEN j.MAX_SALARY BETWEEN 20001
+        WHEN MAX_SALARY BETWEEN 20001
         AND 30000 THEN 'Alto'
-        WHEN j.MAX_SALARY > 30000 THEN 'Altíssimo'
+        WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
     END AS 'Nível'
 FROM
-    hr.jobs AS j
+    hr.jobs
 ORDER BY
-    'Nível';
+    'Cargo';
