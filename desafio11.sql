dbfa0a1d4591d1e7bd4ac712f0e85d4a58adc4cf
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-09-27 21:40:52 -0300

    exercicio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,10 +1,12 @@
-SELECT 
-    t1.ContactName AS `Nome`,
-    t1.Country AS `País`,
-    COUNT(t2.ContactName) AS `Número de compatriotas`
+SELECT
+  t1.ContactName AS `Nome`,
+  t1.Country AS `País`,
+  COUNT(t2.ContactName) AS `Número de compatriotas`
 FROM
-    customers AS t1,
-    customers AS t2
+  customers AS t1,
+  customers AS t2
 WHERE
-    t1.Country = t2.Country AND t1.ContactName <> t2.ContactName
- GROUP BY t1.ContactName;
+  t1.Country = t2.Country
+  AND t1.ContactName <> t2.ContactName
+GROUP BY
+  t1.ContactName;
