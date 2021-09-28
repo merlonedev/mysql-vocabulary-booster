c1fc3d83a027a236a5fac9af429ac2233e13f4c9
Sass <dudu_sass12@hotmail.com>
2021-09-28 12:11:17 -0300

    30001 e nome job_ mudado

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 SELECT
 CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-j.JOB_ID AS Cargo,
+j.JOB_TITLE AS Cargo,
 sd.START_DATE AS 'Data de início do cargo',
-d.DEPARTMENT_ID AS 'Departamento'
+d.DEPARTMENT_NAME AS Departamento
 FROM hr.job_history AS sd
 JOIN hr.employees AS e ON sd.EMPLOYEE_ID = e.EMPLOYEE_ID
 JOIN hr.jobs AS j ON sd.JOB_ID = j.JOB_ID
