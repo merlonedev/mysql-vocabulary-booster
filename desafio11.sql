2d7f379cb1e12505af8ed7b85967dbce11d6a3ce
Matheus Figueiredo <matheus.b.figueiredo@outlook.com>
2021-09-28 09:06:22 -0300

    fazendo as vontades do code climate dnv e deixando o código ainda pior

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 SELECT
-	CUST1.ContactName AS 'Nome',
-    CUST1.Country AS 'País',
-    count(CUST1.Country) AS 'Número de compatriotas'
+CUST1.ContactName AS 'Nome',
+CUST1.Country AS 'País',
+count(CUST1.Country) AS 'Número de compatriotas'
 FROM
 w3schools.customers as CUST1, w3schools.customers AS CUST2
 WHERE
