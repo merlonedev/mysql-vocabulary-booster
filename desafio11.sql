2988b56cfb4229fd5569e7d2afbeb12ed2d91ca8
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-09-27 21:37:55 -0300

    exercicio 11

---
+++ desafio11.sql
@@ -0,0 +1,10 @@
+SELECT 
+    t1.ContactName AS `Nome`,
+    t1.Country AS `País`,
+    COUNT(t2.ContactName) AS `Número de compatriotas`
+FROM
+    customers AS t1,
+    customers AS t2
+WHERE
+    t1.Country = t2.Country AND t1.ContactName <> t2.ContactName
+ GROUP BY t1.ContactName;
