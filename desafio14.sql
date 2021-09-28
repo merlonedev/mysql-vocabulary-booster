60bc8b3345c7172c68e1d54fdb5f4522d1c20a8b
Eurides <euridesalves@yahoo.com.br>
2021-09-28 11:33:08 -0300

    Adicionado requisito 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,9 +1,11 @@
 SELECT DISTINCT
     c.Country AS 'País'
 FROM
-    w3schools.customers as c
-    join
-    w3schools.suppliers as s
-    where c.Country = s.Country
-    order by c.Country
-    limit 5;
+    w3schools.customers AS c
+    union
+SELECT DISTINCT
+    s.Country AS 'País'
+FROM
+    w3schools.suppliers AS s
+order by `País`
+limit 5;
