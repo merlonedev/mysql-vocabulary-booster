ebb339db0990b93b192807bc39a81af0597413c2
Sass <dudu_sass12@hotmail.com>
2021-09-28 08:21:01 -0300

    requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+SELECT
+CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+j.JOB_ID AS Cargo,
+sd.START_DATE AS 'Data de início do cargo',
+d.DEPARTMENT_ID AS 'Departamento'
+FROM hr.job_history AS sd
+JOIN hr.employees AS e ON sd.EMPLOYEE_ID = e.EMPLOYEE_ID
+JOIN hr.jobs AS j ON sd.JOB_ID = j.JOB_ID
+JOIN hr.departments AS d ON sd.DEPARTMENT_ID = d.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, Cargo;
