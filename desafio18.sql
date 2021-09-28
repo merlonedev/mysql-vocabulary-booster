c3860ef0a4aebd13a3c113cc6d569168fd1d248e
Marcela Silva <marcela.ops@hotmail.br>
2021-09-28 11:54:28 -0300

    fixing codeclimate desafios 18,20

--- desafio18.sql
+++ desafio18.sql
@@ -1,10 +1,11 @@
 SELECT 
-	CONCAT(e.first_name, ' ',e.last_name) AS 'Nome completo',
-	-- CONCAT(DAY(jh.start_date), '/', MONTH(jh.start_date), '/', YEAR(jh.start_date)) as 'Data de início', Como não leva o zero dá ruim nos testes, achei esse DATE_FORMAT:
-	-- CONCAT(DAY(jh.end_date), '/', MONTH(jh.end_date), '/', YEAR(jh.end_date)) as 'Data de rescisão',
-    DATE_FORMAT(jh.start_date, '%d/%m/%Y') as 'Data de início',
-    DATE_FORMAT(jh.end_date, '%d/%m/%Y') as 'Data de rescisão',
-    ROUND((DATEDIFF(end_date, start_date) / 365), 2) as 'Anos trabalhados'
-FROM job_history as jh
-JOIN employees as e ON e.employee_id = jh.employee_id
-ORDER BY `Nome completo`, `Anos trabalhados`;
+    CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
+    DATE_FORMAT(jh.start_date, '%d/%m/%Y') AS 'Data de início',
+    DATE_FORMAT(jh.end_date, '%d/%m/%Y') AS 'Data de rescisão',
+    ROUND((DATEDIFF(end_date, start_date) / 365),
+            2) AS 'Anos trabalhados'
+FROM
+    job_history AS jh
+        JOIN
+    employees AS e ON e.employee_id = jh.employee_id
+ORDER BY `Nome completo` , `Anos trabalhados`;
