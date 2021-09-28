bac7dc862fb31df8935b641b7f633d79e005c812
martinsph <ph.mmartins@gmail.com>
2021-09-28 18:22:55 -0300

    fix req 15

--- desafio15.sql
+++ desafio15.sql
@@ -1,9 +1,9 @@
 -- DROP PROCEDURE IF EXISTS buscar_media_por_cargo;
+USE hr;
 
 DELIMITER $$
 
-CREATE PROCEDURE buscar_media_por_cargo(
-IN job_title VARCHAR(100))
+CREATE PROCEDURE buscar_media_por_cargo(IN job_title VARCHAR(100))
 BEGIN
 SELECT ROUND(AVG(SALARY), 2) AS `Média salarial` FROM employees
 WHERE JOB_ID IN (SELECT JOB_ID FROM jobs WHERE JOB_TITLE = job_title);
