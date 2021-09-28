f8c8d3806688618309e9879b838632ef461af68f
Matheus Figueiredo <matheus.b.figueiredo@outlook.com>
2021-09-28 09:11:33 -0300

    arrumado erro na 10

--- desafio10.sql
+++ desafio10.sql
@@ -2,11 +2,11 @@ SELECT
 PROD.ProductName AS 'Produto',
 MIN(OD.Quantity) AS 'Mínima',
 MAX(OD.Quantity) AS 'Máxima',
-AVG(OD.Quantity) AS 'Média'
+ROUND(AVG(OD.Quantity), 2) AS 'Média'
 FROM
 w3schools.products AS PROD
 JOIN
 w3schools.order_details AS OD ON OD.ProductID = PROD.ProductID
 GROUP BY PROD.ProductName
-HAVING AVG(OD.Quantity) > 20.00
-ORDER BY AVG(OD.Quantity) , PROD.ProductName;
+HAVING ROUND(AVG(OD.Quantity), 2) > 20.00
+ORDER BY ROUND(AVG(OD.Quantity), 2) , PROD.ProductName;
