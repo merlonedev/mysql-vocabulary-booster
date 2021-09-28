c8312d55d77fac7e939864ed85fe205e50270d52
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-09-27 22:16:19 -0300

    exercicio 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,4 +1,16 @@
 SELECT
-  CONCAT(FirstName, ' ', LastName) AS `Nome completo Funcionario `
+  CONCAT(t1.First_Name, ' ', t1.Last_Name) AS `Nome completo funcionário 1`,
+  t1.salary AS `Salário funcionário 1`,
+  t1.phone_number AS `Telefone funcionário 1`,
+  CONCAT(t2.First_Name, ' ', t2.Last_Name) AS `Nome completo funcionário 2`,
+  t2.salary AS `Salário funcionário 2`,
+  t2.phone_number AS `Telefone funcionário 2`
 FROM
-  employees;
+  employees AS t1,
+  employees AS t2
+WHERE
+  t1.job_id = t2.job_id
+  AND CONCAT(t1.First_Name, ' ', t1.Last_Name) <> CONCAT(t2.First_Name, ' ', t2.Last_Name)
+ORDER BY
+  1,
+  4;
