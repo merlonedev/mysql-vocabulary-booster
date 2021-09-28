19d30a7a7d1b02dc0b3056539131c2a4684a04a5
Antonio Malato <malatoneto@gmail.com>
2021-09-28 01:45:10 -0300

    Requisito 1 - mudar sinal

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,9 @@
 SELECT 
-    COUNTRY_NAME AS Pais,
-    IF(REGION_ID = 1,
-        'incluido',
-        'não incluido') AS 'Status Inclusão'
-FROM
-    hr.countries
-ORDER BY COUNTRY_NAME ASC;
+  COUNTRY_NAME AS 'País', 
+  IF(
+    REGION_ID = 1, 'incluído', 'não incluído'
+  ) AS 'Status Inclusão' 
+FROM 
+  hr.countries 
+ORDER BY 
+  COUNTRY_NAME;
