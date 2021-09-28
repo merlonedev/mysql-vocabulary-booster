1f52f80b8e0cf5d3824fc9ce91e577ce4b901780
martinsph <ph.mmartins@gmail.com>
2021-09-28 20:01:15 -0300

    add req 16

--- desafio15.sql
+++ desafio15.sql
@@ -6,7 +6,7 @@ CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(50))
 
 BEGIN
 SELECT ROUND(AVG(SALARY), 2) AS `Média salarial` FROM employees
-WHERE JOB_ID IN (SELECT JOB_ID FROM hr.jobs WHERE JOB_TITLE = job);
+WHERE JOB_ID IN (SELECT JOB_ID FROM jobs WHERE JOB_TITLE = job);
 
 END
 
---
+++ desafio16.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE qtd_empregos INT;
+SELECT COUNT(jh.EMPLOYEE_ID) FROM job_history jh INNER JOIN hr.employees e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE e.EMAIL = email
+INTO qtd_empregos;
+RETURN qtd_empregos;
+END
+
+$$ DELIMITER ;
+
+-- SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
