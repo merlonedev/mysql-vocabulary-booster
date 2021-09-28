cad8920dc61544fff415091e05a764d4c6205b59
Adryan Rosa <adryanrosa374@gmail.com>
2021-09-27 22:59:19 -0300

    Resolve desafio 14

---
+++ desafio14.sql
@@ -0,0 +1,17 @@
+(
+  SELECT
+    Country AS `País`
+  FROM
+    w3schools.customers
+)
+UNION
+(
+  SELECT
+    Country AS `País`
+  FROM
+    w3schools.suppliers
+)
+ORDER BY
+  `País`
+LIMIT
+  5;
