94b60c34cc78a4d08e69625a157bf00de2ab0240
Adryan Rosa <adryanrosa374@gmail.com>
2021-09-27 21:36:56 -0300

    Teste desafio 2

--- desafio1.sql
+++ desafio1.sql
@@ -1,11 +1,11 @@
 SELECT
-    c.COUNTRY_NAME AS 'País',
+    COUNTRY_NAME AS 'País',
     IF(
-        c.REGION_ID = 1,
+        REGION_ID = 1,
         'incluído',
         'não incluído'
     ) AS 'Status Inclusão'
 FROM
-    hr.countries AS c
+    hr.countries
 ORDER BY
-    c.COUNTRY_NAME;
+    COUNTRY_NAME;
