f9f21a18f6c54c07d5f5a45364a6a15aa25bdaf6
Antonio Malato <malatoneto@gmail.com>
2021-09-28 01:24:39 -0300

    Teste correçao requisito 1 - sem acento

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,8 @@
 SELECT 
-    COUNTRY_NAME AS País,
+    COUNTRY_NAME AS Pais,
     IF(REGION_ID = 1,
         'incluido',
         'não incluido') AS 'Status Inclusão'
 FROM
     hr.countries
-ORDER BY COUNTRY_NAME;
+ORDER BY COUNTRY_NAME ASC;
--- desafio10.sql
+++ desafio10.sql
@@ -1,8 +1,8 @@
 SELECT 
     p.ProductName AS 'Produto',
-    MIN(o.Quantity) AS 'Mínima',
-    MAX(o.Quantity) AS 'Máxima',
-    ROUND(AVG(o.Quantity), 2) AS 'Média'
+    MIN(Quantity) AS 'Mínima',
+    MAX(Quantity) AS 'Máxima',
+    ROUND(AVG(Quantity), 2) AS 'Média'
 FROM
     w3schools.order_details AS o
         INNER JOIN
