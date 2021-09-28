f52f264d94ae837a97748de5df49a0c7ab2b7b34
Luís Seixas <luiseixas2@gmail.com>
2021-09-28 11:48:02 -0300

    ajusta o desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,7 @@
 SELECT
-    UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
-    jh.START_DATE AS 'Data de início'
+    UPPER(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+    jh.START_DATE AS 'Data de início',
+    e.SALARY AS 'Salário' 
 FROM
     hr.employees AS e
         INNER JOIN
