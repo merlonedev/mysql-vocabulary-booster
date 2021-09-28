a5a38ffdf9a9620fea5da2b04d083a9d27c6ddef
Luís Seixas <luiseixas2@gmail.com>
2021-09-28 11:07:59 -0300

    ajusta o desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -9,6 +9,6 @@ FROM
     hr.job_history AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
         INNER JOIN
     hr.jobs AS j ON j.JOB_ID = jh.JOB_ID
-		INNER JOIN
+        INNER JOIN
     hr.departments AS d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC , j.JOB_TITLE;
