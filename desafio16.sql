63cc1d62588477808d46090c35e59ba2dbd499a4
Rafael Cardoso <rafael.vimur@gmail.com>
2021-09-29 16:35:20 -0300

    adiciona requisitos 15 ao 17

---
+++ desafio16.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+CREATE FUNCTION
+    buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(80))
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE jobsons INT;
+    SELECT COUNT(*) AS total_empregos
+    FROM     
+        hr.job_history
+            INNER JOIN
+        hr.employees AS e ON e.EMPLOYEE_ID = hr.job_history.EMPLOYEE_ID
+    WHERE e.EMAIL = email INTO jobsons;
+    RETURN jobsons;
+END
+$$
+
+DELIMITER ;
