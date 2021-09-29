6ca75205420798b4580124e273a2e49c8b7c1ea3
Rafael Cardoso <rafael.vimur@gmail.com>
2021-09-29 15:54:08 -0300

    correção requisitos 4, e adiciona requisitos 7 ao 10

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+SELECT 
+    UCASE(CONCAT(i.FIRST_NAME, ' ', i.LAST_NAME)) AS 'Nome completo',
+    h.START_DATE AS 'Data de início',
+    i.SALARY AS 'Salário'
+FROM
+    job_history AS h
+        INNER JOIN
+    employees AS i ON i.EMPLOYEE_ID = h.EMPLOYEE_ID
+WHERE
+    MONTH(h.START_DATE) BETWEEN 1 AND 3
+ORDER BY `Nome completo` , `Data de início`;
---
+++ desafio9.sql
@@ -0,0 +1,9 @@
+SELECT 
+    CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
+    COUNT(*) AS 'Total de pedidos'
+FROM
+    orders AS o
+        INNER JOIN
+    employees AS e ON o.EmployeeID = e.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`;
