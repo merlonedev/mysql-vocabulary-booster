fc1a058d8c2e58ad569f5f7249b0345487e590f4
Rafael Cardoso <rafael.vimur@gmail.com>
2021-09-29 15:17:26 -0300

    requisitos 1 ao 3

---
+++ desafio1.sql
@@ -0,0 +1,8 @@
+SELECT 
+    hr.countries.COUNTRY_NAME AS 'País',
+    IF(hr.countries.REGION_ID = 1,
+        'incluído',
+        'não incluído') AS 'Status Inclusão'
+FROM
+    hr.countries
+ORDER BY País;
