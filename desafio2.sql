3bbd2711da1efefa5dbd7fa365c7d9677c10c92c
Elisa-Libanio <elisalibanio1@gmail.com>
2021-09-29 15:33:49 -0300

    ola

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,9 @@
 select JOB_ID as "Cargo" ,
 case 
-when salary between  5000 and  10000 then " Baixo" 
+ WHEN salary BETWEEN  5000 and  10000 then " Baixo" 
  WHEN salary BETWEEN 5000 and 10000 THEN 'Baixo'
-when salary between  10001 and 20000  then "Médio" 
-when salary between  20001 and 30000 then "Alto"	
+ WHEN salary BETWEEN  10001 and 20000  then "Médio" 
+ WHEN salary BETWEEN  20001 and 30000 then "Alto"	
 else  "Altíssimo"	
 end as 'Nível' 
 from hr.employees; 
